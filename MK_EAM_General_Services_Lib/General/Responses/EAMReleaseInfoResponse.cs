7518fe884d0a95352a5d087ed935d7fb2815de0a
MaikEight <mail@maik8.de>
2023-04-10 04:29:02 +0200

    🎨 Update file

---
+++ MK_EAM_General_Services_Lib/General/Responses/EAMReleaseInfoResponse.cs
@@ -0,0 +1,10 @@
+﻿
+namespace MK_EAM_General_Services_Lib.General.Responses
+{
+    [System.Serializable]
+    public sealed class EAMReleaseInfoResponse
+    {
+        public string ReleaseLink { get; set; } = string.Empty;
+        public string ReleaseDownloadLink { get; set; } = string.Empty;
+    }
+}
