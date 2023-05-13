e6e57d26a7f970e6fbe7742a6ebd0d73797e7617
MaikEight <mail@maik8.de>
2023-05-13 23:45:13 +0200

    ✨Added a new class for DiscordUser data

---
+++ Libraries/MK_EAM_General_Services_Lib/General/Responses/DiscordUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MK_EAM_General_Services_Lib.General.Responses
+{
+    [System.Serializable]
+    public sealed class DiscordUser
+    {
+        public Guid DiscordUserId { get; set; }
+        public DateTime LastSeen { get; set; }
+        public DateTime FirstSeen { get; set; }
+        public int AmountOfAccounts { get; set; }
+        public int AmountOfSessions { get; set; }
+        public int minutesOfEamUseTime { get; set; }
+        public bool EasterEggFound { get; set; }
+    }
+}
