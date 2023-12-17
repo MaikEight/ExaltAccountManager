4b2891fb1ae4bfa37517beecec2002d7723482ab
MaikEight <mail@maik8.de>
2023-12-17 10:50:54 +0100

    💄Added a headline and icon

--- src/components/ComponentBox.jsx
+++ src/components/ComponentBox.jsx
@@ -1,22 +1,46 @@
 import { useTheme } from "@emotion/react";
-import { Box, Paper } from "@mui/material";
-
-function ComponentBox({children, sx}) {
+import { Box, Paper, Typography } from "@mui/material";
+function ComponentBox({ children, headline, icon, sx }) {
     const theme = useTheme();
-    
+
     return (
         <Paper sx={{
             display: "flex",
+            flexDirection: "column",
             flexGrow: 1,
             borderRadius: 1.5,
             paddingLeft: 1.5,
             paddingRight: 1.5,
-            m: 3,
+            paddingTop: 1.5,
+            paddingBottom: 1.5,
+            m: 2,
             background: theme.palette.background.paper,
             ...sx
-        
+
         }}>
-            {children}
+            {
+                (icon || headline) &&
+                <Box 
+                    sx={{
+                        display: "flex",
+                        flexDirection: "row",
+                        alignItems: "center",
+                        justifyContent: "start",
+                        mb: 1,
+                    }}
+                >                                        
+                    {icon && icon}
+                    {
+                        headline &&
+                        <Typography variant="h6" sx={{ fontWeight: 600, textAlign: 'center' }}>
+                            {headline}
+                        </Typography>
+                    }
+                </Box>
+            }
+            <Box>
+                {children}
+            </Box>
         </Paper>
     );
 }
