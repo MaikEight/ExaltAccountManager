ceae05e37dcf402a2ed9ed4c1d1d05f72ee452ac
MaikEight <mail@maik8.de>
2023-12-17 10:59:28 +0100

    ⚡️Add colorMap to optimize useStringToColor function

--- src/hooks/useStringToColor.js
+++ src/hooks/useStringToColor.js
@@ -3,7 +3,10 @@ import useColorList from './useColorList';
 
 const useStringToColor = (inputString) => {    
     const colorList = useColorList();
+    const colorMap = { Default: 0 };
 
+    if(typeof(colorMap[inputString]) !== 'undefined') return colorList[colorMap[inputString]];
+    
     const generateColorIndex = (name) => {
         const hash = hashString(name);
         const hashInteger = Math.abs(parseInt(hash, 16));
@@ -11,6 +14,7 @@ const useStringToColor = (inputString) => {
     };
 
     const index = generateColorIndex(inputString);
+    colorMap[inputString] = index;
     return colorList[index];
 };
 
