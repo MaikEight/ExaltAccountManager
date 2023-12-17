23afc7051aa2967fb1eed3a224bebf1d8d2fb62b
MaikEight <mail@maik8.de>
2023-12-17 10:59:02 +0100

    ✨Add navigation functionality to Sidebar component

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -4,40 +4,51 @@ import NewspaperOutlinedIcon from '@mui/icons-material/NewspaperOutlined';
 import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
 import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
 import SidebarButton from "./SidebarButton";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useTheme } from '@mui/system';
 import ColorContext from '../../contexts/ColorContext';
 import CustomToolbar from "./CustomToolbar";
 import SideBarLogo from "./SideBarLogo";
+import { useNavigate } from "react-router-dom";
 
 
 function Sidebar({ children }) {
     const [selectedIndex, setSelectedIndex] = useState(0);
     const theme = useTheme();
     const colorContext = useContext(ColorContext);
+    const navigate = useNavigate();
 
     const menuItems = [
         {
             name: 'Accounts',
             icon: <GroupOutlinedIcon />,
             action: () => setSelectedIndex(0),
+            navigate: '/accounts'
         },
         {
             name: 'News',
             icon: <NewspaperOutlinedIcon />,
             action: () => setSelectedIndex(1),
+            navigate: '/news'
         },
         {
             name: 'Settings',
             icon: <SettingsOutlinedIcon />,
             action: () => setSelectedIndex(2),
+            navigate: '/settings'
         },
         {
             name: 'About',
             icon: <InfoOutlinedIcon />,
             action: () => { setSelectedIndex(3); colorContext.toggleColorMode(); },
+            navigate: '/about'
         },
     ];
+
+    useEffect(() => {
+        console.log(menuItems[selectedIndex].navigate);
+        navigate(menuItems[selectedIndex].navigate);
+    }, [selectedIndex]);
 
     return (
         <Box sx={{ display: "flex", flexDirection: 'column' }}>
