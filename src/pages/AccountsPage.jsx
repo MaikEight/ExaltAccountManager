436f18599875af73d2f8cac1cdec6e2a197e4341
MaikEight <mail@maik8.de>
2023-12-17 11:00:03 +0100

    ✨Add account selection functionality to AccountsPage component

--- src/pages/AccountsPage.jsx
+++ src/pages/AccountsPage.jsx
@@ -1,7 +1,60 @@
 import { Box } from "@mui/material";
 import AccountGrid from "../components/AccountGrid";
+import { useEffect, useState } from "react";
+import { SAVE_FILE_NAME, SAVE_FILE_PATH } from "../constants";
+import { readFileUTF8 } from "../utils/readFileUtil";
+import AccountDetails from "../components/AccountDetails/AccountDetails";
+import { useSearchParams } from "react-router-dom";
 
 function AccountsPage() {
+    const [accounts, setAccounts] = useState([]);
+    const [selectedAccount, setSelectedAccount] = useState(null);
+
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    useEffect(() => {
+        const filePath = SAVE_FILE_PATH + SAVE_FILE_NAME;
+        readFileUTF8(filePath, true)
+            .then((accounts) => {
+                if (accounts) {
+                    setAccounts(accounts);
+                    handleSelectedAccountParameter(accounts);
+                }
+            });
+    }, []);
+
+    useEffect(() => {
+        handleSelectedAccountParameter(accounts);
+    }, [searchParams]);
+
+    useEffect(() => {
+        handleSelectedAccountParameter(accounts);
+    }, [accounts]);
+
+    useEffect(() => {
+        const updatedSearchParams = new URLSearchParams(searchParams.toString());
+
+        if (!selectedAccount || !selectedAccount.email) {
+            updatedSearchParams.delete('selectedAccount');
+            setSearchParams(updatedSearchParams.toString());
+            return;
+        }
+
+        updatedSearchParams.set('selectedAccount', selectedAccount.email);
+        setSearchParams(updatedSearchParams.toString());
+        searchParams.set('selectedAccount', selectedAccount.email);
+    }, [selectedAccount]);
+
+    const handleSelectedAccountParameter = (accs) => {
+        if (searchParams.has('selectedAccount')) {
+            const selectedAccountEmail = searchParams.get('selectedAccount');
+            const selectedAccount = accs.find((account) => account.email === selectedAccountEmail);
+            if (selectedAccount) {
+                setSelectedAccount(selectedAccount);
+            }
+        }
+    }
+
     return (
         <Box id="accountspage"
             sx={{
@@ -9,7 +62,8 @@ function AccountsPage() {
                 p: 2,
             }}
         >
-            <AccountGrid />
+            <AccountGrid acc={accounts} selected={selectedAccount} setSelected={setSelectedAccount} />
+            <AccountDetails acc={selectedAccount} onClose={() => setSelectedAccount(null)} />            
         </Box>
     );
 }
