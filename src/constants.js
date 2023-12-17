e0867b7ed310881ebe96f53386e5b870ae534014
MaikEight <mail@maik8.de>
2023-12-17 06:22:57 +0100

    ✨Added a a few constants

---
+++ src/constants.js
@@ -0,0 +1,97 @@
+export const SAVE_FILE_PATH = 'C:\\Users\\Maik8\\Desktop\\ExaltAccountManager\\';
+export const SAVE_FILE_NAME = 'accounts.json';
+
+
+//TEMPORARY
+export const SERVERS = [
+    {
+        "name": "EUEast",
+        "ip": "18.184.218.174",
+        "usage": 0
+    },
+    {
+        "name": "EUSouthWest",
+        "ip": "35.180.67.120",
+        "usage": 0
+    },
+    {
+        "name": "USEast2",
+        "ip": "54.209.152.223",
+        "usage": 0
+    },
+    {
+        "name": "EUNorth",
+        "ip": "18.159.133.120",
+        "usage": 0
+    },
+    {
+        "name": "USEast",
+        "ip": "54.234.226.24",
+        "usage": 75
+    },
+    {
+        "name": "USWest4",
+        "ip": "54.235.235.140",
+        "usage": 0
+    },
+    {
+        "name": "EUWest2",
+        "ip": "52.16.86.215",
+        "usage": 0
+    },
+    {
+        "name": "Asia",
+        "ip": "3.0.147.127",
+        "usage": 0
+    },
+    {
+        "name": "USSouth3",
+        "ip": "52.207.206.31",
+        "usage": 0
+    },
+    {
+        "name": "EUWest",
+        "ip": "15.237.60.223",
+        "usage": 0
+    },
+    {
+        "name": "USWest",
+        "ip": "54.86.47.176",
+        "usage": 0
+    },
+    {
+        "name": "USMidWest2",
+        "ip": "3.140.254.133",
+        "usage": 0
+    },
+    {
+        "name": "USMidWest",
+        "ip": "18.221.120.59",
+        "usage": 0
+    },
+    {
+        "name": "USSouth",
+        "ip": "3.82.126.16",
+        "usage": 0
+    },
+    {
+        "name": "USWest3",
+        "ip": "18.144.30.153",
+        "usage": 0
+    },
+    {
+        "name": "USSouthWest",
+        "ip": "54.153.13.68",
+        "usage": 0
+    },
+    {
+        "name": "USNorthWest",
+        "ip": "34.238.176.119",
+        "usage": 0
+    },
+    {
+        "name": "Australia",
+        "ip": "13.236.87.250",
+        "usage": 0
+    }
+];
