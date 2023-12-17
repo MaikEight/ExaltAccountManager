0024bee7fb6c4691d9fd46e4cf75794bf488f736
MaikEight <mail@maik8.de>
2023-12-17 21:14:33 +0100

    💡Removed the gray color as it is hard to read text on

--- src/hooks/useColorList.js
+++ src/hooks/useColorList.js
@@ -7,7 +7,7 @@ const useColorList = () => {
     const colorList = useMemo(
         () => [
             { background: '#9155fd1f', color: theme.palette.primary.main },
-            { background: '#8a8d931f', color: '#8a8d931f' },
+            // { background: '#8a8d931f', color: '#8a8d931f' },
             { background: '#16b1ff1f', color: theme.palette.info.main },
             { background: '#56ca0021', color: theme.palette.success.main },
             { background: '#ff4c511f', color: theme.palette.error.main },
