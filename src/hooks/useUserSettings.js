eadd4d64202a8a8c74c6aef673525c0062af7252
MaikEight <mail@maik8.de>
2023-12-19 02:23:21 +0100

    ✨Add useUserSettings hook

---
+++ src/hooks/useUserSettings.js
@@ -0,0 +1,8 @@
+
+import { useContext } from 'react';
+import UserSettingsContext from './../contexts/UserSettingsContext';
+function useUserSettings() {
+    return useContext(UserSettingsContext);
+}
+
+export default useUserSettings;
