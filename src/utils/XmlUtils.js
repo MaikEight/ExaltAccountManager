30b2d7bc6b60d281eb5d04421870b488350e9f2c
MaikEight <mail@maik8.de>
2023-12-20 08:24:52 +0100

    🎨Refactor XML to JSON conversion in XmlUtils.js

--- src/utils/XmlUtils.js
+++ src/utils/XmlUtils.js
@@ -1,57 +1,35 @@
+import convert from 'xml-js';
 
 // Function to convert XML Document to JavaScript object
 function xmlToJson(xml) {
-    let obj = {};
-    if (xml.nodeType == 1) {                
-        if (xml.attributes.length > 0) {
-            obj["@attributes"] = {};
-            for (let j = 0; j < xml.attributes.length; j++) {
-                let attribute = xml.attributes.item(j);
-                obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
-            }
-        }
-    } else if (xml.nodeType == 3) { 
-        obj = xml.nodeValue;
-    }            
+    if (!xml) return null;
 
-    if (xml.hasChildNodes()) {
-        for(let i = 0; i < xml.childNodes.length; i++) {
-            let item = xml.childNodes.item(i);
-            let nodeName = item.nodeName;
-            if (typeof(obj[nodeName]) == "undefined") {
-                obj[nodeName] = xmlToJson(item);
-            } else {
-                if (typeof(obj[nodeName].push) == "undefined") {
-                    let old = obj[nodeName];
-                    obj[nodeName] = [];
-                    obj[nodeName].push(old);
-                }
-                obj[nodeName].push(xmlToJson(item));
-            }
-        }
-    }
-
-    if (typeof obj === 'object' && Object.keys(obj).length === 1 && '#text' in obj) {
-        obj = obj['#text'];
-    }
-
-    return cleanObject(obj);
+    const convertedObject = convert.xml2js(xml, { compact: true, ignoreAttributes: true, ignoreDeclaration: true })
+    console.log('convertedObject', convertedObject);
+    return cleanUp(convertedObject);
 };
 
-function cleanObject(obj) {
-    let cleanObj = Array.isArray(obj) ? [] : {};
+function cleanUp(convertedObject) {
+    if (!convertedObject) return null;
 
-    for (let key in obj) {
-        if (key !== '@attributes' && key !== '#text') {
-            if (typeof obj[key] === 'object' && obj[key] !== null) {
-                cleanObj[key] = cleanObject(obj[key]);
+    if (Array.isArray(convertedObject)) {
+        return convertedObject.map(item => item["_text"]);
+    }
+
+    let cleanedObject = {};
+
+    for (let key in convertedObject) {
+        if (convertedObject.hasOwnProperty(key)) {
+            let value = convertedObject[key];
+            if (typeof value === 'object' && value !== null && "_text" in value) {
+                cleanedObject[key] = value["_text"];
             } else {
-                cleanObj[key] = obj[key];
+                cleanedObject[key] = cleanUp(value);
             }
         }
     }
 
-    return cleanObj;
+    return cleanedObject;
 }
 
 export { xmlToJson };
