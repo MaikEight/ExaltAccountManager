846da5a464e114e3ecb34357b02057a6c3e6ac49
MaikEight <mail@maik8.de>
2023-12-21 07:53:24 +0100

    ✨Refactor writeFileUtil.js to support writing binary files
    This commit refactors the `writeFileUtil.js` file to support writing binary files. It introduces a new `writeFile` function that accepts binary content and uses the Tauri API to write the file. The existing `writeFileUTF8` function is modified to handle UTF-8 content. This change improves the flexibility and usability of the `writeFileUtil` module.

--- src/utils/writeFileUtil.js
+++ src/utils/writeFileUtil.js
@@ -9,6 +9,19 @@ export async function writeFileUTF8(filePath, content, stringifyAsJSON = false) 
             contents: stringContent,
         });
     } catch (error) {
+        console.error('Error writing file (UTF8):', error);
+    }
+}
+
+export async function writeFile(filePath, content) {
+    try {       
+        let uint8ArrayContent = new Uint8Array(content);
+
+        await window.__TAURI__.fs.writeBinaryFile({
+            path: filePath,
+            contents: uint8ArrayContent,
+        });
+    } catch (error) {
         console.error('Error writing file:', error);
     }
 }
