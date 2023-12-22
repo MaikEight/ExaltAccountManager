dde0fc84aa7ce1b3baa235f87061eadf5d5c3ba1
MaikEight <mail@maik8.de>
2023-12-22 06:53:54 +0100

    💄Refactor ComponentBox to include LinearProgress and innerSx

--- src/components/ComponentBox.jsx
+++ src/components/ComponentBox.jsx
@@ -1,34 +1,50 @@
 import { useTheme } from "@emotion/react";
-import { Box, Paper, Typography } from "@mui/material";
-function ComponentBox({ children, headline, icon, sx }) {
+import { Box, LinearProgress, Paper, Typography } from "@mui/material";
+function ComponentBox({ children, isLoading, headline, icon, sx, innerSx }) {
     const theme = useTheme();
 
     return (
-        <Paper sx={{
-            display: "flex",
-            flexDirection: "column",
-            flexGrow: 1,
-            borderRadius: 1.5,
-            paddingLeft: 1.5,
-            paddingRight: 1.5,
-            paddingTop: 1.5,
-            paddingBottom: 1.5,
-            m: 2,
-            background: theme.palette.background.paper,
-            ...sx
+        <Paper
+            sx={{
+                position: "relative",
+                display: "flex",
+                flexDirection: "column",
+                borderRadius: 1.5,
+                paddingLeft: 1.5,
+                paddingRight: 1.5,
+                paddingTop: 1.5,
+                paddingBottom: 1.5,
+                m: 2,
+                background: theme.palette.background.paper,
+                ...sx
+            }}
+        >
+            {
+                isLoading &&
 
-        }}>
+                <LinearProgress
+                    sx={{
+                        position: 'absolute',
+                        top: 0,
+                        left: 0,
+                        width: '100%',
+                        borderRadius: '6px 6px 0 0',
+                        zIndex: 9999
+                    }}
+                />
+            }
             {
                 (icon || headline) &&
-                <Box 
+                <Box
                     sx={{
                         display: "flex",
                         flexDirection: "row",
                         alignItems: "center",
                         justifyContent: "start",
-                        mb: 1,
+                        mb: 2,
+                        gap: 1,
                     }}
-                >                                        
+                >
                     {icon && icon}
                     {
                         headline &&
@@ -38,7 +54,7 @@ function ComponentBox({ children, headline, icon, sx }) {
                     }
                 </Box>
             }
-            <Box>
+            <Box sx={innerSx}>
                 {children}
             </Box>
         </Paper>
