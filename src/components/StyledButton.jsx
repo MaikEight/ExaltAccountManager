35336ea5a782b67e0a5704d84f54da18a328dba4
MaikEight <mail@maik8.de>
2023-12-22 06:54:01 +0100

    💄Refactor StyledButton component

--- src/components/StyledButton.jsx
+++ src/components/StyledButton.jsx
@@ -10,9 +10,9 @@ function StyledButton({ children, fullWidth, ...props }) {
             fullWidth={fullWidth}
             {...props}
             sx={{
-                ...props.sx,
                 borderRadius: 1.5,                
                 boxShadow: theme.palette.mode === 'dark' ? 'rgba(19, 17, 32, 0.42) 0px 4px 8px -4px;' : 'rgba(58, 53, 65, 0.42) 0px 4px 8px -4px;',
+                ...props.sx,
             }}
         >
             {children}
