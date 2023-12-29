20224c049bb48b63c2d04e962dfac646ab3510c0
MaikEight <mail@maik8.de>
2023-12-29 22:11:05 +0100

    ✨Add copy functionality to PaddedTableCell component

--- src/components/AccountDetails/PaddedTableCell.jsx
+++ src/components/AccountDetails/PaddedTableCell.jsx
@@ -1,9 +1,27 @@
-import { TableCell } from "@mui/material";
+import { IconButton, TableCell } from "@mui/material";
+import ContentCopyOutlinedIcon from '@mui/icons-material/ContentCopyOutlined';
 
-function PaddedTableCell({ children, sx, ...props }) {
+function PaddedTableCell({ children, sx, isEditMode, allowCopy, ...props }) {
     return (
         <TableCell {...props} sx={{ pl: 4, pr: 4, pt: 1.5, pb: 1.5, ...sx }}>
             {children}
+            {
+                allowCopy && !isEditMode ?
+                    <IconButton
+                        size="small"
+                        sx={{
+                           ml: 0.5
+                        }}
+                        onClick={(e) => {
+                            e.stopPropagation();
+                            console.log(children);
+                            navigator.clipboard.writeText(children.props.children);
+                        }}
+                    >
+                        <ContentCopyOutlinedIcon fontSize="small" />
+                    </IconButton>
+                    : null
+            }
         </TableCell>
     );
 }
