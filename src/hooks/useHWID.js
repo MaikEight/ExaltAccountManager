d0ff0a8dc5d08568336867f0bb4ece1004555b1a
MaikEight <mail@maik8.de>
2023-12-29 22:08:36 +0100

    ✨Add useHWID hook for retrieving and managing hardware identifier

---
+++ src/hooks/useHWID.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+import { HWID_FILE_PATH } from "../constants";
+import { readFileUTF8 } from "../utils/readFileUtil";
+import { tauri } from "@tauri-apps/api";
+
+function useHWID() {
+    const [hwid, setHwid] = useState(null);
+
+    useEffect(() => {
+        console.log('hwid changed:', hwid);
+    }, [hwid]);
+
+    useEffect(() => {
+        const readHwidFile = async () => {
+            const path = await HWID_FILE_PATH();
+            const hwid = await readFileUTF8(path, false);
+            if (hwid === null || hwid === undefined || hwid === '') {
+                tauri.invoke('get_device_unique_identifier')
+                    .then((id) => setHwid(id));
+                return;
+            }
+            setHwid(hwid);
+        }
+        readHwidFile().catch(error => console.error(error));
+    }, []);
+
+    return hwid;
+}
+
+export default useHWID;
