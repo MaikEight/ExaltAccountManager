103da2384bac6e7a977235a687383ea7fbb3145f
MaikEight <mail@maik8.de>
2023-12-29 22:09:07 +0100

    🎨Add file existence check in readFileUTF8 function

--- src/utils/readFileUtil.js
+++ src/utils/readFileUtil.js
@@ -1,11 +1,17 @@
 
 async function readFileUTF8(filePath, parseAsJSON = false) {
-    try {
-        const bytes = await window.__TAURI__.fs.readBinaryFile(filePath);
-        const stringContent = new TextDecoder().decode(bytes);
-        return parseAsJSON ? JSON.parse(stringContent) : stringContent;
+    try {        
+        if (window.__TAURI__.fs.exists(filePath)) {
+            const bytes = await window.__TAURI__.fs.readBinaryFile(filePath);
+            const stringContent = new TextDecoder().decode(bytes);
+            return parseAsJSON ? JSON.parse(stringContent) : stringContent;
+        }
+        console.log("File does not exist:", filePath);
+        return null;
     } catch (error) {
-        console.error('Error reading file:', error);
+        if(error.includes('os error 2')) return;
+
+        console.error('Error reading file:',filePath, error);
     }
 };
 
