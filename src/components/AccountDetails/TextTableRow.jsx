2837aa00b0c69a91d4b237e33e89969edf82c1d3
MaikEight <mail@maik8.de>
2024-01-02 00:49:42 +0100

    🎨Update TextTableRow props

--- src/components/AccountDetails/TextTableRow.jsx
+++ src/components/AccountDetails/TextTableRow.jsx
@@ -1,7 +1,7 @@
 import { TableRow, Typography } from "@mui/material";
 import PaddedTableCell from "./PaddedTableCell";
 
-function TextTableRow({ keyValue, value, innerSx, editMode, allowCopy, ...rest }) {
+function TextTableRow({ keyValue, value, innerSx, editMode, isPassword, onChange, allowCopy, ...rest }) {
   return (
     <TableRow {...rest}>
       <PaddedTableCell sx={innerSx}>
@@ -9,7 +9,7 @@ function TextTableRow({ keyValue, value, innerSx, editMode, allowCopy, ...rest }
           {keyValue}
         </Typography>
       </PaddedTableCell>
-      <PaddedTableCell sx={innerSx} isEditMode={editMode} allowCopy={allowCopy}>
+      <PaddedTableCell sx={innerSx} isEditMode={editMode} isPassword={isPassword} onChange={onChange} allowCopy={allowCopy} align="left">
         <Typography variant="body2" fontWeight={300} component="span" sx={{ textAlign: 'center'}}>
           {value}
         </Typography>
