619057b826d233e92530a858d0bcd05711d84ed9
MaikEight <mail@maik8.de>
2024-01-05 00:30:02 +0100

    💄Refactor CustomToolbar component to add "Add New" button

--- src/components/GridComponents/CustomToolbar.jsx
+++ src/components/GridComponents/CustomToolbar.jsx
@@ -1,18 +1,18 @@
 import { useTheme } from "@emotion/react";
-import { Box } from "@mui/material";
-import { GridToolbarColumnsButton, GridToolbarContainer, GridToolbarDensitySelector, GridToolbarExport, GridToolbarFilterButton } from "@mui/x-data-grid";
+import { Box, Button} from "@mui/material";
+import { GridToolbarColumnsButton, GridToolbarContainer, GridToolbarFilterButton } from "@mui/x-data-grid";
 import Searchbar from "./Searchbar";
+import AddIcon from '@mui/icons-material/Add';
 
-
-function CustomToolbar({onSearchChanged}) {
+function CustomToolbar({ onSearchChanged, onAddNew }) {
     const theme = useTheme();
 
     return (
-        <Box style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+        <Box style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
             <GridToolbarContainer
                 sx={{
                     display: "flex",
-                    justifyContent: "start",                    
+                    justifyContent: "start",
                     height: 49,
                     backgroundColor: theme.palette.background.paper,
                     borderRadius: '6px 6px 0 0',
@@ -23,13 +23,41 @@ function CustomToolbar({onSearchChanged}) {
                     pr: 1,
                 }}
             >
+
                 <GridToolbarColumnsButton />
                 <GridToolbarFilterButton />
                 {/* <GridToolbarDensitySelector /> */}
                 {/* <GridToolbarExport /> */}
+
             </GridToolbarContainer>
-            <Box sx={{ mr: 0.5 }} >
-                <Searchbar onSearchChanged={onSearchChanged} />
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                }}
+            >
+                <Button
+                    variant="text"
+                    color="primary"
+                    onClick={onAddNew}
+                    size="small"
+                    sx={{
+                        mt: 0.5,
+                        mb: 0.5,
+                    }}
+                    startIcon={<AddIcon />}
+                >
+                    ADD New
+                </Button>
+                <Box
+                    sx={{
+                        mr: 0.5,
+                        display: 'flex',
+                        flexDirection: 'row',
+                    }}
+                >
+                    <Searchbar onSearchChanged={onSearchChanged} />
+                </Box>
             </Box>
         </Box>
     );
