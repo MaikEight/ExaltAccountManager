6853632379714c1cacecdb80db33f7102dfbfe98
MaikEight <mail@maik8.de>
2024-01-08 00:53:00 +0100

    ✨Add ServerContext and ServerContextProvider components

---
+++ src/contexts/ServerContext.jsx
@@ -0,0 +1,40 @@
+import { createContext, useEffect, useState } from "react";
+import { SERVER_LIST_FILE_PATH } from "../constants";
+import { readFileUTF8 } from "../utils/readFileUtil";
+import { writeFileUTF8 } from "../utils/writeFileUtil";
+
+const ServerContext = createContext();
+
+function ServerContextProvider({ children }) {
+    const [serverList, setServerList] = useState([]);
+
+    const getServers = async () => {
+        SERVER_LIST_FILE_PATH()
+            .then((path) => {
+                readFileUTF8(path, true)
+                    .then((data) => {
+                        setServerList(data);
+                    });
+            });
+    };
+
+    useEffect(() => {
+        getServers();
+    }, []);
+
+    const saveServerList = (serverList) => {
+        setServerList(serverList);
+        SERVER_LIST_FILE_PATH().then((path) => {
+            writeFileUTF8(path, JSON.stringify(serverList));
+        });
+    };
+
+    return (
+        <ServerContext.Provider value={{ serverList, saveServerList }}>
+            {children}
+        </ServerContext.Provider>
+    );
+}
+
+export default ServerContext;
+export { ServerContextProvider };
