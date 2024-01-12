5077c2a46b22228f83ce7c4c8baf11a5c51f32c1
MaikEight <mail@maik8.de>
2024-01-12 01:50:29 +0100

    ✨Add onStartUp function call in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import { useEffect } from "react";
 import { ColorContextProvider } from "./contexts/ColorContext";
 import MainRouter from "./MainRouter";
 import { appWindow, PhysicalSize } from '@tauri-apps/api/window';
+import { onStartUp } from "./utils/startUpUtils";
 
 function App() {
   
   useEffect(() => {
     appWindow.setMinSize(new PhysicalSize(850, 600));
-  }, []);
+
+    onStartUp();    
+  }, []);  
 
   return (
     <ColorContextProvider>
