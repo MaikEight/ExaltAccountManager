106c841e7176389fbd32b4470842e9400e2fa602
MaikEight <mail@maik8.de>
2024-01-13 03:17:42 +0100

    🎨Fix formatting in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
 import { useEffect } from "react";
 import { ColorContextProvider } from "./contexts/ColorContext";
 import MainRouter from "./MainRouter";
-import { appWindow, PhysicalSize } from '@tauri-apps/api/window';
 import { onStartUp } from "./utils/startUpUtils";
 
 function App() {
   
   useEffect(() => {
-    appWindow.setMinSize(new PhysicalSize(850, 600));
-
     onStartUp();    
   }, []);  
 
   return (
     <ColorContextProvider>
-      <MainRouter />        
+      <MainRouter />
     </ColorContextProvider>
   );
 }
