a902f6e9668298dfbc5feee3c251e1a75c31b830
MaikEight <mail@maik8.de>
2024-01-27 02:52:46 +0100

    ✨ Update MainRouter component with SnackbarProvider and styling

--- src/MainRouter.jsx
+++ src/MainRouter.jsx
@@ -1,7 +1,7 @@
 import { CssBaseline } from "@mui/material";
-import { ThemeProvider as MuiThemeProvider } from "@mui/material/styles";
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import { ThemeProvider as StyledThemeProvider } from "styled-components";
+import { ThemeProvider as MuiThemeProvider, alpha, darken } from "@mui/material/styles";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import styled, { ThemeProvider as StyledThemeProvider } from "styled-components";
 import Sidebar from "./components/Sidebar/Sidebar";
 import ColorContext from "./contexts/ColorContext";
 import { useContext } from "react";
@@ -11,6 +11,40 @@ import { GroupsContextProvider } from "./contexts/GroupsContext";
 import SettingsPage from "./pages/SettingsPage";
 import { ServerContextProvider } from "./contexts/ServerContext";
 import AboutPage from "./pages/AboutPage";
+import { AccountsContextProvider } from "./contexts/AccountsContext";
+import { MaterialDesignContent, SnackbarProvider, useSnackbar } from "notistack";
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+
+const getSnackbarStyles = (theme) => ({
+    '&.notistack-MuiContent-default': {
+        backgroundColor: theme.palette.secondary.full,
+    },
+    '&.notistack-MuiContent-success': {
+        backgroundColor: theme.palette.primary.main,
+    },
+    '&.notistack-MuiContent-error': {
+        backgroundColor: (theme.palette.mode === 'dark' ? theme.palette.error.dark : theme.palette.error.main),
+    },
+});
+
+const StyledMaterialDesignContent = styled(MaterialDesignContent)(({ theme }) => getSnackbarStyles(theme));
+
+const CloseAction = (key) => {
+    const { closeSnackbar } = useSnackbar();
+
+    return (
+        <IconButton
+            size="small"
+            aria-label="close"
+            color="inherit"
+            onClick={() => closeSnackbar(key)}
+        >
+            <CloseIcon fontSize="small" />
+        </IconButton>
+    );
+};
 
 function MainRouter() {
     const colorContext = useContext(ColorContext);
@@ -20,26 +54,40 @@ function MainRouter() {
         <StyledThemeProvider theme={theme}>
             <MuiThemeProvider theme={theme}>
                 <CssBaseline enableColorScheme />
-                <div style={{ width: '100%' }}>
-                    <Router id="router">
-                        <Sidebar id="sidebar">
-                            <GroupsContextProvider>
-                                <ServerContextProvider>
-                                    <Routes>
-                                        <Route path='/' element={<AccountsPage />}></Route>
-                                        <Route path='/accounts' element={<AccountsPage />}></Route>
-                                        <Route path='/gameUpdater' element={<GameUpdaterPage />}></Route>
-                                        <Route path='/settings' element={<SettingsPage />}></Route>
-                                        <Route path='/about' element={<AboutPage />}></Route>
-                                        <Route path='*' element={<AccountsPage />}></Route>
-                                    </Routes>
-                                </ServerContextProvider>
-                            </GroupsContextProvider>
-                        </Sidebar>
-                    </Router>
-                </div>
+                <SnackbarProvider
+                    iconVariant={{
+                        default: <InfoOutlinedIcon size='small' sx={{ width: '20px', height: '20px', mr: 1 }} />,
+                    }}
+                    Components={{
+                        default: StyledMaterialDesignContent,
+                        success: StyledMaterialDesignContent,
+                        error: StyledMaterialDesignContent,
+                    }}
+                    action={CloseAction}
+                >
+                    <div style={{ width: '100%' }}>
+                        <Router id="router">
+                            <Sidebar id="sidebar">
+                                <AccountsContextProvider>
+                                    <GroupsContextProvider>
+                                        <ServerContextProvider>
+                                            <Routes>
+                                                <Route path='/' element={<AccountsPage />}></Route>
+                                                <Route path='/accounts' element={<AccountsPage />}></Route>
+                                                <Route path='/gameUpdater' element={<GameUpdaterPage />}></Route>
+                                                <Route path='/settings' element={<SettingsPage />}></Route>
+                                                <Route path='/about' element={<AboutPage />}></Route>
+                                                <Route path='*' element={<AccountsPage />}></Route>
+                                            </Routes>
+                                        </ServerContextProvider>
+                                    </GroupsContextProvider>
+                                </AccountsContextProvider>
+                            </Sidebar>
+                        </Router>
+                    </div>
+                </SnackbarProvider>
             </MuiThemeProvider>
-        </StyledThemeProvider>
+        </StyledThemeProvider >
     );
 }
 
