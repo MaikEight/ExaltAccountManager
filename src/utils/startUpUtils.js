57d7217c7878126b88dbfaec2bc693843463596b
MaikEight <mail@maik8.de>
2024-01-27 02:47:59 +0100

    ✨ Add startSession and setApiHwidHash functions to startUpUtils.js

--- src/utils/startUpUtils.js
+++ src/utils/startUpUtils.js
@@ -1,14 +1,15 @@
-import { getLatestEamVersion } from "../backend/eamApi";
+import { getLatestEamVersion, startSession } from "../backend/eamApi";
 import { isUpdateAvailable } from "../constants";
 import { appWindow, PhysicalSize } from '@tauri-apps/api/window';
+import { getAPIClientIdHash } from "./testUtils";
+import { invoke } from '@tauri-apps/api/tauri';
 
-async function onStartUp() {    
+async function onStartUp() {
     appWindow.setMinSize(new PhysicalSize(850, 600));
 
     getLatestEamVersion()
         .then((version) => {
             if (isUpdateAvailable(version)) {
-                console.log("Update available!");
                 localStorage.setItem("EAMUpdateAvailable", true);
             }
             else if (localStorage.getItem("EAMUpdateAvailable")) {
@@ -17,4 +18,27 @@ async function onStartUp() {
         });
 }
 
-export { onStartUp };
+function setApiHwidHash(hwid) {
+
+    if (hwid === null || hwid === undefined) {
+        console.log("HWID is null or undefined, not setting apiHwidHash");
+        return;
+    }
+
+    const calculcateApiHwidHash = async () => {
+        const hash = await getAPIClientIdHash();
+        while (hwid === null || hwid === undefined) {
+            await new Promise(resolve => setTimeout(resolve, 100));
+        }
+        const compSecret = hwid + hash;
+        const hwidHash = await invoke('quick_hash', { secret: compSecret });
+
+        const stored = localStorage.getItem("apiHwidHash");
+        if (hwidHash !== null && (stored === null || stored !== hwidHash)) {
+            localStorage.setItem("apiHwidHash", hwidHash);
+        }
+    }
+    calculcateApiHwidHash();
+}
+
+export { onStartUp, setApiHwidHash };
