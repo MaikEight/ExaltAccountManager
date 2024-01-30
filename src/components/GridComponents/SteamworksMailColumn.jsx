a207d8cb3668cbc5a40fd66f0b229844fa839d9f
MaikEight <mail@maik8.de>
2024-01-30 02:46:28 +0100

    ✨ Add SteamworksMailColumn component

---
+++ src/components/GridComponents/SteamworksMailColumn.jsx
@@ -0,0 +1,25 @@
+import { Box, Typography } from "@mui/material";
+
+function SteamworksMailColumn(params) {
+    console.log(params);
+    const id = params.params.value && params.params.value.startsWith('steamworks:') ? params.params.value.split(':')[1] : "-";
+    console.log('id', id);
+
+    return (
+        <Box
+            sx={{
+                display: 'flex',
+                alignItems: 'center',
+                textAlign: 'center',
+                height: '100%',
+            }}
+        >
+            <img src="/steam.svg" alt="Steam Logo" height='20px' width='20px' style={{ marginRight: '6px' }} />
+            <Typography variant="body1" sx={{ fontSize: '0.875rem', fontFamily: 'Roboto', }}>
+                {id}
+            </Typography>
+        </Box>
+    );
+}
+
+export default SteamworksMailColumn;
