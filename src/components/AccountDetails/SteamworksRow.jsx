70568a25d7d89121f61d373f655c425ee42354d4
MaikEight <mail@maik8.de>
2024-02-03 05:55:04 +0100

    ✨ Add SteamworksRow component for displaying Steam ID and linking to Steam profile

---
+++ src/components/AccountDetails/SteamworksRow.jsx
@@ -0,0 +1,36 @@
+import { TableRow, Tooltip, Typography } from "@mui/material";
+import PaddedTableCell from "./PaddedTableCell";
+import OpenInNewOutlinedIcon from '@mui/icons-material/OpenInNewOutlined';
+
+function SteamworksRow({ guid, innerSx, ...rest }) {
+    const steamId = guid.split(':')[1];
+
+    return (
+        <TableRow {...rest}>
+            <PaddedTableCell sx={innerSx}>
+
+                <Typography variant="body2" fontWeight={300} component="span">
+                    Steam ID
+                </Typography>
+            </PaddedTableCell>
+            <PaddedTableCell sx={innerSx} align="left">
+                <Tooltip title="Open Steam profile">
+                    <a
+                        href={`https://steamcommunity.com/profiles/${steamId}`}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        style={{ display: 'flex', alignItems: 'center', textDecoration: 'none' }}
+                    >
+                        <img src="/steam.svg" alt="Steam Logo" height='20px' width='20px' style={{ marginRight: '6px' }} />
+                        <Typography variant="body2" fontWeight={300} component="span" sx={{ textAlign: 'center', mr: 0.5 }}>
+                            {steamId}
+                        </Typography>
+                        <OpenInNewOutlinedIcon />
+                    </a>
+                </Tooltip>
+            </PaddedTableCell>
+        </TableRow>
+    );
+}
+
+export default SteamworksRow;
