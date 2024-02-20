dca437247d4a180b4c4a26b7b1dc24f7b42ebcf3
MaikEight <mail@maik8.de>
2024-02-20 01:46:40 +0100

    ✨ Added new models to the models.rs

--- src-tauri/src/models.rs
+++ src-tauri/src/models.rs
@@ -1,3 +1,5 @@
+#![allow(non_snake_case)]
+
 use crate::schema;
 use diesel::insertable::Insertable;
 use diesel::prelude::*;
@@ -121,6 +123,60 @@ pub struct UpdateAccount {
     pub stats_id: Option<i32>,
 }
 
+impl From<Account> for NewAccount {
+    fn from(account: Account) -> Self {
+        NewAccount {
+            dataset_id: account.dataset_id,
+            credits: account.credits,
+            fortune_token: account.fortune_token,
+            unity_campaign_points: account.unity_campaign_points,
+            early_game_event_tracker: account.early_game_event_tracker,
+            account_id: account.account_id,
+            creation_timestamp: account.creation_timestamp,
+            enchanter_support_dust: account.enchanter_support_dust,
+            max_num_chars: account.max_num_chars,
+            last_server: account.last_server,
+            originating: account.originating,
+            pet_yard_type: account.pet_yard_type,
+            forge_fire_energy: account.forge_fire_energy,
+            name: account.name,
+            guild: account.guild,
+            guildrank: account.guildrank,
+            access_token_timestamp: account.access_token_timestamp,
+            access_token_expiration: account.access_token_expiration,
+            owned_skins: account.owned_skins,
+            stats_id: account.stats_id,
+        }
+    }
+}
+
+impl From<Account> for UpdateAccount {
+    fn from(account: Account) -> Self {
+        UpdateAccount {
+            dataset_id: account.dataset_id,
+            credits: account.credits,
+            fortune_token: account.fortune_token,
+            unity_campaign_points: account.unity_campaign_points,
+            early_game_event_tracker: account.early_game_event_tracker,
+            account_id: account.account_id,
+            creation_timestamp: account.creation_timestamp,
+            enchanter_support_dust: account.enchanter_support_dust,
+            max_num_chars: account.max_num_chars,
+            last_server: account.last_server,
+            originating: account.originating,
+            pet_yard_type: account.pet_yard_type,
+            forge_fire_energy: account.forge_fire_energy,
+            name: account.name,
+            guild: account.guild,
+            guildrank: account.guildrank,
+            access_token_timestamp: account.access_token_timestamp,
+            access_token_expiration: account.access_token_expiration,
+            owned_skins: account.owned_skins,
+            stats_id: account.stats_id,
+        }
+    }
+}
+
 // ############################
 // #      AccountStorage      #
 // ############################
@@ -147,6 +203,26 @@ pub struct UpdateAccountStorage {
     pub dataset_id: Option<String>,
     pub item_id: Option<i32>,
     pub storage_type: Option<String>,
+}
+
+impl From<AccountStorage> for NewAccountStorage {
+    fn from(account_storage: AccountStorage) -> Self {
+        NewAccountStorage {
+            dataset_id: account_storage.dataset_id,
+            item_id: account_storage.item_id,
+            storage_type: account_storage.storage_type,
+        }
+    }
+}
+
+impl From<AccountStorage> for UpdateAccountStorage {
+    fn from(account_storage: AccountStorage) -> Self {
+        UpdateAccountStorage {
+            dataset_id: account_storage.dataset_id,
+            item_id: account_storage.item_id,
+            storage_type: account_storage.storage_type,
+        }
+    }
 }
 
 // ############################
@@ -281,6 +357,74 @@ pub struct UpdateCharacter {
     pub dataset_id: Option<String>,
 }
 
+impl From<Character> for NewCharacter {
+    fn from(character: Character) -> Self {
+        NewCharacter {
+            object_type: character.object_type,
+            seasonal: character.seasonal,
+            level: character.level,
+            exp: character.exp,
+            current_fame: character.current_fame,
+            equip_qs: character.equip_qs,
+            max_hit_points: character.max_hit_points,
+            hit_points: character.hit_points,
+            max_magic_points: character.max_magic_points,
+            magic_points: character.magic_points,
+            attack: character.attack,
+            defense: character.defense,
+            speed: character.speed,
+            dexterity: character.dexterity,
+            hp_regen: character.hp_regen,
+            mp_regen: character.mp_regen,
+            pc_stats: character.pc_stats,
+            health_stack_count: character.health_stack_count,
+            magic_stack_count: character.magic_stack_count,
+            dead: character.dead,
+            pet_id: character.pet_id,
+            account_name: character.account_name,
+            texture: character.texture,
+            backpack_slots: character.backpack_slots,
+            has_3_quickslots: character.has_3_quickslots,
+            creation_date: character.creation_date,
+            dataset_id: character.dataset_id,
+        }
+    }
+}
+
+impl From<Character> for UpdateCharacter {
+    fn from(character: Character) -> Self {
+        UpdateCharacter {
+            object_type: character.object_type,
+            seasonal: character.seasonal,
+            level: character.level,
+            exp: character.exp,
+            current_fame: character.current_fame,
+            equip_qs: character.equip_qs,
+            max_hit_points: character.max_hit_points,
+            hit_points: character.hit_points,
+            max_magic_points: character.max_magic_points,
+            magic_points: character.magic_points,
+            attack: character.attack,
+            defense: character.defense,
+            speed: character.speed,
+            dexterity: character.dexterity,
+            hp_regen: character.hp_regen,
+            mp_regen: character.mp_regen,
+            pc_stats: character.pc_stats,
+            health_stack_count: character.health_stack_count,
+            magic_stack_count: character.magic_stack_count,
+            dead: character.dead,
+            pet_id: character.pet_id,
+            account_name: character.account_name,
+            texture: character.texture,
+            backpack_slots: character.backpack_slots,
+            has_3_quickslots: character.has_3_quickslots,
+            creation_date: character.creation_date,
+            dataset_id: character.dataset_id,
+        }
+    }
+}
+
 // ############################
 // #        ClassStats        #
 // ############################
@@ -320,6 +464,28 @@ pub struct UpdateClassStats {
     pub best_total_fame: Option<i32>,
 }
 
+impl From<ClassStats> for NewClassStats {
+    fn from(class_stats: ClassStats) -> Self {
+        NewClassStats {
+            object_type: class_stats.object_type,
+            best_level: class_stats.best_level,
+            best_base_fame: class_stats.best_base_fame,
+            best_total_fame: class_stats.best_total_fame,
+        }
+    }
+}
+
+impl From<ClassStats> for UpdateClassStats {
+    fn from(class_stats: ClassStats) -> Self {
+        UpdateClassStats {
+            object_type: class_stats.object_type,
+            best_level: class_stats.best_level,
+            best_base_fame: class_stats.best_base_fame,
+            best_total_fame: class_stats.best_total_fame,
+        }
+    }
+}
+
 // ############################
 // #        EamAccount        #
 // ############################
@@ -329,12 +495,18 @@ pub struct EamAccount {
     pub id: Option<i32>,
     pub name: Option<String>,
     pub email: String,
+    pub isSteam: bool,
+    pub steamId: Option<String>,
     pub password: String,
+    #[serde(rename = "serverName")]
     pub server_name: Option<String>,
+    #[serde(rename = "performDailyLogin")]
     pub perform_daily_login: bool,
     pub state: Option<String>,
+    #[serde(rename = "lastLogin")]
     pub last_login: Option<String>,
     pub group: Option<String>,
+    pub token: Option<String>,
     pub extra: Option<String>,
 }
 
@@ -344,6 +516,8 @@ pub struct NewEamAccount {
     pub id: Option<i32>,
     pub name: Option<String>,
     pub email: String,
+    pub isSteam: bool,
+    pub steamId: Option<String>,
     pub password: String,
     #[diesel(column_name = "serverName")]
     pub server_name: Option<String>,
@@ -353,14 +527,18 @@ pub struct NewEamAccount {
     #[diesel(column_name = "lastLogin")]
     pub last_login: Option<String>,
     pub group: Option<String>,
+    pub token: Option<String>,
     pub extra: Option<String>,
 }
 
 #[derive(AsChangeset, Serialize)]
 #[diesel(table_name = schema::EamAccount)]
 pub struct UpdateEamAccount {
+    pub id: Option<i32>,
     pub name: Option<String>,
     pub email: String,
+    pub isSteam: bool,
+    pub steamId: Option<String>,
     pub password: String,
     #[diesel(column_name = "serverName")]
     pub server_name: Option<String>,
@@ -370,6 +548,7 @@ pub struct UpdateEamAccount {
     #[diesel(column_name = "lastLogin")]
     pub last_login: Option<String>,
     pub group: Option<String>,
+    pub token: Option<String>,
     pub extra: Option<String>,
 }
 
@@ -379,12 +558,15 @@ impl From<EamAccount> for NewEamAccount {
             id: account.id,
             name: account.name,
             email: account.email,
+            isSteam: account.isSteam,
+            steamId: account.steamId,
             password: account.password,
             server_name: account.server_name,
             perform_daily_login: account.perform_daily_login,
             state: account.state,
             last_login: account.last_login,
             group: account.group,
+            token: account.token,
             extra: account.extra,
         }
     }
@@ -393,15 +575,82 @@ impl From<EamAccount> for NewEamAccount {
 impl From<EamAccount> for UpdateEamAccount {
     fn from(account: EamAccount) -> Self {
         UpdateEamAccount {
+            id: account.id,
             name: account.name,
             email: account.email,
+            isSteam: account.isSteam,
+            steamId: account.steamId,
             password: account.password,
             server_name: account.server_name,
             perform_daily_login: account.perform_daily_login,
             state: account.state,
             last_login: account.last_login,
             group: account.group,
+            token: account.token,
             extra: account.extra,
+        }
+    }
+}
+
+// ############################
+// #         EamGroup         #
+// ############################
+
+#[derive(Queryable, Serialize, Deserialize, Clone)]
+pub struct EamGroup {
+    pub id: i32,
+    pub name: Option<String>,
+    pub color: Option<i32>,
+    #[serde(rename = "iconType")]
+    pub icon_type: Option<String>,
+    pub icon: Option<String>,
+    pub padding: Option<String>,
+}
+
+#[derive(Insertable, Serialize)]
+#[diesel(table_name = schema::EamGroup)]
+pub struct NewEamGroup {
+    pub name: Option<String>,
+    pub color: Option<i32>,
+    #[diesel(column_name = "iconType")]
+    pub icon_type: Option<String>,
+    pub icon: Option<String>,
+    pub padding: Option<String>,
+}
+
+#[derive(AsChangeset, Serialize)]
+#[diesel(table_name = schema::EamGroup)]
+pub struct UpdateEamGroup {    
+    pub id: Option<i32>,
+    pub name: Option<String>,
+    pub color: Option<i32>,
+    #[diesel(column_name = "iconType")]
+    pub icon_type: Option<String>,
+    pub icon: Option<String>,
+    pub padding: Option<String>,
+}
+
+impl From<EamGroup> for NewEamGroup {
+    fn from(group: EamGroup) -> Self {
+        NewEamGroup {
+            name: group.name,
+            color: group.color,
+            icon_type: group.icon_type,
+            icon: group.icon,
+            padding: group.padding,
+        }
+    }
+}
+
+impl From<EamGroup> for UpdateEamGroup {
+    fn from(group: EamGroup) -> Self {
+        UpdateEamGroup {
+            id: Some(group.id),
+            name: group.name,
+            color: group.color,
+            icon_type: group.icon_type,
+            icon: group.icon,
+            padding: group.padding,
         }
     }
 }
@@ -437,6 +686,28 @@ pub struct UpdateEquipment {
     pub slot: Option<i32>,
 }
 
+impl From<Equipment> for NewEquipment {
+    fn from(equipment: Equipment) -> Self {
+        NewEquipment {
+            character_id: equipment.character_id,
+            dataset_id: equipment.dataset_id,
+            item_type: equipment.item_type,
+            slot: equipment.slot,
+        }
+    }
+}
+
+impl From<Equipment> for UpdateEquipment {
+    fn from(equipment: Equipment) -> Self {
+        UpdateEquipment {
+            character_id: equipment.character_id,
+            dataset_id: equipment.dataset_id,
+            item_type: equipment.item_type,
+            slot: equipment.slot,
+        }
+    }
+}
+
 // ############################
 // #         ItemData         #
 // ############################
@@ -457,6 +728,22 @@ pub struct NewItemData {
 #[diesel(table_name = schema::ItemData)]
 pub struct UpdateItemData {
     pub type_: Option<i32>,
+}
+
+impl From<ItemData> for NewItemData {
+    fn from(item_data: ItemData) -> Self {
+        NewItemData {
+            type_: item_data.type_,
+        }
+    }
+}
+
+impl From<ItemData> for UpdateItemData {
+    fn from(item_data: ItemData) -> Self {
+        UpdateItemData {
+            type_: item_data.type_,
+        }
+    }
 }
 
 // ############################
@@ -527,6 +814,46 @@ pub struct UpdatePet {
     pub ability3: Option<String>,
 }
 
+impl From<Pet> for NewPet {
+    fn from(pet: Pet) -> Self {
+        NewPet {
+            name: pet.name,
+            type_: pet.type_,
+            instance_id: pet.instance_id,
+            rarity: pet.rarity,
+            max_ability_power: pet.max_ability_power,
+            skin: pet.skin,
+            shader: pet.shader,
+            created_on: pet.created_on,
+            inc_inv: pet.inc_inv,
+            inv: pet.inv,
+            ability1: pet.ability1,
+            ability2: pet.ability2,
+            ability3: pet.ability3,
+        }
+    }
+}
+
+impl From<Pet> for UpdatePet {
+    fn from(pet: Pet) -> Self {
+        UpdatePet {
+            name: pet.name,
+            type_: pet.type_,
+            instance_id: pet.instance_id,
+            rarity: pet.rarity,
+            max_ability_power: pet.max_ability_power,
+            skin: pet.skin,
+            shader: pet.shader,
+            created_on: pet.created_on,
+            inc_inv: pet.inc_inv,
+            inv: pet.inv,
+            ability1: pet.ability1,
+            ability2: pet.ability2,
+            ability3: pet.ability3,
+        }
+    }
+}
+
 // ############################
 // #           Stats          #
 // ############################
@@ -559,4 +886,24 @@ pub struct UpdateStats {
     pub total_fame: Option<i32>,
     #[diesel(column_name = "Fame")]
     pub fame: Option<i32>,
+}
+
+impl From<Stats> for NewStats {
+    fn from(stats: Stats) -> Self {
+        NewStats {
+            best_char_fame: stats.best_char_fame,
+            total_fame: stats.total_fame,
+            fame: stats.fame,
+        }
+    }
+}
+
+impl From<Stats> for UpdateStats {
+    fn from(stats: Stats) -> Self {
+        UpdateStats {
+            best_char_fame: stats.best_char_fame,
+            total_fame: stats.total_fame,
+            fame: stats.fame,
+        }
+    }
 }
