8e973d3919a094d266cd518a214f571a6a83da92
MaikEight <mail@maik8.de>
2024-03-22 21:17:50 +0100

    ✨ Add diesel_setup module for database setup

---
+++ src-modules/eam_commons/src/diesel_setup.rs
@@ -0,0 +1,21 @@
+use diesel::sqlite::SqliteConnection;
+use diesel::r2d2::{self, ConnectionManager};
+use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
+
+pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!("migrations");
+
+pub type DbPool = r2d2::Pool<ConnectionManager<SqliteConnection>>;
+
+pub fn setup_database(database_url: &str) -> DbPool {
+    std::env::set_var("DATABASE_URL", database_url);
+    let manager = ConnectionManager::<SqliteConnection>::new(database_url);
+    let pool = r2d2::Pool::builder()
+        .build(manager)
+        .expect("Failed to create pool.");
+
+    let mut conn = pool.get().expect("Failed to get connection from pool.");
+
+    conn.run_pending_migrations(MIGRATIONS).expect("Failed to run migrations");
+
+    pool
+}
