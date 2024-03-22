5e9a5dadc1b971f85d69d83da1f8d9e43dbfea5d
MaikEight <mail@maik8.de>
2024-03-22 21:17:13 +0100

    ✨ Add modules and utilities for encryption, database setup, and diesel functions

---
+++ src-modules/eam_commons/src/lib.rs
@@ -0,0 +1,12 @@
+pub mod encryption_utils;
+pub use encryption_utils::{encrypt_data, decrypt_data};
+
+pub mod models;
+
+pub mod schema;
+
+pub mod diesel_setup;
+pub use diesel_setup::{DbPool, setup_database};
+
+pub mod diesel_functions;
+pub use diesel_functions::*;
