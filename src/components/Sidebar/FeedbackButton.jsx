7deb82925b338bdbeef901d6eeaddd4f56b2f37d
MaikEight <mail@maik8.de>
2024-03-24 04:12:52 +0100

    💄 Refactor FeedbackButton component styling

--- src/components/Sidebar/FeedbackButton.jsx
+++ src/components/Sidebar/FeedbackButton.jsx
@@ -1,47 +1,67 @@
 import { useTheme } from "@emotion/react";
-import { Button } from "@mui/material";
-import InsertCommentOutlinedIcon from '@mui/icons-material/InsertCommentOutlined';
+import { Box, Button } from "@mui/material";
 import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
-function FeedbackButton({action}) {
+const GradientBorderButton = styled(Box)(({ theme, selected }) => ({
+    position: 'relative',
+    color: !selected ? theme.palette.text.primary : theme.palette.mode === 'light' ? theme.palette.background.default : theme.palette.text.primary,
+    textTransform: 'none',
+    borderRadius: '30px',
+    cursor: 'pointer',
+    padding: '2px',
+    background: 'linear-gradient(98deg, rgb(198, 167, 254), rgba(145, 85, 253) 94%)',
+    '&:hover': {
+        color: theme.palette.mode === 'light' ? theme.palette.background.default : theme.palette.text.primary,
+    },
+}));
+
+const GradientBorderButtonInner = styled(Box)(({ theme, selected }) => ({
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: '6px',
+    gap: '8px',
+    borderRadius: '30px',
+    background: selected ? 'linear-gradient(98deg, rgb(198, 167, 254), rgba(145, 85, 253) 94%)' : theme.palette.background.default,
+    transition: 'background 0.25s',
+    '&:hover': {
+        background: 'linear-gradient(98deg, rgb(198, 167, 254), rgba(145, 85, 253) 94%)',
+    }
+}));
+
+function FeedbackButton({ action }) {
     const selected = window.location.pathname === '/feedback';
     const navigate = useNavigate();
     const theme = useTheme();
 
     return (
-            <Button
-                variant="contained"
-                startIcon={<FavoriteBorderOutlinedIcon />}
-                sx={{
-                    borderRadius: "30px",
-                    transition: theme.transitions.create(['color', 'background-color', 'background-image', 'box-shadow']),
-                    textTransform: 'none',
-                    ...(!selected ?
-                        {
-                            color: theme.palette.text.primary,
-                            backgroundImage: 'linear-gradient(98deg, rgb(198, 167, 254), rgba(145, 85, 253) 94%)',
-                            "&:hover": {
-                                backgroundImage: 'linear-gradient(98deg, rgb(145, 85, 253), rgb(198, 167, 254) 94%)',
-                            }
-                        } : {
-                            color: theme.palette.mode === 'light' ? theme.palette.background.default : theme.palette.text.primary,
-                            backgroundImage: 'linear-gradient(98deg, rgb(198, 167, 254), rgb(145, 85, 253) 94%)',
-                            boxShadow: theme.palette.mode === 'light'
-                                ? 'rgba(58, 53, 65, 0.42) 0px 4px 8px -4px'
-                                : 'rgba(19, 17, 32, 0.42) 0px 4px 8px -4px',
-                            "&:hover": {
-                                backgroundImage: 'linear-gradient(98deg, rgb(198, 167, 254), rgb(145, 85, 253) 94%)',
-                            }
-                        })
-                }}
-                onClick={() => {
-                    navigate('/feedback');
-                    action?.();
-                }}
+        <GradientBorderButton
+            selected={selected}
+            onClick={() => {
+                navigate('/feedback');
+                action?.();
+            }}
+        >
+            <GradientBorderButtonInner 
+                selected={selected}
             >
-                Feedback
-            </Button>
+                <Box
+                    sx={{                        
+                        display: 'flex',
+                        gap: '8px',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        ml: 0.25,
+                        mr: 0.5,
+                    }}
+                >
+                    <FavoriteBorderOutlinedIcon />
+                    Feedback
+                </Box>
+            </GradientBorderButtonInner>
+        </GradientBorderButton>
     );
 }
 
