b6bc28bd657673b24d9b54d904762a1ba867cb55
MaikEight <mail@maik8.de>
2024-03-24 05:29:18 +0100

    💄 Update search bar width for medium-sized screens

--- src/components/GridComponents/Searchbar.jsx
+++ src/components/GridComponents/Searchbar.jsx
@@ -40,11 +40,10 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
     width: '100%',
     '& .MuiInputBase-input': {
         padding: theme.spacing(1, 1, 1, 0),
-        // vertical padding + font size from searchIcon
         paddingLeft: `calc(1em + ${theme.spacing(4)})`,
         transition: theme.transitions.create('width'),
-        [theme.breakpoints.up('sm')]: {
-            width: '12ch',
+        [theme.breakpoints.up('md')]: {
+            width: '18ch',
             '&:focus': {
                 width: '30ch',
             },
