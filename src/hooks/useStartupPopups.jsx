09f44395f0a89a98ce537be8b7bdcd849231e961
MaikEight <mail@maik8.de>
2024-03-30 23:17:15 +0100

    ✨ Add changelog popup functionality

--- src/hooks/useStartupPopups.jsx
+++ src/hooks/useStartupPopups.jsx
@@ -2,9 +2,18 @@ import { invoke } from "@tauri-apps/api";
 import ImportOldAccountsPopup from '../components/Popups/ImportOldAccountsPopup';
 import WelcomeToEamPopup from '../components/Popups/WelcomeToEamPopup';
 import useAccounts from "./useAccounts";
+import ChangelogVersion4_1_0 from "../components/Popups/Changelogs/ChangelogVersion4_1_0";
 
 function useStartupPopups() {
     const { accounts } = useAccounts();
+
+    const changelogPopups = [
+        {
+            version: "4.1.0",
+            preventClose: false,
+            content: <ChangelogVersion4_1_0 />
+        }        
+    ]
 
     const checkForFirstEamStart = () => {
         const firstEamStart = localStorage.getItem('firstEamStart');
@@ -29,10 +38,28 @@ function useStartupPopups() {
         };
     };
 
+    const getChangelogPopup = () => {
+        const lastChangelog = localStorage.getItem('lastChangelog');
+        const lastChangelogIndex = changelogPopups.findIndex(changelog => changelog.version === lastChangelog);
+
+        if (lastChangelogIndex === -1 || lastChangelogIndex > 0) {
+            localStorage.setItem('lastChangelog', changelogPopups[0].version);
+            return changelogPopups[0];
+        }        
+
+        return null;
+    }
+
     const performPopupCheck = async () => {
         const isFirstEamStart = checkForFirstEamStart();
         if (isFirstEamStart) {
             return await getFirstWelcomePopup();
+        }
+
+        const changelogPopup = getChangelogPopup();
+
+        if (changelogPopup) {
+            return changelogPopup;
         }
 
         return null;
