46f00d746b4c4b061b9ff38265700c76fc195df9
MaikEight <mail@maik8.de>
2024-03-30 23:16:24 +0100

    ✨ Add ChangelogPopupBase component

---
+++ src/components/Popups/Changelogs/ChangelogPopupBase.jsx
@@ -0,0 +1,61 @@
+import { Box, Typography } from "@mui/material";
+import ComponentBox from "../../ComponentBox";
+
+function ChangelogPopupBase({ version, releaseDate, title, icon, children }) {
+
+    const getTitle = () => {
+        return <Box
+            sx={{
+                display: 'flex',
+                width: '100%',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                gap: 2
+            }}
+        >
+            <Box sx={{
+                display: 'flex',
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'center',
+            }}
+            >
+                <Typography variant="h6"
+                                sx={{
+                                    fontWeight: 600,
+                                    textAlign: 'center',
+                                }}
+                >
+                EAM v{version} - {title}
+                </Typography>
+            </Box>
+            <Typography variant="body2">
+                {releaseDate}
+            </Typography>
+        </Box>
+    }
+
+    return (
+        <ComponentBox
+            title={getTitle()}
+            icon={icon}
+            sx={{
+                userSelect: "none",
+            }}
+        >
+            <Box
+                sx={{
+                    mt: -1,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: 1.5
+                }}
+            >
+                {children}
+            </Box>
+        </ComponentBox>
+    );
+}
+
+export default ChangelogPopupBase;
