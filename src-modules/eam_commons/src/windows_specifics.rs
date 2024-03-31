9db39b6cd4cf6e7f31f85bf72143b9f655845db9
MaikEight <mail@maik8.de>
2024-03-31 21:46:17 +0200

    🎨 Refactor Windows-specific code in eam_commons module

--- src-modules/eam_commons/src/windows_specifics.rs
+++ src-modules/eam_commons/src/windows_specifics.rs
@@ -1,4 +1,3 @@
-extern crate winapi;
 extern crate dirs;
 
 use std::fs::File;
@@ -7,14 +6,7 @@ use std::io::Write;
 use std::os::windows::process::CommandExt;
 use std::process::Stdio;
 
-use std::os::raw::c_void;
-use std::ptr::null_mut;
-use winapi::shared::sddl::ConvertSidToStringSidA;
-use winapi::um::handleapi::CloseHandle;
-use winapi::um::processthreadsapi::OpenProcessToken;
-use winapi::um::securitybaseapi::GetTokenInformation;
-use winapi::um::winnt::{TokenUser, TOKEN_QUERY};
-
+#[cfg(target_os = "windows")]
 const CREATE_NO_WINDOW: u32 = 0x08000000;
 
 // Exit codes of EAM_Task_Tools:
@@ -26,9 +18,10 @@ const CREATE_NO_WINDOW: u32 = 0x08000000;
 // 5 - Task already installed
 // 6 - Task not installed
 #[cfg(target_os = "windows")]
-const EAM_Task_Tools: &'static [u8] =
+const EAM_TASK_TOOLS: &'static [u8] =
     include_bytes!("../../EAM_Task_Installer/EAM_Task_Installer/bin/Release/EAM_Task_Tools.exe");
 
+    #[cfg(target_os = "windows")]
 pub fn check_for_installed_eam_daily_login_task() -> Result<bool, Error> {    
     let path = ensure_eam_task_tools()?;
 
@@ -45,6 +38,7 @@ pub fn check_for_installed_eam_daily_login_task() -> Result<bool, Error> {
     }
 }
 
+#[cfg(target_os = "windows")]
 pub fn install_eam_daily_login_task(exe_path: &str) -> Result<bool, Error> {
     let path = ensure_eam_task_tools()?;
 
@@ -74,6 +68,7 @@ pub fn install_eam_daily_login_task(exe_path: &str) -> Result<bool, Error> {
     }
 }
 
+#[cfg(target_os = "windows")]
 pub fn uninstall_eam_daily_login_task() -> Result<bool, Error> {
     let path = ensure_eam_task_tools()?;
 
@@ -103,51 +98,6 @@ pub fn uninstall_eam_daily_login_task() -> Result<bool, Error> {
 }
 
 #[cfg(target_os = "windows")]
-fn get_current_user_sid() -> Option<String> {
-    unsafe {
-        let mut token: winapi::um::winnt::HANDLE = null_mut();
-        if OpenProcessToken(
-            winapi::um::processthreadsapi::GetCurrentProcess(),
-            TOKEN_QUERY,
-            &mut token,
-        ) == 0
-        {
-            return None;
-        }
-
-        let mut return_length = 0;
-        GetTokenInformation(token, TokenUser, null_mut(), 0, &mut return_length);
-
-        let mut token_user = vec![0u8; return_length as usize];
-        if GetTokenInformation(
-            token,
-            TokenUser,
-            token_user.as_mut_ptr() as *mut _,
-            return_length,
-            &mut return_length,
-        ) == 0
-        {
-            CloseHandle(token);
-            return None;
-        }
-
-        CloseHandle(token);
-        let token_user = token_user.as_ptr() as *mut winapi::um::winnt::TOKEN_USER;
-        let sid = (*token_user).User.Sid;
-
-        let mut sid_str_ptr: *mut i8 = null_mut();
-        if ConvertSidToStringSidA(sid, &mut sid_str_ptr) == 0 {
-            return None;
-        }
-
-        let sid_str = std::ffi::CStr::from_ptr(sid_str_ptr)
-            .to_string_lossy()
-            .into_owned();
-        // winapi::um::winbase::LocalFree(sid_str_ptr as *mut c_void);
-        Some(sid_str)
-    }
-}
-
 fn ensure_eam_task_tools() -> Result<String, Error> {
     let mut path = dirs::data_local_dir().unwrap();
     path.push("ExaltAccountManager");
@@ -161,7 +111,7 @@ fn ensure_eam_task_tools() -> Result<String, Error> {
 
     if !path.exists() {
         let mut file = File::create(&path)?;
-        file.write_all(EAM_Task_Tools)?;
+        file.write_all(EAM_TASK_TOOLS)?;
         file.sync_all()?;
         drop(file);
     }
