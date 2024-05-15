8d6d88230df0167cf94fbea79bde6427b65c0c22
MaikEight <mail@maik8.de>
2024-05-16 01:31:27 +0200

    ⚡️ Refactor realmUpdaterUtils.js for improved performance by using the new game updater (completely in Rust)

--- src/utils/realmUpdaterUtils.js
+++ src/utils/realmUpdaterUtils.js
@@ -1,33 +1,28 @@
-import { tauri } from '@tauri-apps/api';
+import { invoke, tauri } from '@tauri-apps/api';
 import { getAppInit, getGameFileList } from '../backend/decaApi';
 import { getCurrentTime } from './timeUtils';
 import { UPDATE_URLS } from '../constants';
 import { logToErrorLog } from './loggingUtils';
 
-async function checkForUpdates(gameExePath) {
+async function checkForUpdates(force) {
     if (sessionStorage.getItem('updateCheckInProgress') === 'true' ||
         sessionStorage.getItem('updateInProgress') === 'true') {
         return
     };
 
     sessionStorage.setItem('updateCheckInProgress', 'true');
+    console.log('Checking for updates...');
+    const updateNeeded = await invoke('check_for_game_update', { force: force });
+    console.log('Update needed:', updateNeeded);
 
-    const buildHash = await getClientBuildHash();
-    const fileList = await getFileList(buildHash, gameExePath);
-
-    if (fileList.length > 0) {
-        localStorage.setItem('updateNeeded', 'true');
-    } else {
-        localStorage.removeItem('updateNeeded');
-    }
-
+    localStorage.setItem('updateNeeded', updateNeeded ? 'true' : 'false');
     localStorage.setItem('lastUpdateCheck', getCurrentTime());
     sessionStorage.setItem('updateCheckInProgress', 'false');
 
-    return fileList;
+    return updateNeeded;
 }
 
-async function updateGame(gameExePath) {
+async function updateGame() {
     if (sessionStorage.getItem('updateCheckInProgress') === 'true' ||
         sessionStorage.getItem('updateInProgress') === 'true') {
         return
@@ -35,31 +30,7 @@ async function updateGame(gameExePath) {
 
     sessionStorage.setItem('updateInProgress', 'true');
 
-    const buildHash = await getClientBuildHash();
-    const fileList = await getFileList(buildHash, gameExePath);
-
-    if(!sessionStorage.getItem('buildCDN')){
-        await getAppInit();
-    }
-    const buildCDN = sessionStorage.getItem('buildCDN');
-
-    if(!buildHash || !buildCDN) throw new Error('Build hash or CDN not found', buildHash, buildCDN);
-
-    const updateFiles = fileList.map((file) => {
-        return {
-            ...file,
-            url: `${buildCDN}${UPDATE_URLS(2, [buildHash, file.file])}`,
-        };
-    });
-    
-    tauri.invoke('perform_game_update', {
-        args: {
-            game_exe_path: gameExePath,
-            game_files_data: JSON.stringify(updateFiles),
-        }
-    }).then(() => {
-        sessionStorage.removeItem('buildHash');
-        sessionStorage.removeItem('fileList');
+    tauri.invoke('perform_game_update').then(() => {        
         localStorage.removeItem('updateNeeded');
     }).catch((error) => {
         logToErrorLog('updateGame', error);
@@ -67,54 +38,6 @@ async function updateGame(gameExePath) {
         sessionStorage.setItem('updateInProgress', 'false');
     });
 }
-
-async function getClientBuildHash() {
-
-    if (sessionStorage.getItem('buildHash') !== null) {
-        return sessionStorage.getItem('buildHash');
-    }
-
-    return await getAppInit()
-        .then(async (appInit) => {
-            console.log('appInit', appInit);
-            const appSettings = appInit.AppSettings;
-            sessionStorage.setItem('buildHash', appSettings.BuildHash);
-            sessionStorage.setItem('buildCDN', appSettings.BuildCDN ? appSettings.BuildCDN.replace('build-release/', '') : 'https://rotmg-build.decagames.com/');
-            return appSettings.BuildHash;
-        });
-};
-
-async function getFileList(buildHash, gameExePath) {
-    if (sessionStorage.getItem('fileList') !== null) {
-        return JSON.parse(sessionStorage.getItem('fileList'));
-    }
-
-    if (!buildHash || buildHash === '' && sessionStorage.getItem('buildHash') !== null) {
-        buildHash = sessionStorage.getItem('buildHash');
-    }
-
-    if(!buildHash || buildHash === '') {
-        buildHash = await getClientBuildHash();
-    }
-
-    return await getGameFileList(buildHash)
-        .then(async (fileList) => {
-            const file = await tauri.invoke(
-                'get_game_files_to_update',
-                {
-                    args: {
-                        game_exe_path: gameExePath,
-                        game_files_data: JSON.stringify(fileList),
-                    }
-                });
-
-            if (file !== null && file.length > 0) {
-                sessionStorage.setItem('fileList', JSON.stringify(file));
-            }
-
-            return file;
-        });
-};
 
 export {
     checkForUpdates,
