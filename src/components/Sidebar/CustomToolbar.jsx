a1fd82b1a0db80d3f5b6cd83c7acfe6450782ded
MaikEight <mail@maik8.de>
2024-05-20 04:11:11 +0200

    🔥 Remove unused code and simplify imports

--- src/components/Sidebar/CustomToolbar.jsx
+++ src/components/Sidebar/CustomToolbar.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, ButtonGroup, IconButton, Typography } from "@mui/material";
+import { Box, Button, ButtonGroup } from "@mui/material";
 import { appWindow } from '@tauri-apps/api/window'
 import MinimizeIcon from '@mui/icons-material/Minimize';
 import CloseIcon from '@mui/icons-material/Close';
@@ -17,27 +17,6 @@ function CustomToolbar(props) {
                 ...props.sx
             }}
         >
-            {/* WATERMARK */}
-            {/* <Box>
-                <a href="https://github.com/MaikEight/ExaltAccountManager" target="_blank" rel="noopener noreferrer">
-                    <Typography
-                        sx={{
-                            position: "absolute",
-                            top: 2,
-                            left: 242,
-                            color: theme.palette.text.primary,
-                            fontSize: 12,
-                            fontWeight: 500,
-                            letterSpacing: 0.5,
-                            ml: 0.5,
-                            mt: 0.25,
-                        }}
-                    >
-                        EAM DEVELOPMENT BUILD v4.0.0 by Maik8
-                    </Typography>
-                </a>
-            </Box> */}
-
             <ButtonGroup
                 disableElevation
                 variant="text"
