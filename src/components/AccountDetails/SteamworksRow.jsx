1f79d051dd25a45d173a97ac451141d3d322f7c5
MaikEight <mail@maik8.de>
2024-05-24 01:37:38 +0200

    🎨 Refactor SteamworksRow and SteamworksMailColumn to use theme palette mode for selecting the appropriate Steam logo

--- src/components/AccountDetails/SteamworksRow.jsx
+++ src/components/AccountDetails/SteamworksRow.jsx
@@ -1,10 +1,11 @@
 import { TableRow, Tooltip, Typography } from "@mui/material";
 import PaddedTableCell from "./PaddedTableCell";
 import OpenInNewOutlinedIcon from '@mui/icons-material/OpenInNewOutlined';
+import { useTheme } from "@emotion/react";
 
 function SteamworksRow({ guid, innerSx, ...rest }) {
     const steamId = guid.split(':')[1];
-
+    const theme = useTheme();
     return (
         <TableRow {...rest}>
             <PaddedTableCell sx={innerSx}>
@@ -21,7 +22,7 @@ function SteamworksRow({ guid, innerSx, ...rest }) {
                         rel="noopener noreferrer"
                         style={{ display: 'flex', alignItems: 'center', textDecoration: 'none' }}
                     >
-                        <img src="/steam.svg" alt="Steam Logo" height='20px' width='20px' style={{ marginRight: '6px' }} />
+                        <img src={theme.palette.mode === 'dark' ? "/steam.svg" : "/steam_light_mode.svg"} alt="Steam Logo" height='20px' width='20px' style={{ marginRight: '6px' }} />
                         <Typography variant="body2" fontWeight={300} component="span" sx={{ textAlign: 'center', mr: 0.5 }}>
                             {steamId}
                         </Typography>
--- src/components/GridComponents/SteamworksMailColumn.jsx
+++ src/components/GridComponents/SteamworksMailColumn.jsx
@@ -1,7 +1,9 @@
+import { useTheme } from "@emotion/react";
 import { Box, Typography } from "@mui/material";
 
 function SteamworksMailColumn(params) {
     const id = params.params.value && params.params.value.startsWith('steamworks:') ? params.params.value.split(':')[1] : "-";
+    const theme = useTheme();
 
     return (
         <Box
@@ -12,7 +14,7 @@ function SteamworksMailColumn(params) {
                 height: '100%',
             }}
         >
-            <img src="/steam.svg" alt="Steam Logo" height='20px' width='20px' style={{ marginRight: '6px' }} />
+            <img src={theme.palette.mode === 'dark' ? "/steam.svg" : "/steam_light_mode.svg"} alt="Steam Logo" height='20px' width='20px' style={{ marginRight: '6px' }} />
             <Typography variant="body1" sx={{ fontSize: '0.875rem', fontFamily: 'Roboto', }}>
                 {id}
             </Typography>
