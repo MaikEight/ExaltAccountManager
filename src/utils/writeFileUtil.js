2600034bbcc85f020d796b3a5198ee380e1ab63e
MaikEight <mail@maik8.de>
2024-05-25 03:49:07 +0200

    🔥 Remove unused code

--- src/utils/writeFileUtil.js
+++ src/utils/writeFileUtil.js
@@ -3,7 +3,6 @@ import { logToErrorLog } from "./loggingUtils";
 export async function writeFileUTF8(filePath, content, stringifyAsJSON = false) {
     try {
         const stringContent = stringifyAsJSON ? JSON.stringify(content) : content;
-        const bytes = new TextEncoder().encode(stringContent);
 
         await window.__TAURI__.fs.writeFile({
             path: filePath,
