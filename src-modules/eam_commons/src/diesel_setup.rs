4fdb84a8b98569984a622908b4eea8fe8d9372cf
MaikEight <mail@maik8.de>
2024-05-29 00:33:27 +0200

    ⚡️ Update diesel_setup.rs to use Result type for setup_database function

--- src-modules/eam_commons/src/diesel_setup.rs
+++ src-modules/eam_commons/src/diesel_setup.rs
@@ -1,21 +1,24 @@
 use diesel::sqlite::SqliteConnection;
 use diesel::r2d2::{self, ConnectionManager};
 use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
+use diesel::r2d2::{Pool, PooledConnection};
+use std::error::Error;
 
 pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!("migrations");
 
 pub type DbPool = r2d2::Pool<ConnectionManager<SqliteConnection>>;
+pub type DbConn = PooledConnection<ConnectionManager<SqliteConnection>>;
 
-pub fn setup_database(database_url: &str) -> DbPool {
+pub fn setup_database(database_url: &str) -> Result<DbPool, Box<dyn Error + Send + Sync>> {
     std::env::set_var("DATABASE_URL", database_url);
     let manager = ConnectionManager::<SqliteConnection>::new(database_url);
     let pool = r2d2::Pool::builder()
-        .build(manager)
-        .expect("Failed to create pool.");
+        .build(manager)?;
 
-    let mut conn = pool.get().expect("Failed to get connection from pool.");
+    let mut conn = pool.get()?;
 
-    conn.run_pending_migrations(MIGRATIONS).expect("Failed to run migrations");
+    conn.run_pending_migrations(MIGRATIONS)?;
 
-    pool
+    Ok(pool)
 }
+
