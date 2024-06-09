79eb649521021fee4fe0ce03b60199e69c974785
MaikEight <mail@maik8.de>
2024-06-09 06:19:06 +0200

    ✨ Add ErrorBoundaryFallback component

---
+++ src/components/ErrorBoundaryFallback.jsx
@@ -0,0 +1,106 @@
+import { Box, Typography } from "@mui/material";
+import { useEffect } from "react";
+import { useErrorBoundary } from "react-error-boundary";
+import { useLocation } from "react-router-dom";
+import ComponentBox from "./ComponentBox";
+import WarningAmberOutlinedIcon from '@mui/icons-material/WarningAmberOutlined';
+import SupportAgentOutlinedIcon from '@mui/icons-material/SupportAgentOutlined';
+import { invoke } from "@tauri-apps/api/tauri";
+import { useTheme } from "@emotion/react";
+
+function ErrorBoundaryFallback() {
+    const autoFixLocations = sessionStorage.getItem('autoFixTried');
+    const { resetBoundary } = useErrorBoundary();
+    const location = useLocation();
+    const theme = useTheme();
+
+    useEffect(() => {
+        const locs = autoFixLocations ? autoFixLocations.split(',') : [];
+        if (locs.includes(location.pathname)) {
+            return;
+        }
+        locs.push(location.pathname);
+        sessionStorage.setItem('autoFixTried', locs.join(','));
+
+        console.log('ErrorBoundaryFallback', location);
+        resetBoundary();
+
+        return () => {
+            console.log('ErrorBoundaryFallback cleanup', location);
+            sessionStorage.removeItem('autoFixTried');
+        }
+    }, [location]);
+
+    const handleClickUrl = async (url) => {
+        await invoke("open_url", { url: url });
+    }
+
+    return (
+        <Box
+            sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                height: '100%',
+                width: '100%',
+                mt: 2,
+                gap: 2,
+            }}
+        >
+            <ComponentBox
+                title={"A critical error occurred"}
+                icon={<WarningAmberOutlinedIcon />}
+                sx={{
+                    my: 0,
+                    color: 'error.main',
+                }}
+                innerSx={{
+                    color: 'text.primary',
+                }}
+            >
+                {
+                    <Box>
+                        <Typography variant='body1'>
+                            Please try to refresh the page by pressing <b>CTRL + R</b> or <b>F5</b>. If the error persists, please contact support.
+                        </Typography>
+                        <Typography variant='body1'>
+                            If the problem still persists, please change the page on the sidebar and try again.
+                        </Typography>
+                    </Box>
+                }
+            </ComponentBox>
+            <ComponentBox
+                title="Contact Support"
+                icon={<SupportAgentOutlinedIcon />}
+                sx={{
+                    my: 0,
+                }}
+            >
+                <Typography variant='body1'>
+                    If you can't solve the problem, here are some ways to contact support:
+                </Typography>
+
+                <ul>
+                    <li>
+
+                        <Typography variant='body1' component={'span'}>
+                            <span
+                                onClick={() => handleClickUrl("https://discord.exalt-account-manager.eu/")}
+                                style={{ cursor: 'pointer', color: theme.palette.primary.main, textDecoration: 'underline', paddingRight: '0.3rem' }}
+                            >
+                                Join our Discord server
+                            </span>
+                            and ask for help in the support-center or help-request channel.
+                        </Typography>
+                    </li>
+                    <li>
+                        <Typography variant='body1' component={'span'}>
+                            Send an email to <a style={{ cursor: 'pointer', color: theme.palette.primary.main }} href="mailto:mail@maik8.de" rel="noopener noreferrer">mail@maik8.de</a>
+                        </Typography>
+                    </li>
+                </ul>
+            </ComponentBox>
+        </Box>
+    );
+}
+
+export default ErrorBoundaryFallback;
