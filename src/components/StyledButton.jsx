16132b39fdb579ecb1695b7d7691f71e4bd15b47
MaikEight <mail@maik8.de>
2024-06-12 18:15:39 +0200

    💄 Adjusted the borderRadius

--- src/components/StyledButton.jsx
+++ src/components/StyledButton.jsx
@@ -10,7 +10,7 @@ function StyledButton({ children, fullWidth, ...props }) {
             fullWidth={fullWidth}
             {...props}
             sx={{
-                borderRadius: 1.5,                
+                borderRadius: `${theme.shape.borderRadius}px`,                
                 boxShadow: theme.palette.mode === 'dark' ? 'rgba(19, 17, 32, 0.42) 0px 4px 8px -4px;' : 'rgba(58, 53, 65, 0.42) 0px 4px 8px -4px;',
                 ...props.sx,
             }}
