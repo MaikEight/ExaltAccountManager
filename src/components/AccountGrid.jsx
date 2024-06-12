9a48f3b8a3593a7da9db312ce8147013aa8638f0
MaikEight <mail@maik8.de>
2024-06-12 18:14:33 +0200

    💄Adjusted the borderRadius

--- src/components/AccountGrid.jsx
+++ src/components/AccountGrid.jsx
@@ -30,12 +30,12 @@ const StyledDataGrid = styled(DataGrid)`
 
 function AccountGrid({ setShowAddNewAccount }) {
     const { accounts, selectedAccount, setSelectedAccount, updateAccount } = useAccounts();
-    
+
     const [shownAccounts, setShownAccounts] = useState(accounts);
-    const [search, setSearch] = useState('');    
+    const [search, setSearch] = useState('');
     const [paginationModel, setPaginationModel] = useState({ page: 0, pageSize: 100 });
 
-    const settings = useUserSettings();    
+    const settings = useUserSettings();
 
     useEffect(() => {
         setShownAccounts(getSearchedAccounts());
@@ -56,16 +56,16 @@ function AccountGrid({ setShowAddNewAccount }) {
         if (!group) return null;
 
         return <GroupUI group={group} />;
-    };   
-    
+    };
+
 
     const columns = [
         { field: 'group', headerName: 'Group', width: 65, renderCell: (params) => getGroupUI(params) },
         { field: 'name', headerName: 'Accountname', minWidth: 150, width: 230, flex: 0.2 },
-        { field: 'email', headerName: 'Email', minWidth: 150, flex: 0.3, renderCell: (params) => { return (params.value && params.value.startsWith('steamworks:')) ? <SteamworksMailColumn params={params} /> : params.value} },
-        { field: 'lastLogin', headerName: 'Last Login', minWidth: 115, flex: 0.125, type: 'dateTime', renderCell: (params) => <div style={{ textAlign: 'center' }}> {formatTime(params.value)} </div>},
+        { field: 'email', headerName: 'Email', minWidth: 150, flex: 0.3, renderCell: (params) => { return (params.value && params.value.startsWith('steamworks:')) ? <SteamworksMailColumn params={params} /> : params.value } },
+        { field: 'lastLogin', headerName: 'Last Login', minWidth: 115, flex: 0.125, type: 'dateTime', renderCell: (params) => <div style={{ textAlign: 'center' }}> {formatTime(params.value)} </div> },
         { field: 'serverName', headerName: 'Server', width: 125, renderCell: (params) => <ServerChip params={params} /> },
-        { field: 'lastRefresh', headerName: 'Last refresh', minWidth: 115, flex: 0.125, renderCell: (params) => <div style={{ textAlign: 'center' }}> {formatTime(params.value)} </div>},
+        { field: 'lastRefresh', headerName: 'Last refresh', minWidth: 115, flex: 0.125, renderCell: (params) => <div style={{ textAlign: 'center' }}> {formatTime(params.value)} </div> },
         { field: 'performDailyLogin', headerName: 'Daily Login', width: 95, renderCell: (params) => <DailyLoginCheckbox params={params} onChange={(event) => handleDailyLoginCheckboxChange(event, params)} /> },
         { field: 'state', headerName: 'Last State', width: 110 },
     ];
@@ -86,74 +86,75 @@ function AccountGrid({ setShowAddNewAccount }) {
 
         const filteredAccounts = accounts.filter((account) => {
             const { name, email, serverName, group } = account;
-            
-            return name?.toLowerCase().includes(search.toLowerCase()) 
-                || email?.toLowerCase().includes(search.toLowerCase()) 
-                || serverName?.toLowerCase().includes(search.toLowerCase()) 
+
+            return name?.toLowerCase().includes(search.toLowerCase())
+                || email?.toLowerCase().includes(search.toLowerCase())
+                || serverName?.toLowerCase().includes(search.toLowerCase())
                 || group?.toLowerCase().includes(search.toLowerCase());
-        }); 
+        });
         return filteredAccounts;
     };
 
     return (
-            <Paper sx={{ minHeight: '200px', height: 'calc(100vh - 70px)', width: '100%', borderRadius: 1.5, background: theme.palette.background.paper, }}>
-                <StyledDataGrid
-                    sx={{
-                        minHeight: '200px',
-                        width: '100%',
-                        border: 0,
-                        '&, [class^=MuiDataGrid]': { border: 'none' },
-                        '& .MuiDataGrid-columnHeaders': {
-                            backgroundColor: theme.palette.background.paperLight,
-                        },
-                        '& .MuiDataGrid-virtualScroller::-webkit-scrollbar': {
-                            backgroundColor: theme.palette.background.paper,
-                        },
-                        '& .MuiDataGrid-virtualScroller::-webkit-scrollbar-thumb': {
-                            backgroundColor: theme.palette.mode === 'dark' ? theme.palette.background.default : darken(theme.palette.background.default, 0.15),
-                            border: `3px solid ${theme.palette.background.paper}`,
-                            borderRadius: 1.5
-                        },
-                    }}
-                    initialState={{
-                        columns: {
-                          columnVisibilityModel: settings.getByKeyAndSubKey('accounts', 'columnsHidden'),
-                        },
-                      }}
-                    rows={shownAccounts}
-                    getRowId={(row) => row.id}
-                    columns={columns}
-                    pageSizeOptions={[10, 25, 50, 100]}
-                    getRowHeight={() => "auto"}
-                    rowSelection
-                    getEstimatedRowHeight={() => 41}
-                    rowCount={shownAccounts.length}
-                    onCellClick={handleCellClick}
-                    onRowSelectionModelChange={(ids) => {
-                        const selectedId = ids[0];
-                        const selected = accounts.find((account) => account.id === selectedId);
-                        if (selected && selected !== selectedAccount) {
-                            setSelectedAccount(selected);
-                            return;
-                        }
-                        setSelectedAccount(null);
-                    }}
-                    rowSelectionModel={selectedAccount ? [selectedAccount.id] : []}
-                    paginationModel={paginationModel}
-                    onPaginationModelChange={setPaginationModel}
-                    checkboxSelection={false}
-                    hideFooterSelectedRowCount
-                    slots={{
-                        pagination: CustomPagination,
-                        toolbar: CustomToolbar,
-                        loadingOverlay: LinearProgress,
-                    }}
-                    slotProps={{
-                        toolbar: { onSearchChanged: (search) => setSearch(search), onAddNew: () => setShowAddNewAccount(true) },
-                        pagination: { labelRowsPerPage: "Accounts per page:"}
-                    }}
-                />
-            </Paper>
+        <Paper sx={{ minHeight: '200px', height: 'calc(100vh - 70px)', width: '100%', background: theme.palette.background.paper, }}>
+            <StyledDataGrid
+                sx={{
+                    minHeight: '200px',
+                    width: '100%',
+                    border: 0,
+                    '&, [class^=MuiDataGrid]': { border: 'none' },
+                    '& .MuiDataGrid-columnHeaders': {
+                        backgroundColor: theme.palette.background.paperLight,
+                    },
+                    '& .MuiDataGrid-virtualScroller::-webkit-scrollbar': {
+                        borderRadius: theme.shape.borderRadius,
+                        backgroundColor: theme.palette.background.paper,
+                    },
+                    '& .MuiDataGrid-virtualScroller::-webkit-scrollbar-thumb': {
+                        backgroundColor: theme.palette.mode === 'dark' ? theme.palette.background.default : darken(theme.palette.background.default, 0.15),
+                        border: `3px solid ${theme.palette.background.paper}`,
+                        borderRadius: theme.shape.borderRadius,
+                    },
+                }}
+                initialState={{
+                    columns: {
+                        columnVisibilityModel: settings.getByKeyAndSubKey('accounts', 'columnsHidden'),
+                    },
+                }}
+                rows={shownAccounts}
+                getRowId={(row) => row.id}
+                columns={columns}
+                pageSizeOptions={[10, 25, 50, 100]}
+                getRowHeight={() => "auto"}
+                rowSelection
+                getEstimatedRowHeight={() => 41}
+                rowCount={shownAccounts.length}
+                onCellClick={handleCellClick}
+                onRowSelectionModelChange={(ids) => {
+                    const selectedId = ids[0];
+                    const selected = accounts.find((account) => account.id === selectedId);
+                    if (selected && selected !== selectedAccount) {
+                        setSelectedAccount(selected);
+                        return;
+                    }
+                    setSelectedAccount(null);
+                }}
+                rowSelectionModel={selectedAccount ? [selectedAccount.id] : []}
+                paginationModel={paginationModel}
+                onPaginationModelChange={setPaginationModel}
+                checkboxSelection={false}
+                hideFooterSelectedRowCount
+                slots={{
+                    pagination: CustomPagination,
+                    toolbar: CustomToolbar,
+                    loadingOverlay: LinearProgress,
+                }}
+                slotProps={{
+                    toolbar: { onSearchChanged: (search) => setSearch(search), onAddNew: () => setShowAddNewAccount(true) },
+                    pagination: { labelRowsPerPage: "Accounts per page:" }
+                }}
+            />
+        </Paper>
     );
 }
 
