426fa46f9b6c10ff3cc91328af25eb415fa8454c
MaikEight <mail@maik8.de>
2024-06-15 04:07:13 +0200

    🎨 Update UPDATE_URLS function in constants.js

--- src/constants.js
+++ src/constants.js
@@ -40,10 +40,11 @@ export function UPDATE_URLS(index, values) {
     switch (index) {
         case 0:
             return updateBaseUrls[index];
-        case 1:
+        case 1: {
             if (!values) return updateBaseUrls[index];
             const v = updateBaseUrls[index].replace("{0}", values);        
             return v;
+        }
         case 2:
             if (values.length < 2) return updateBaseUrls[index];
             return updateBaseUrls[index].replace("{0}", values[0]).replace("{1}", values[1]);
