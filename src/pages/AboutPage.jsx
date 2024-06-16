8ade3b21c622f293d8d1024cbed09aa223c38e3a
MaikEight <mail@maik8.de>
2024-06-16 03:02:34 +0200

    🔥 Refactor AboutPage.jsx to remove unused imports and improve code readability

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import ComponentBox from './../components/ComponentBox';
 import NumbersOutlinedIcon from '@mui/icons-material/NumbersOutlined';
 import CodeOutlinedIcon from '@mui/icons-material/CodeOutlined';
