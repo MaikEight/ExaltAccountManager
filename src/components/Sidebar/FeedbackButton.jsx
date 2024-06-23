40e8738afcf23edcae09323161b48ab678f8994b
MaikEight <mail@maik8.de>
2024-06-23 02:30:44 +0200

    🎨 Update FeedbackButton component to use the new GadientButton-Parts

--- src/components/Sidebar/FeedbackButton.jsx
+++ src/components/Sidebar/FeedbackButton.jsx
@@ -1,65 +1,59 @@
 import { Box } from "@mui/material";
 import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 import { useNavigate } from "react-router-dom";
-import styled from "styled-components";
+import { useState, useEffect } from "react";
+import GradientBorderButton from "./ButtonParts/GradientBorderButton";
+import GradientBorderButtonInner from "./ButtonParts/GradientBorderButtonInner";
 
-const GradientBorderButton = styled(Box)(({ theme, selected }) => ({
-    position: 'relative',
-    color: !selected ? theme.palette.text.primary : theme.palette.mode === 'light' ? theme.palette.background.default : theme.palette.text.primary,
-    textTransform: 'none',
-    borderRadius: '30px',
-    cursor: 'pointer',
-    padding: '2px',
-    background: 'linear-gradient(98deg, rgb(198, 167, 254), rgba(145, 85, 253) 94%)',
-    '&:hover': {
-        color: theme.palette.mode === 'light' ? theme.palette.background.default : theme.palette.text.primary,
-    },
-}));
-
-const GradientBorderButtonInner = styled(Box)(({ theme, selected }) => ({
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: '6px',
-    gap: '8px',
-    borderRadius: '30px',
-    background: selected ? 'linear-gradient(98deg, rgb(198, 167, 254), rgba(145, 85, 253) 94%)' : theme.palette.background.default,
-    transition: theme.transitions.create('background-color'),
-    '&:hover': {
-        background: 'linear-gradient(98deg, rgb(198, 167, 254), rgba(145, 85, 253) 94%)',
-    }
-}));
-
-function FeedbackButton({ action }) {
+function FeedbackButton({ smallSize, action }) {
     const selected = window.location.pathname === '/feedback';
     const navigate = useNavigate();
+    const [showText, setShowText] = useState(true);
+
+    useEffect(() => {
+
+        const timeOut = setTimeout(() => {
+            setShowText(smallSize);
+        }, smallSize ? 25 : 150);
+
+        return () => {
+            clearTimeout(timeOut);
+        };
+    }, [smallSize]);
 
     return (
-        <GradientBorderButton
-            selected={selected}
-            onClick={() => {
-                navigate('/feedback');
-                action?.();
+        <Box
+            sx={{
+                width: smallSize ? 44 : 122,
+                transition: theme => theme.transitions.create('width'),
             }}
         >
-            <GradientBorderButtonInner 
+            <GradientBorderButton
                 selected={selected}
+                onClick={() => {
+                    navigate('/feedback');
+                    action?.();
+                }}
             >
-                <Box
-                    sx={{                        
-                        display: 'flex',
-                        gap: '8px',
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                        ml: 0.25,
-                        mr: 0.5,
-                    }}
+                <GradientBorderButtonInner
+                    selected={selected}
                 >
-                    <FavoriteBorderOutlinedIcon />
-                    Feedback
-                </Box>
-            </GradientBorderButtonInner>
-        </GradientBorderButton>
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            gap: '8px',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            ml: 0.25,
+                            mr: 0.25,
+                        }}
+                    >
+                        <FavoriteBorderOutlinedIcon />
+                        {!showText && 'Feedback'}
+                    </Box>
+                </GradientBorderButtonInner>
+            </GradientBorderButton>
+        </Box>
     );
 }
 
