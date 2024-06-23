37d030d4c5f86f1b9e45fd96e661438f404c8a83
MaikEight <mail@maik8.de>
2024-06-23 02:29:59 +0200

    ✨ Add GradientBorderButton component

---
+++ src/components/Sidebar/ButtonParts/GradientBorderButton.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+import Box from '@mui/material/Box';
+
+const GradientBorderButton = styled(Box)(({ theme, selected }) => ({
+    position: 'relative',
+    color: !selected ? theme.palette.text.primary : theme.palette.mode === 'light' ? theme.palette.background.default : theme.palette.text.primary,
+    textTransform: 'none',
+    borderRadius: '30px',
+    cursor: 'pointer',
+    padding: '2px',
+    background: 'linear-gradient(98deg, rgb(198, 167, 254), rgba(145, 85, 253) 94%)',
+    '&:hover': {
+        color: theme.palette.mode === 'light' ? theme.palette.background.default : theme.palette.text.primary,
+    },
+}));
+
+export default GradientBorderButton;
