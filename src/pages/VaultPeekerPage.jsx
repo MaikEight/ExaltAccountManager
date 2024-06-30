4fe426f263a750e90b3be94aecb9ee8258b27571
MaikEight <mail@maik8.de>
2024-06-30 05:02:10 +0200

    🎨 Refactor VaultPeekerPage

--- src/pages/VaultPeekerPage.jsx
+++ src/pages/VaultPeekerPage.jsx
@@ -3,31 +3,36 @@ import ItemCanvas from "../components/Realm/ItemCanvas";
 import items from "../assets/constants";
 import ComponentBox from "../components/ComponentBox";
 import FilterListOutlinedIcon from '@mui/icons-material/FilterListOutlined';
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import Searchbar from './../components/GridComponents/Searchbar';
 import { invoke } from "@tauri-apps/api";
-import { extractRealmItemsFromCharListDatasets } from './../utils/realmItemUtils';
-import ItemLocationPopper from "../components/Realm/ItemLocationPopper";
-
-const chunkArray = (array, chunkSize) => {
-    const chunks = [];
-    for (let i = 0; i < array.length; i += chunkSize) {
-        chunks.push(array.slice(i, i + chunkSize));
-    }
-    return chunks;
-};
+import { extractRealmItemsFromCharListDatasets, formatAccountDataFromCharListDataset, formatAccountDataFromCharListDatasets } from './../utils/realmItemUtils';
+import { useTheme } from "@emotion/react";
+import useAccounts from "../hooks/useAccounts";
+import Character from "../components/Realm/Character";
 
 function VaultPeekerPage() {
-    const [totalItems, setTotalItems] = useState();
+    const [totalItems, setTotalItems] = useState([]);
+    const [accountsData, setAccountsData] = useState([]);
     const [filteredTotals, setFilteredTotals] = useState([]);
-    const [popperAnchor, setPopperAnchor] = useState(null);
+    const theme = useTheme();
+    const { getAccountByEmail } = useAccounts();
 
     useEffect(() => {
         const loadItems = async () => {
             const res = await invoke('get_latest_char_list_dataset_for_each_account');
             const items = extractRealmItemsFromCharListDatasets(res);
             setTotalItems(items);
-            console.log(items);
+            let accs = formatAccountDataFromCharListDatasets(res);
+            if (accs?.length > 0) {
+                accs = accs.map((acc) => {
+                    return {
+                        ...acc,
+                        name: getAccountByEmail(acc.email)?.name,
+                    }
+                });
+            }
+            setAccountsData(accs);
         };
         loadItems();
     }, []);
@@ -77,12 +82,14 @@ function VaultPeekerPage() {
                 icon={<FilterListOutlinedIcon />}
                 isCollapseable={true}
             >
-
+                <Character character={null} />
             </ComponentBox>
 
+            {/* Totals */}
             <ComponentBox
                 title='Totals'
                 isCollapseable={true}
+                defaultCollapsed
                 innerSx={{ position: 'relative', overflow: 'hidden', }}
             >
                 <ItemCanvas
@@ -93,7 +100,7 @@ function VaultPeekerPage() {
                 />
 
                 <img
-                    src={'/logo/logo_inner_big.png'}
+                    src={theme.palette.mode === 'dark' ? '/logo/logo_inner_big.png' : '/logo/logo_inner_big_dark.png'}
                     alt="EAM Logo"
                     style={{
                         height: '50%',
@@ -102,12 +109,37 @@ function VaultPeekerPage() {
                         top: '50%',
                         left: '50%',
                         transform: 'translate(-50%, -50%)',
-                        opacity: 0.05,
+                        opacity: theme.palette.mode === 'dark' ? 0.05 : 0.1,
                         pointerEvents: 'none',
                         zIndex: 1,
                     }}
                 />
             </ComponentBox>
+
+            {/* Accounts */}
+            {
+                accountsData &&
+                accountsData.map((accountData, index) => {
+                    return (
+                        <ComponentBox
+                            key={index}
+                            title={accountData.name ? accountData.name : accountData.email}
+                            isCollapseable={true}
+                            innerSx={{ position: 'relative', overflow: 'hidden', }}
+                        >
+                            {
+                                // accountData.character &&
+                                // accountData.character.map((char, index) => {
+                                //     // console.log(accountData.email + ' : ' + char.class);
+                                //     return (
+                                //         <Character key={index} character={char}/>
+                                //     );
+                                // })
+                            }
+                        </ComponentBox>
+                    );
+                })
+            }
         </Box>
     );
 }
