8771e1137d677e9e9ecf528592ece0e28a0f16ea
MaikEight <mail@maik8.de>
2024-07-05 01:57:39 +0200

    🗃️ Add new migration down.sql to remove columns added in the up.sql

---
+++ src-modules/eam_commons/migrations/2024-07-04-230126_migrationV6/down.sql
@@ -0,0 +1,8 @@
+ALTER TABLE Character DROP COLUMN pc_stats;
+ALTER TABLE Character DROP COLUMN tex1;
+ALTER TABLE Character DROP COLUMN tex2;
+ALTER TABLE Character DROP COLUMN texture;
+ALTER TABLE Character DROP COLUMN xp_boosted;
+ALTER TABLE Character DROP COLUMN xp_timer;
+ALTER TABLE Character DROP COLUMN ld_timer;
+ALTER TABLE Character DROP COLUMN lt_timer;
