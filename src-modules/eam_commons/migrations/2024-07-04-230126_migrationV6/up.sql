16ee51a394f013df743329a42e6e1d3b5c9502e0
MaikEight <mail@maik8.de>
2024-07-05 01:56:54 +0200

    🗃️ Add new migration to add new fields for character stats and timers

---
+++ src-modules/eam_commons/migrations/2024-07-04-230126_migrationV6/up.sql
@@ -0,0 +1,9 @@
+Alter Table Character Add Column pc_stats Text;
+Alter Table Character Add Column tex1 Text;
+Alter Table Character Add Column tex2 Text;
+Alter Table Character Add Column texture Text;
+Alter Table Character Add Column xp_boosted Number;
+Alter Table Character Add Column xp_timer Number;
+Alter Table Character Add Column ld_timer Number;
+Alter Table Character Add Column lt_timer Number;
+
