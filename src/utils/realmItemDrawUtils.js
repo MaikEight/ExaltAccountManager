c192d3a50209c7b3a9f9aed6573c7c666762a87e
MaikEight <mail@maik8.de>
2024-07-05 02:04:22 +0200

    ✨ Add realmItemDrawUtils.js to support drawing a single item image

---
+++ src/utils/realmItemDrawUtils.js
@@ -0,0 +1,29 @@
+
+export const drawItem = (imgSrc, item, callback) => {
+    
+    const itemSize = 40;
+    const itemPadding = 5;
+    const canvasSize = itemSize + 2 * itemPadding;
+    
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+
+    canvas.width = canvasSize;
+    canvas.height = canvasSize;
+
+    const img = new Image();
+    img.src = imgSrc;
+
+    img.onload = () => {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(img, item[3], item[4], itemSize, itemSize, itemPadding, itemPadding, itemSize, itemSize);
+
+        // Convert canvas to image
+        const imageUrl = canvas.toDataURL('image/png');
+        callback(imageUrl);
+    };
+
+    img.onerror = (error) => {
+        console.error('Failed to load image', imgSrc, error);
+    };
+};
