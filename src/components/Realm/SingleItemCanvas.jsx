f7028e03f9fe245779eb6fdab91637871c60326e
MaikEight <mail@maik8.de>
2024-07-05 02:04:36 +0200

    ✨ Add SingleItemCanvas component for displaying a single item image

---
+++ src/components/Realm/SingleItemCanvas.jsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+import { drawItem } from "../../utils/realmItemDrawUtils";
+
+function SingleItemCanvas({ item }) {
+    const [imageData, setImageData] = useState(null);
+
+    useEffect(() => {
+        drawItem("renders.png", item, (imageUrl) => {
+            setImageData(imageUrl);
+        });
+    }, [item]);
+
+    return (<img src={imageData} width={50} height={50} id="itemImage" alt="item Image" />)
+}
+
+export default SingleItemCanvas;
