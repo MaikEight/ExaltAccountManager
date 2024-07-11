fb9e920c0253e0d39260b91f57b94e75ddefaaba
MaikEight <mail@maik8.de>
2024-07-11 21:37:01 +0200

    ✨ Add item filtering functionality to the VaultPeekerContext

--- src/contexts/VaultPeekerContext.jsx
+++ src/contexts/VaultPeekerContext.jsx
@@ -3,16 +3,39 @@ import useAccounts from "../hooks/useAccounts";
 import { invoke } from "@tauri-apps/api";
 import { extractRealmItemsFromCharListDatasets, formatAccountDataFromCharListDatasets } from "../utils/realmItemUtils";
 import ItemLocationPopper from "../components/Realm/ItemLocationPopper";
+import items from "../assets/constants";
 
 const VaultPeekerContext = createContext();
+
+const defaultFilter = {
+    search: '',
+};
 
 function VaultPeekerContextProvider({ children }) {
     const [selectedItem, setSelectedItem] = useState(null);
     const [popperPosition, setPopperPosition] = useState(null);
     const [totalItems, setTotalItems] = useState([]);
     const [accountsData, setAccountsData] = useState([]);
-    const [filteredTotals, setFilteredTotals] = useState([]);
+    const [filter, setFilter] = useState(defaultFilter);
+    const [filterItemsCallbacks, setFilterItemsCallbacks] = useState({});
+
     const { getAccountByEmail } = useAccounts();
+
+    const applyFilter = (itemIds) => {
+        let filteredItemIds = itemIds;
+        
+        // TEXT SEARCH
+        if (filter.search !== '') {
+            const searchTextLower = filter.search.toLowerCase();
+            filteredItemIds = itemIds.filter((itemId) => {
+                const item = items[itemId];
+                if (!item) return false;
+                return item[0].toLowerCase().includes(searchTextLower);
+            });
+        }
+        
+        return filteredItemIds;
+    };
 
     useEffect(() => {
         const loadItems = async () => {
@@ -33,15 +56,55 @@ function VaultPeekerContextProvider({ children }) {
         loadItems();
     }, []);
 
+    useEffect(() => {        
+        Object.keys(filterItemsCallbacks).forEach((id) => {
+            const { callback, items } = filterItemsCallbacks[id];
+            if (callback && items && items.length > 0) {
+                callback(applyFilter(items));
+            } 
+        });
+    }, [filter]);
+
+    const addItemFilterCallback = (id, callback, items) => {
+        setFilterItemsCallbacks((prev) => {
+            return {
+                ...prev,
+                [id]: { callback: callback, items: items },
+            };
+        });
+    };
+
+    const removeItemFilterCallback = (id) => {
+        setFilterItemsCallbacks((prev) => {
+            const newCallbacks = { ...prev };
+            delete newCallbacks[id];
+            return newCallbacks;
+        });
+    };
+
+    const changeFilter = (filterType, value) => {
+        setFilter((prev) => {
+            return {
+                ...prev,
+                [filterType]: value,
+            };
+        });
+    };    
+
     const contextValue = {
         totalItems,
         accountsData,
-        filteredTotals,
         getAccountByEmail,
         popperPosition,
+        filter,
+
         setPopperPosition,
         selectedItem,
         setSelectedItem,
+        changeFilter,
+
+        addItemFilterCallback,
+        removeItemFilterCallback,
     };
 
     return (
