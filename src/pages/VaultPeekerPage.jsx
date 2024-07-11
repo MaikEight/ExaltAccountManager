fe9dbaf53b06d1931d512ca77e858b3c475e1c6f
MaikEight <mail@maik8.de>
2024-07-11 21:39:01 +0200

    ✨ Add ItemFilterBox component for filtering items in VaultPeeker

--- src/pages/VaultPeekerPage.jsx
+++ src/pages/VaultPeekerPage.jsx
@@ -1,84 +1,17 @@
-import { Box, Typography } from "@mui/material";
-import items from "../assets/constants";
-import ComponentBox from "../components/ComponentBox";
-import FilterListOutlinedIcon from '@mui/icons-material/FilterListOutlined';
-import Searchbar from './../components/GridComponents/Searchbar';
-import SingleItemCanvas from "../components/Realm/SingleItemCanvas";
+import { Box } from "@mui/material";
 import { VaultPeekerContextProvider } from "../contexts/VaultPeekerContext";
 import TotalsView from "../components/VaultPeeker/TotalsView";
 import AccountsView from "../components/VaultPeeker/AccountsView";
 import { ItemCanvasContextProvider } from "../contexts/ItemCanvasContext";
+import ItemFilterBox from "../components/VaultPeeker/ItemFilterBox";
 
-function VaultPeekerPage() {
-
-    const searchChanged = (search) => {
-        if (search === '') {
-            setFilteredTotals(itemChunks[0]);
-            return;
-        }
-
-        // const filteredItems = itemIds.filter((id) => {
-        //     return items[id][0].toLowerCase().includes(search.toLowerCase());
-        // });
-        // setFilteredTotals(filteredItems);
-    };
+function VaultPeekerPage() {    
 
     return (
         <VaultPeekerContextProvider>
             <ItemCanvasContextProvider>
                 <Box sx={{ width: '100%', overflow: 'auto' }}>
-                    <ComponentBox
-                        title={
-                            <Box
-                                sx={{
-                                    display: 'flex',
-                                    justifyContent: 'space-between',
-                                    alignItems: 'center',
-                                    width: '100%',
-                                }}
-                            >
-                                <Typography
-                                    variant="h6"
-                                    sx={{
-                                        fontWeight: 600,
-                                        textAlign: 'center',
-                                    }}
-                                >
-                                    Filter
-                                </Typography>
-                                <Box
-                                //onClick={(event) => { event.stopPropagation(); }}
-                                >
-                                    <Typography
-                                        variant="h6"
-                                        sx={{
-                                            fontWeight: 600,
-                                            textAlign: 'center',
-                                        }}
-                                    >
-                                        EAM v4.2.0b Vault Peeker Beta #1
-                                    </Typography>
-                                    {
-                                        /* <Searchbar
-                                            onSearchChanged={searchChanged}
-                                        /> */
-                                    }
-                                </Box>
-                            </Box>
-                        }
-                        icon={<FilterListOutlinedIcon />}
-                        isCollapseable={true}
-                        defaultCollapsed={true}
-                        innerSx={{
-                            display: 'flex',
-                            flexDirection: 'row',
-                            gap: 1,
-                            flexWrap: 'wrap',
-                            justifyContent: 'center',
-                        }}
-                    >
-                        <Typography variant="h6">Filters are coming soon&#x2122;</Typography>
-                    </ComponentBox>
+                    <ItemFilterBox />
 
                     {/* Totals */}
                     <TotalsView />
