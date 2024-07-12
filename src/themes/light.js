959c320010454bd1e6e87eaf0e00ffd7a1ed3bf4
MaikEight <mail@maik8.de>
2024-07-12 22:37:05 +0200

    💄 Changed default style of the select component

--- src/themes/light.js
+++ src/themes/light.js
@@ -1,4 +1,4 @@
-import { createTheme, darken, lighten } from "@mui/material";
+import { alpha, createTheme, darken, lighten } from "@mui/material";
 import Zoom from '@mui/material/Zoom';
 
 export const lightTheme = createTheme({
@@ -39,8 +39,8 @@ export const lightTheme = createTheme({
     components: {
         MuiTooltip: {
             defaultProps: {
-                placement: "bottom",                
-                TransitionComponent:Zoom,
+                placement: "bottom",
+                TransitionComponent: Zoom,
                 slotProps: {
                     popper: {
                         modifiers: [
@@ -57,64 +57,77 @@ export const lightTheme = createTheme({
         },
         MuiDataGrid: {
             styleOverrides: {
-              root: {
-                '& .MuiDataGrid-columnHeader:focus, & .MuiDataGrid-cell': {
-                  outline: 'none',
-                  height: '42px',
-                  display: 'flex',
-                  alignItems: 'center',
+                root: {
+                    '& .MuiDataGrid-columnHeader:focus, & .MuiDataGrid-cell': {
+                        outline: 'none',
+                        height: '42px',
+                        display: 'flex',
+                        alignItems: 'center',
+                    },
+                    '& .MuiDataGrid-cell:focus-within, & .MuiDataGrid-cell:focus': {
+                        outline: 'none',
+                    },
+                    '& .css-1oudwrl::after': {
+                        height: 0,
+                    },
                 },
-                '& .MuiDataGrid-cell:focus-within, & .MuiDataGrid-cell:focus': {
-                  outline: 'none',
-                },
-                '& .css-1oudwrl::after': {
-                  height: 0,
-                },
-              },
             },
             defaultProps: {
-              sx: {
-                minHeight: '200px',
-                width: '100%',
-                border: 0,
-                '& [class^=MuiDataGrid]': { 
-                    border: 'none' 
+                sx: {
+                    minHeight: '200px',
+                    width: '100%',
+                    border: 0,
+                    '& [class^=MuiDataGrid]': {
+                        border: 'none'
+                    },
+                    '& .MuiDataGrid-columnHeaders': {
+                        backgroundColor: 'background.paperLight',
+                    },
+                    '& .MuiDataGrid-columnHeader': {
+                        backgroundColor: 'background.paperLight',
+                    },
+                    '& .MuiDataGrid-scrollbarFiller, .MuiDataGrid-scrollbarFiller--header': {
+                        backgroundColor: 'background.paperLight',
+                    },
+                    '& [class^=MuiDataGrid]::-webkit-scrollbar': {
+                        backgroundColor: 'background.paper',
+                    },
+                    '& [class^=MuiDataGrid]::-webkit-scrollbar-track': {
+                        borderRadius: theme => theme.shape.borderRadius,
+                    },
+                    '& [class^=MuiDataGrid]::-webkit-scrollbar-thumb': {
+                        backgroundColor: theme => theme.palette.mode === 'dark'
+                            ? theme.palette.background.default
+                            : darken(theme.palette.background.default, 0.15),
+                        border: theme => `3px solid ${theme.palette.background.paper}`,
+                        borderRadius: theme => theme.shape.borderRadius,
+                    },
+                    '& [class^=MuiDataGrid]::-webkit-scrollbar-thumb:hover': {
+                        backgroundColor: theme => theme.palette.mode === 'dark'
+                            ? lighten(theme.palette.background.default, 0.12)
+                            : darken(theme.palette.background.default, 0.2),
+                        border: theme => `3px solid ${theme.palette.background.paper}`,
+                        borderRadius: theme => theme.shape.borderRadius,
+                    },
+                    '& .css-tgsonj': {
+                        borderTop: 'none',
+                    }
                 },
-                '& .MuiDataGrid-columnHeaders': {
-                  backgroundColor: 'background.paperLight',
-                },
-                '& .MuiDataGrid-columnHeader': {
-                  backgroundColor: 'background.paperLight',
-                },
-                '& .MuiDataGrid-scrollbarFiller, .MuiDataGrid-scrollbarFiller--header': {
-                  backgroundColor: 'background.paperLight',
-                },
-                '& [class^=MuiDataGrid]::-webkit-scrollbar': {
-                  backgroundColor: 'background.paper',
-                },
-                '& [class^=MuiDataGrid]::-webkit-scrollbar-track': {
-                  borderRadius: theme => theme.shape.borderRadius,
-                },
-                '& [class^=MuiDataGrid]::-webkit-scrollbar-thumb': {
-                  backgroundColor: theme => theme.palette.mode === 'dark'
-                    ? theme.palette.background.default
-                    : darken(theme.palette.background.default, 0.15),
-                  border: theme => `3px solid ${theme.palette.background.paper}`,
-                  borderRadius: theme => theme.shape.borderRadius,
-                },
-                '& [class^=MuiDataGrid]::-webkit-scrollbar-thumb:hover': {
-                  backgroundColor: theme => theme.palette.mode === 'dark'
-                    ? lighten(theme.palette.background.default, 0.12)
-                    : darken(theme.palette.background.default, 0.2),
-                  border: theme => `3px solid ${theme.palette.background.paper}`,
-                  borderRadius: theme => theme.shape.borderRadius,
-                },
-                '& .css-tgsonj': {
-                  borderTop: 'none',
-                }
-              },
             },
-          },
+        },
+        MuiSelect: {
+            defaultProps: {
+                sx: {
+                    height: 39,                                
+                    backgroundColor: theme =>alpha(theme.palette.background.default, 0.75),
+                    '&:hover': {
+                        backgroundColor:theme => alpha(theme.palette.text.primary, 0.05),
+                    },
+                    transition: theme => theme.transitions.create('background-color'),
+                    borderRadius: theme => `${theme.shape.borderRadius}px`,
+                },
+            },
+        },
     },
     shadows: [
         "none",
