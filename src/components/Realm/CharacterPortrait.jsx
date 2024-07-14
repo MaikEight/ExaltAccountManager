1ff4d325289ef0d919702e2e1f6248f1782f980c
MaikEight <mail@maik8.de>
2024-07-14 23:18:58 +0200

    💄 Refactor CharacterPortrait component to include a Skeleton loading during portrait generation

--- src/components/Realm/CharacterPortrait.jsx
+++ src/components/Realm/CharacterPortrait.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { portrait } from "../../utils/portraitUtils";
+import { Skeleton } from "@mui/material";
 
 function CharacterPortrait({type, skin, tex1, tex2, adjust}) {
     const [url, setUrl] = useState('');
@@ -8,6 +9,13 @@ function CharacterPortrait({type, skin, tex1, tex2, adjust}) {
         const _url = portrait(type, skin, tex1, tex2, adjust);
         setUrl(_url);
     }, [type, skin, tex1, tex2, adjust]);
+
+    if(!url) {
+        return (
+            <Skeleton sx={{mx: '-1px'}} variant="rounded" width={45} height={60} />
+        );
+    }
+
 
     return (
         <img src={url} width={34} height={34} alt="Character Portrait" />
