c7b7d206ebc7cc48ba98a4388147c25955875ea3
MaikEight <mail@maik8.de>
2024-07-21 21:05:05 +0200

    ✨ Add null return statement during loading of the SingleItemCanvas image

--- src/components/Realm/SingleItemCanvas.jsx
+++ src/components/Realm/SingleItemCanvas.jsx
@@ -10,6 +10,7 @@ function SingleItemCanvas({ item }) {
         });
     }, [item]);
 
+    if (!imageData) return null;
     return (<img src={imageData} width={50} height={50} id="itemImage" alt="item Image" />)
 }
 
