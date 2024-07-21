f59677e9d3f9db7d52aa50fdfd9936227a644446
MaikEight <mail@maik8.de>
2024-07-21 21:08:08 +0200

    💄 Refactor AccountsView component to use Fragment instead of Box

--- src/components/VaultPeeker/AccountsView.jsx
+++ src/components/VaultPeeker/AccountsView.jsx
@@ -2,22 +2,23 @@ import { Box } from "@mui/material";
 import AccountView from "./AccountView";
 import useVaultPeeker from "../../hooks/useVaultPeeker";
 import ComponentBox from "../ComponentBox";
+import { Fragment } from "react";
 
 function AccountsView() {
     const { accountsData } = useVaultPeeker();
 
-    return(
-        <Box>
+    return (
+        <Fragment>
             {
                 accountsData ?
-                accountsData.map((accountData, index) => {
-                    return <AccountView key={index} account={accountData} />
-                })
-                :
-                <ComponentBox title='No Accounts Found' isCollapseable={true} />
+                    accountsData.map((accountData, index) => {
+                        return <AccountView key={index} account={accountData} />
+                    })
+                    :
+                    <ComponentBox title='No Accounts Found' isCollapseable={true} sx={{ mx: 0 }} />
                 //TODO: Add no accounts found message
             }
-        </Box>
+        </Fragment>
     );
 }
 
