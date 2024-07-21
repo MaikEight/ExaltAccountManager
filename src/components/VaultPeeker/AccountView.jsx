bb53e1a0c06280a5c319e54c1e59e0e2eb41ff01
MaikEight <mail@maik8.de>
2024-07-21 21:08:46 +0200

    🎨 Add item filtering to AccountView component

--- src/components/VaultPeeker/AccountView.jsx
+++ src/components/VaultPeeker/AccountView.jsx
@@ -4,15 +4,71 @@ import Character from "../Realm/Character";
 import ItemCanvas from "../Realm/ItemCanvas";
 import items from "../../assets/constants";
 import useVaultPeeker from "../../hooks/useVaultPeeker";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
+import GroupUI from "../GridComponents/GroupUI";
+import useUserSettings from "../../hooks/useUserSettings";
 
 function AccountView({ account }) {
-    const { totalItems } = useVaultPeeker();
+    const [accountItemIds, setAccountItemIds] = useState([]);
+    const [filteredAccountItemIds, setfilteredAccountItemIds] = useState([]);
+    const { totalItems, filter, addItemFilterCallback, removeItemFilterCallback } = useVaultPeeker();
+    const collapsedFileds = useUserSettings().getByKeyAndSubKey('vaultPeeker', 'collapsedFileds');
+    
+    const isDefaultCollapsed = !account ? false : collapsedFileds !== undefined ? collapsedFileds.accounts?.includes(account?.email) : false;
+
+    useEffect(() => {
+        if (account && account.account) {
+            const itemIds = [];
+            itemIds.push(...account.account.vault.itemIds);
+            itemIds.push(...account.account.gifts.itemIds);
+            itemIds.push(...account.account.material_storage.itemIds);
+            itemIds.push(...account.account.temporary_gifts.itemIds);
+            itemIds.push(...account.account.potions.itemIds);
+
+            account.character.forEach((char) => {
+                itemIds.push(...char.equipment);
+            });
+            const ids = [...new Set(itemIds)];
+            setAccountItemIds(ids);
+            setfilteredAccountItemIds(ids);
+        }
+    }, [account]);
+
+    useEffect(() => {
+        const id = `${account.email}_ACC`;
+        addItemFilterCallback(id, (itemIds) => { setfilteredAccountItemIds(itemIds); }, accountItemIds);
+
+        return () => {
+            removeItemFilterCallback(id);
+        };
+    }, [accountItemIds]);
+
+    const boxTitle = useMemo(() => {
+        return (
+            <>
+                {account.group && <GroupUI group={account.group} />}
+                <Typography
+                    variant="h6"
+                    sx={{
+                        fontWeight: 600,
+                        textAlign: 'center',
+                    }}
+                >
+                    {account.name ? account.name : account.email}
+                </Typography>
+            </>
+        );
+    }, [account]);
+
+    if (!account || !filteredAccountItemIds || filteredAccountItemIds.length === 0) {
+        return null;
+    }
 
     return (
         <ComponentBox
-            title={account.name ? account.name : account.email}
+            title={boxTitle}
             isCollapseable={true}
+            defaultCollapsed={isDefaultCollapsed}
             sx={{
                 mx: 0,
             }}
@@ -21,6 +77,7 @@ function AccountView({ account }) {
                 flexDirection: 'coulmn',
                 gap: 1
             }}
+
         >
             {/* Characters */}
             <Box
@@ -33,6 +90,7 @@ function AccountView({ account }) {
             >
                 {
                     account.character &&
+                    !(filter.characterType.enabled && filter.characterType.value === 3) &&
                     account.character.map((char, index) => {
                         return (
                             <Character charIdentifier={`${account.email}_${char.char_id}_${index}`} key={index} character={char} />
@@ -44,15 +102,15 @@ function AccountView({ account }) {
                 account && account.account &&
                 <Box>
                     {/* Vault */}
-                    <StorageView canvasIdentifier={account.email + "_Vault"} title={<StorageViewTitle title="Vault" image="realm/vault_portal.png" sx={{gap: 0.5}}/>} itemIds={account.account.vault.itemIds} totals={totalItems?.totals} />
+                    <StorageView canvasIdentifier={account.email + "_Vault"} title={<StorageViewTitle title="Vault" image="realm/vault_portal.png" sx={{ gap: 0.5 }} />} itemIds={account.account.vault.itemIds} totals={totalItems?.totals} />
                     {/* Gift Chest */}
-                    <StorageView canvasIdentifier={account.email + "_Gift"} title={<StorageViewTitle title="Gift Chest" image="realm/gift_chest.png"/>} itemIds={account.account.gifts.itemIds} totals={totalItems?.totals} />
+                    <StorageView canvasIdentifier={account.email + "_Gift"} title={<StorageViewTitle title="Gift Chest" image="realm/gift_chest.png" />} itemIds={account.account.gifts.itemIds} totals={totalItems?.totals} />
                     {/* Trade Chest */}
-                    <StorageView canvasIdentifier={account.email + "_Trade"} title={<StorageViewTitle title="Material Storage" image="realm/material_storage.png"/>} itemIds={account.account.material_storage.itemIds} totals={totalItems?.totals} />
+                    <StorageView canvasIdentifier={account.email + "_Trade"} title={<StorageViewTitle title="Material Storage" image="realm/material_storage.png" />} itemIds={account.account.material_storage.itemIds} totals={totalItems?.totals} />
                     {/* Temporary Gifts */}
-                    <StorageView canvasIdentifier={account.email + "_Temp"} title={<StorageViewTitle title="Temporary Gifts" image="realm/chest.png"/>} itemIds={account.account.temporary_gifts.itemIds} totals={totalItems?.totals} />
+                    <StorageView canvasIdentifier={account.email + "_Temp"} title={<StorageViewTitle title="Temporary Gifts" image="realm/chest.png" />} itemIds={account.account.temporary_gifts.itemIds} totals={totalItems?.totals} />
                     {/* Potion Storage */}
-                    <StorageView canvasIdentifier={account.email + "_Potion"} title={<StorageViewTitle title="Potion Storage" image="realm/potion_storage_small.png"/>} itemIds={account.account.potions.itemIds} totals={totalItems?.totals} />
+                    <StorageView canvasIdentifier={account.email + "_Potion"} title={<StorageViewTitle title="Potion Storage" image="realm/potion_storage_small.png" />} itemIds={account.account.potions.itemIds} totals={totalItems?.totals} />
                 </Box>
             }
         </ComponentBox>
@@ -74,7 +132,7 @@ function StorageView({ canvasIdentifier, title, itemIds, totals }) {
         };
     }, [itemIds]);
 
-    if(!filteredItemIds || filteredItemIds.length === 0) return null;
+    if (!filteredItemIds || filteredItemIds.length === 0) return null;
 
     return (
         <Box
