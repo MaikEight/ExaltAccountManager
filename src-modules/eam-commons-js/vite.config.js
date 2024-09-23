7500338bd403b35b1d3db01b71f9a357d4b5aaa3
MaikEight <mail@maik8.de>
2024-09-23 02:26:30 +0200

    🔧 Improve the vite.config.js file

--- src-modules/eam-commons-js/vite.config.js
+++ src-modules/eam-commons-js/vite.config.js
@@ -1,18 +1,22 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  plugins: [react()],
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/index.js'),
+      entry: resolve(__dirname, 'src/index.jsx'),
       name: 'EamCommonsJs',
       fileName: (format) => `eam-commons-js.${format}.js`,
     },
     rollupOptions: {
-      // Ensure to externalize dependencies that shouldn't be bundled
-      external: [],
+      external: ['react', 'react-dom'],
       output: {
-        globals: {},
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
       },
     },
   },
