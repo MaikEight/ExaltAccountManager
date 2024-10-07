ef8593e9d1e8a07d96c787a0c5b0d2add122f480
MaikEight <mail@maik8.de>
2024-10-07 03:59:37 +0200

    🎨 Refactor import statement in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles.css";
 import { UserSettingsProvider } from "eam-commons-js";
-
+import "eam-commons-js/src/themes/styles.css";
 
 ReactDOM.createRoot(document.getElementById("root"))
 .render(
