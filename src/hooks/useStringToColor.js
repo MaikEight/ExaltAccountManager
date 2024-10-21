ed09a69114bdafcc08a3ca52294ffc80522c100d
MaikEight <mail@maik8.de>
2024-10-22 00:23:11 +0200

    🎨 Refactor useStringToColor to import useColorList from 'eam-commons-js'

--- src/hooks/useStringToColor.js
+++ src/hooks/useStringToColor.js
@@ -1,12 +1,12 @@
 import hashString from 'hash-string';
-import useColorList from './useColorList';
+import { useColorList } from 'eam-commons-js';
 
-const useStringToColor = (inputString) => {    
+const useStringToColor = (inputString) => {
     const colorList = useColorList();
     const colorMap = { Default: 0 };
 
-    if(typeof(colorMap[inputString]) !== 'undefined') return colorList[colorMap[inputString]];
-    
+    if (typeof (colorMap[inputString]) !== 'undefined') return colorList[colorMap[inputString]];
+
     const generateColorIndex = (name) => {
         const hash = hashString(name);
         const hashInteger = Math.abs(parseInt(hash, 16));
