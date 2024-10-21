7d8a3dbd53b608e3a0220b50840ae00680c7352d
MaikEight <mail@maik8.de>
2024-10-22 00:11:46 +0200

    ✨ Add Auth0Provider to main.jsx for authentication

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { UserSettingsProvider } from "eam-commons-js";
+import { Auth0Provider } from "@auth0/auth0-react";
 import "eam-commons-js/src/themes/styles.css";
 
 ReactDOM.createRoot(document.getElementById("root"))
-.render(
-  <div
-    style={{
-      borderRadius: '10px',
-      overflow: 'hidden',
-    }}
-  >
-    <UserSettingsProvider>
-      <App />
-    </UserSettingsProvider>
-  </div>
-);
+    .render(
+        <div>
+            <UserSettingsProvider>
+                <Auth0Provider                    
+                    domain="https://login.exaltaccountmanager.com"
+                    clientId="o1W1coVQMV9qrIg4G2SmZJbz1G5vRCpZ"
+                    useRefreshTokens={true}
+                    useRefreshTokensFallback={false}
+                    authorizationParams={{
+                        redirect_uri: "eam:profile/callback"
+                    }}
+                >
+                    <App />
+                </Auth0Provider>
+            </UserSettingsProvider>
+        </div>
+    );
