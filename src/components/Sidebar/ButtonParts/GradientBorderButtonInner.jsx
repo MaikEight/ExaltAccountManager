0c8f76232cf5c66791a956763fde54d8d567054a
MaikEight <mail@maik8.de>
2024-10-22 00:19:30 +0200

    💄 Refactor borderRadius in GradientBorderButtonInner component

--- src/components/Sidebar/ButtonParts/GradientBorderButtonInner.jsx
+++ src/components/Sidebar/ButtonParts/GradientBorderButtonInner.jsx
@@ -1,17 +1,18 @@
 import styled from "styled-components";
 
-const GradientBorderButtonInner = styled('div')(({ theme, selected }) => ({
+const GradientBorderButtonInner = styled('div')(({ theme, selected, sx }) => ({
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
     padding: '6px',
     gap: '8px',
-    borderRadius: '30px',
+    borderRadius: '28px',
     background: selected ? 'linear-gradient(98deg, rgb(198, 167, 254), rgba(145, 85, 253) 94%)' : theme.palette.background.default,
     transition: theme.transitions.create('background-color'),
     '&:hover': {
         background: 'linear-gradient(98deg, rgb(198, 167, 254), rgba(145, 85, 253) 94%)',
-    }
+    },
+    ...sx
 }));
 
 export default GradientBorderButtonInner;
