f80bb94b491703228e60c7dad7f7ca623b4c878b
MaikEight <mail@maik8.de>
2024-10-31 01:32:31 +0100

    ✨ Replace Auth0Provider with UserLoginProvider in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { UserSettingsProvider } from "eam-commons-js";
-import { Auth0Provider } from "@auth0/auth0-react";
+import { UserSettingsProvider, UserLoginProvider } from "eam-commons-js";
+
 import "eam-commons-js/src/themes/styles.css";
 
 ReactDOM.createRoot(document.getElementById("root"))
     .render(
         <div>
             <UserSettingsProvider>
-                <Auth0Provider                    
-                    domain="https://login.exaltaccountmanager.com"
-                    clientId="o1W1coVQMV9qrIg4G2SmZJbz1G5vRCpZ"
-                    useRefreshTokens={true}
-                    useRefreshTokensFallback={false}
-                    authorizationParams={{
-                        redirect_uri: "eam:profile/callback"
-                    }}
-                >
+                <UserLoginProvider>
                     <App />
-                </Auth0Provider>
+                </UserLoginProvider>
             </UserSettingsProvider>
         </div>
     );
