10b7f13bcb44ac4eddc3952c89fdf76aaa4671b1
MaikEight <mail@maik8.de>
2024-10-31 01:35:15 +0100

    ✨ Enhance SidebarLoginBox with dynamic greeting and profile image fetching

--- src/components/Sidebar/SidebarLoginBox.jsx
+++ src/components/Sidebar/SidebarLoginBox.jsx
@@ -1,5 +1,5 @@
 import { Box } from '@mui/material';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import GradientBorderButton from './ButtonParts/GradientBorderButton';
 import GradientBorderButtonInner from './ButtonParts/GradientBorderButtonInner';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
@@ -7,12 +7,76 @@ import { useTheme } from 'styled-components';
 import StyledButton from '../StyledButton';
 import { Typography } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
-import { useAuth0 } from "@auth0/auth0-react";
+import { useUserLogin, getProfileImage } from "eam-commons-js";
+
+const GREETING_TEXTS = [
+    'Hello, ',
+    'Welcome back, ',
+    'Hi, ',
+    'Greetings, ',
+    'Howdy, ',
+    'Hey, ',
+    'Good day, ',
+    'Salutations, ',
+    'Missed ya, ',
+    'You\'re back, ',
+    'Nice to see you, ',
+    'Well, hello  ',
+    'Hello again, ',
+    'Hey there, ',
+    'You\'re here, ',
+    'Welcome, ',
+    'Good to see you, ',
+    'Hi there, ',
+    'Back again,',
+    'Hello once more, ',
+    'The legend returns, ',
+    'Party time, ',
+    'There you are, ',
+];
 
 function SidebarLoginBox() {
     const theme = useTheme();
     const navigate = useNavigate();
-    const { user, isAuthenticated } = useAuth0();
+    const { user, isAuthenticated } = useUserLogin();
+    const [greetingText, setGreetingText] = useState(null);
+    const [profileImage, setProfileImage] = useState(null);
+
+    const selected = window.location.pathname.startsWith('/profile');
+
+    useEffect(() => {
+        const fetchImageData = async () => {
+            if (!user) {
+                return;
+            }
+
+            const storeImage = sessionStorage.getItem('profileImage');
+            if (storeImage !== null) {
+                setProfileImage(storeImage);
+                return;
+            }
+
+            const imageData = await getProfileImage(user.picture);
+            setProfileImage(`data:image/jpeg;base64,${imageData}`);
+            sessionStorage.setItem('profileImage', `data:image/jpeg;base64,${imageData}`);
+        }
+        fetchImageData();
+        if (sessionStorage.getItem('greetingText')) {
+            setGreetingText(sessionStorage.getItem('greetingText'));
+        } else {
+            const greeting = GREETING_TEXTS[Math.floor(Math.random() * GREETING_TEXTS.length)];
+            setGreetingText(greeting);
+            sessionStorage.setItem('greetingText', greeting);
+        }
+
+        const timeoutId = setTimeout(() => {
+            setGreetingText(null);
+        }, 15_000);
+
+        return () => {
+            clearTimeout(timeoutId);
+        }
+    }, [user]);
 
     return (
         <Box
@@ -23,19 +87,28 @@ function SidebarLoginBox() {
             }}
         >
             <GradientBorderButton
+                selected={!greetingText && selected}
                 sx={{
                     transition: theme.transitions.create('background-color'),
-                    ...(isAuthenticated && {
+                    ...(isAuthenticated && user && greetingText && {
                         cursor: 'default',
                         borderRadius: `${theme.shape.borderRadius}px`,
                     }),
                 }}
+                onClick={
+                    !greetingText ?
+                        () => {
+                            navigate('/profile');
+                        }
+                        : null
+                }
             >
                 <GradientBorderButtonInner
+                    selected={!greetingText && selected}
                     sx={{
                         transition: theme.transitions.create('background-color'),
 
-                        ...(isAuthenticated && {
+                        ...(isAuthenticated && user && greetingText && {
                             borderRadius: `${theme.shape.borderRadius - 2}px`,
                             backgroundColor: theme.palette.background.default,
                             '&:hover': {
@@ -45,7 +118,11 @@ function SidebarLoginBox() {
                     }}
                 >
                     {
-                        isAuthenticated ?
+                        isAuthenticated &&
+                            user &&
+                            greetingText
+                            ?
+
                             <Box
                                 sx={{
                                     display: 'flex',
@@ -67,17 +144,12 @@ function SidebarLoginBox() {
                                         gap: 0.25,
                                     }}
                                 >
-                                    {
-                                        user &&
-                                        <>
-                                            <Typography variant="body1">
-                                                Hello <strong>{user.name}</strong>!
-                                            </Typography>
-                                            <Typography variant="body2">
-                                                EAM Default User
-                                            </Typography>
-                                        </>
-                                    }
+                                    <Typography
+                                        textAlign={'center'}
+                                        variant="body1"
+                                    >
+                                        {greetingText} <strong>{user.name}</strong>!
+                                    </Typography>
                                     <Box
                                         sx={{
                                             display: 'flex',
@@ -100,7 +172,6 @@ function SidebarLoginBox() {
                                         </StyledButton>
                                     </Box>
                                 </Box>
-
                             </Box>
                             :
                             <Box
@@ -112,14 +183,10 @@ function SidebarLoginBox() {
                                     height: '100%',
                                     gap: 1,
                                 }}
-
-                                onClick={() => {
-                                    navigate('/profile');
-                                }}
                             >
-                                <AccountCircleIcon />
+                                {user && profileImage ? <img width={20} src={profileImage} style={{ borderRadius: `50%` }} /> : <AccountCircleIcon />}
                                 <Typography variant="body1">
-                                    EAM Profile
+                                    {user ? 'View Profile' : 'EAM Profile'}
                                 </Typography>
                             </Box>
                     }
