feaeda81ee8436ca45495640c83235c49186c182
MaikEight <mail@maik8.de>
2024-11-10 03:51:52 +0100

    ✨ Add UserLoginProvider to main.jsx for enhanced user authentication

--- src-modules/daily-auto-login/src/main.jsx
+++ src-modules/daily-auto-login/src/main.jsx
@@ -1,19 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { UserSettingsProvider } from 'eam-commons-js';
+import { UserSettingsProvider, UserLoginProvider } from "eam-commons-js";
 import "eam-commons-js/src/themes/styles.css";
 
 ReactDOM.createRoot(document.getElementById("root"))
-.render(
-  <div
-    style={{
-      borderRadius: '10px',
-      overflow: 'hidden',
-    }}
-  >
-    <UserSettingsProvider>
-      <App />
-    </UserSettingsProvider>
-  </div>
-);
+  .render(
+    <div>
+      <UserSettingsProvider>
+        <UserLoginProvider>
+          <App />
+        </UserLoginProvider>
+      </UserSettingsProvider>
+    </div>
+  );
