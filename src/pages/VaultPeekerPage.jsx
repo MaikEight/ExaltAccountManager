e99ec5b0d1805c14b4e5a6ce2419982a909ff875
MaikEight <mail@maik8.de>
2024-11-12 02:46:23 +0100

    ✨ Conditionally render ItemCanvasContextProvider based on window.portraitReady state

--- src/pages/VaultPeekerPage.jsx
+++ src/pages/VaultPeekerPage.jsx
@@ -10,54 +10,58 @@ function VaultPeekerPage() {
 
     return (
         <VaultPeekerContextProvider>
-            <ItemCanvasContextProvider>
-                <Box
-                    sx={{
-                        width: '100%',
-                        display: 'flex',
-                        flexDirection: 'column',
-                    }}
-                >
-                    <Box sx={{ width: '100%', position: 'relative', overflow: 'auto' }}>
-                        <Box
-                            id="filter-box-root"
-                            sx={{
-                                position: 'sticky',
-                                top: 0,
-                                zIndex: 10,
-                                backgroundColor: 'transparent',
-                            }}
-                        >
+
+            {
+                window.portraitReady &&
+                <ItemCanvasContextProvider>
+                    <Box
+                        sx={{
+                            width: '100%',
+                            display: 'flex',
+                            flexDirection: 'column',
+                        }}
+                    >
+                        <Box sx={{ width: '100%', position: 'relative', overflow: 'auto' }}>
                             <Box
+                                id="filter-box-root"
                                 sx={{
-                                    backgroundColor: 'background.default',
+                                    position: 'sticky',
+                                    top: 0,
+                                    zIndex: 10,
+                                    backgroundColor: 'transparent',
                                 }}
                             >
-                                <ItemFilterBox />
+                                <Box
+                                    sx={{
+                                        backgroundColor: 'background.default',
+                                    }}
+                                >
+                                    <ItemFilterBox />
+                                </Box>
+                                <InverseBorderRadiusSpacer
+                                    sx={{
+                                        mx: 2,
+                                    }}
+                                />
                             </Box>
-                            <InverseBorderRadiusSpacer
+                            <Box
+                                id="vault-peeker-content-root"
                                 sx={{
+                                    borderRadius: '9px',
                                     mx: 2,
+                                    mt: 0,
                                 }}
-                            />
-                        </Box>
-                        <Box
-                            id="vault-peeker-content-root"
-                            sx={{
-                                borderRadius: '9px',
-                                mx: 2,
-                                mt: 0,
-                            }}
-                        >
-                            {/* Totals */}
-                            <TotalsView />
+                            >
+                                {/* Totals */}
+                                <TotalsView />
 
-                            {/* Accounts */}
-                            <AccountsView />
+                                {/* Accounts */}
+                                <AccountsView />
+                            </Box>
                         </Box>
                     </Box>
-                </Box>
-            </ItemCanvasContextProvider>
+                </ItemCanvasContextProvider>
+            }
         </VaultPeekerContextProvider>
     );
 }
