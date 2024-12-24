88e6990a2937950f501e4bc2fe3b88b8bc5fb99d
MaikEight <mail@maik8.de>
2024-12-24 09:25:30 +0100

    🔧 Add HWID file constants and save file path functions

--- t-src-modules/eam-commons-js/constants.js
+++ t-src-modules/eam-commons-js/constants.js
@@ -1,4 +1,5 @@
 export const ROTMG_BASE_URL = 'https://www.realmofthemadgod.com';
+import { invoke } from '@tauri-apps/api/core';
 
 const updateBaseUrls = [
     "https://www.realmofthemadgod.com/app/init?platform=standalonewindows64&key=9KnJFxtTvLu2frXv",
@@ -22,6 +23,10 @@ export function UPDATE_URLS(index, values) {
             return null;
     }
 }
+
+export const HWID_FILE_NAME = 'EAM.HWID';
+export const SAVE_FILE_PATH = async () => await invoke('get_save_file_path');
+export const HWID_FILE_PATH = async () => await invoke('combine_paths', { path1: await SAVE_FILE_PATH(), path2: HWID_FILE_NAME });
 
 export const AUTH0_CLIENT_ID = 'o1W1coVQMV9qrIg4G2SmZJbz1G5vRCpZ';
 export const AUTH0_DOMAIN = 'login.exaltaccountmanager.com';
