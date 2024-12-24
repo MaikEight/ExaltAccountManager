fba598794cf6055592d8de143ce2de8a218d2fef
MaikEight <mail@maik8.de>
2024-12-24 09:25:38 +0100

    ✨ Add custom hook for HWID retrieval and management

---
+++ t-src-modules/eam-commons-js/src/hooks/useHWID.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+import { HWID_FILE_PATH } from "../../constants";
+import { readFileUTF8 } from "../utils/readFileUtil";
+import { invoke } from "@tauri-apps/api/core";
+
+function useHWID() {
+    const [hwid, setHwid] = useState(null);
+
+    useEffect(() => {
+        const readHwidFile = async () => {
+            const path = await HWID_FILE_PATH();
+            const _hwid = await readFileUTF8(path, false);
+            if (_hwid === null || _hwid === undefined || _hwid === '') {
+                const id = await invoke('get_device_unique_identifier');
+                setHwid(id);
+
+                return;
+            }
+            setHwid(_hwid);
+        };
+        
+        readHwidFile()
+            .catch(error => console.error(error));
+    }, []);
+
+    return hwid;
+}
+
+export { useHWID };
