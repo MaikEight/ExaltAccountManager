8b863306b645145a7e8ca21702ac10df939621fe
MaikEight <mail@maik8.de>
2025-04-13 06:44:22 +0200

    💫 Add doTransition prop to SingleItemCanvas for enhanced image scaling animation

--- src/components/Realm/SingleItemCanvas.jsx
+++ src/components/Realm/SingleItemCanvas.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { drawItem } from "../../utils/realmItemDrawUtils";
 
-function SingleItemCanvas({ item }) {
+function SingleItemCanvas({ item, doTransition = false }) {
     const [imageData, setImageData] = useState(null);
 
     useEffect(() => {
@@ -10,8 +10,23 @@ function SingleItemCanvas({ item }) {
         });
     }, [item]);
 
-    if (!imageData) return null;
-    return (<img src={imageData} width={50} height={50} id="itemImage" alt="item Image" />)
+    if(!imageData && !doTransition) {
+        return null;        
+    }
+
+    return (
+        <img
+            src={imageData}
+            width={50}
+            height={50}
+            id="itemImage"
+            alt="item Image"
+            style={{
+                scale: imageData ? 1 : 0,
+                transition: doTransition ? "scale 0.2s ease-in-out" : "none",
+            }}
+        />
+    )
 }
 
 export default SingleItemCanvas;
