dd2496af58fff696e037be7587a1f131666bc7cb
MaikEight <mail@maik8.de>
2025-05-01 05:05:46 +0200

    ✨ Enhance StyledButton component to support loading state with LinearProgress

--- src/components/StyledButton.jsx
+++ src/components/StyledButton.jsx
@@ -1,21 +1,23 @@
 import { useTheme } from "@emotion/react";
-import { Button } from "@mui/material";
+import { Button, LinearProgress } from "@mui/material";
 
-function StyledButton({ children, fullWidth, ...props }) {
+function StyledButton({ children, loading, ...props }) {
     const theme = useTheme();
     return (
         <Button
             color="primary"
-            variant="contained"
-            fullWidth={fullWidth}
+            variant="contained"            
             {...props}
             sx={{
                 borderRadius: `${theme.shape.borderRadius}px`,
                 boxShadow: theme.palette.mode === 'dark' ? 'rgba(19, 17, 32, 0.42) 0px 4px 8px -4px;' : 'rgba(58, 53, 65, 0.42) 0px 4px 8px -4px;',
                 transition: theme.transitions.create('background-color'),
+                overflow: 'hidden',
                 ...props.sx,
             }}
+            disabled={loading || props.disabled}
         >
+            {loading && <LinearProgress sx={{ position: 'absolute', top: 0, left: 0, right: 0 }} />}
             {children}
         </Button>
     );
