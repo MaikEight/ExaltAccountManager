1831ad2dd4c25b460983f24bd99c2eec86546ba9
MaikEight <mail@maik8.de>
2025-06-09 18:24:36 +0200

    ♻️ Refactor AccountsView: optimize rendering with useMemo and memoization

--- src/components/VaultPeeker/AccountsView.jsx
+++ src/components/VaultPeeker/AccountsView.jsx
@@ -1,23 +1,27 @@
 import AccountView from "./AccountView";
 import useVaultPeeker from "../../hooks/useVaultPeeker";
 import ComponentBox from "../ComponentBox";
-import { Fragment } from "react";
+import { Fragment, memo, useMemo } from "react";
 
 function AccountsView() {
     const { accountsData } = useVaultPeeker();
 
-    return (
-        <Fragment>
-            {
-                accountsData ?
-                    accountsData.map((accountData, index) => {
-                        return <AccountView key={index} account={accountData} />
-                    })
-                    :
-                    <ComponentBox title='No Accounts Found' isCollapseable={true} sx={{ mx: 0 }} />
-            }
-        </Fragment>
-    );
+    const accountsComponents = useMemo(() => {
+        return (
+            <Fragment>
+                {
+                    accountsData ?
+                        accountsData.map((accountData, index) => {
+                            return <AccountView key={index} account={accountData} />
+                        })
+                        :
+                        <ComponentBox title='No Accounts Found' isCollapseable={true} sx={{ mx: 0 }} />
+                }
+            </Fragment>
+        );
+    }, [accountsData]);
+
+    return accountsComponents;
 }
 
-export default AccountsView;
+export default memo(AccountsView);
