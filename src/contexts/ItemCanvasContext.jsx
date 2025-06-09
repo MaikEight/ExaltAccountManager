68d442b5332139306aa03edcfd72ffc29b2d29ce
MaikEight <mail@maik8.de>
2025-06-09 18:25:42 +0200

    🎨 Enhance ItemCanvasContext: add setHoveredConvasData function for improved hover state management

--- src/contexts/ItemCanvasContext.jsx
+++ src/contexts/ItemCanvasContext.jsx
@@ -5,10 +5,25 @@ const ItemCanvasContext = createContext();
 
 function ItemCanvasContextProvider({ children }) {
     const [hoveredConvasId, setHoveredConvasId] = useState(null);
+    const [hoveredConvasData, setHoveredConvasData] = useState(null);
 
     const contextValue = {
         hoveredConvasId: hoveredConvasId,
         setHoveredConvasId: (id) => setHoveredConvasId(id),
+        setHoveredConvasData: (data) => {
+            if (!data && hoveredConvasData) {
+                hoveredConvasData.callback?.();
+                setHoveredConvasData(null);
+                return;
+            }
+
+            if (data?.canvasId === hoveredConvasData?.canvasId) {
+                return;
+            }
+
+            hoveredConvasData?.callback?.();
+            setHoveredConvasData(data);
+        },
     };
 
     return (
