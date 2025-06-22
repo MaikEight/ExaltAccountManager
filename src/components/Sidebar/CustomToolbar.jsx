639b6af8d2e7a50ab942c227d4e886cbf33d7b35
MaikEight <mail@maik8.de>
2025-06-22 02:05:57 +0200

    ✨ Add API cooldown and remaining limits tracking in CustomToolbar for better API usage information display

--- src/components/Sidebar/CustomToolbar.jsx
+++ src/components/Sidebar/CustomToolbar.jsx
@@ -1,12 +1,98 @@
-import { Box, Button, ButtonGroup } from "@mui/material";
-import { getCurrentWindow  } from '@tauri-apps/api/window'
+import { Box, Button, ButtonGroup, Chip, Tooltip, Typography } from "@mui/material";
+import { getCurrentWindow } from '@tauri-apps/api/window'
 import MinimizeIcon from '@mui/icons-material/Minimize';
 import CloseIcon from '@mui/icons-material/Close';
 import CropSquareIcon from '@mui/icons-material/CropSquare';
 import { useTheme } from "@emotion/react";
+import { listen } from '@tauri-apps/api/event';
+import { useEffect, useState } from "react";
+import VpnLockOutlinedIcon from '@mui/icons-material/VpnLockOutlined';
+import FlagCircleOutlinedIcon from '@mui/icons-material/FlagCircleOutlined';
 
 function CustomToolbar(props) {
     const theme = useTheme();
+    const [hasGlobalApiCooldown, setHasGlobalApiCooldown] = useState(false);
+    const [apiRemainingLimits, setApiRemainingLimits] = useState(new Map([
+        ['account/verify', 30],
+        ['char/list', 5],
+        ['account/register', 5],
+    ]));
+
+    useEffect(() => {
+        let unlistenGlobalApiCooldown;
+        let unlistenApiRemainingLimits;
+
+        const registerListeners = async () => {
+            unlistenGlobalApiCooldown = await listen('api-cooldown', (event) => {
+                setHasGlobalApiCooldown(event.payload);
+            });
+            unlistenApiRemainingLimits = await listen('api-remaining-changed', (event) => {
+                const data = {
+                    api: event.payload[0],
+                    remaining: event.payload[1],
+                    limit: event.payload[2],
+                };
+                
+                setApiRemainingLimits((prev) => {
+                    const newLimits = new Map(prev);
+                    newLimits.set(data.api, data.remaining);
+                    return newLimits;
+                });
+            });
+        };
+        registerListeners()
+            .catch(console.error);
+
+        return () => {
+            unlistenGlobalApiCooldown?.();
+            unlistenApiRemainingLimits?.();
+        }
+    }, []);
+
+    const getEmptyApiLimitChip = () => {
+        const hasEmptyLimit = Array.from(apiRemainingLimits.values()).some(limit => limit <= 0);
+
+        if (!hasEmptyLimit) {
+            return null;
+        }
+
+        return (
+            <Tooltip
+                title={(
+                    <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                        <Typography variant="body2" sx={{ mb: 0.5 }}>
+                            Limit for the following APIs reached
+                        </Typography>
+                        {
+                            Array.from(apiRemainingLimits.entries())
+                                .filter(([_, limit]) => limit <= 0)
+                                .map(([apiType]) => (
+                                    <Typography
+                                        key={apiType}
+                                        variant="caption"
+                                    >
+                                        {apiType}
+                                    </Typography>
+                                ))
+                        }
+                    </Box>
+                )}
+                component="span"
+                sx={{
+                    zIndex: 1001,
+                }}
+            >
+                <Chip
+                    variant="outlined"
+                    label={"API Limit reached"}
+                    size="small"
+                    color={"warning"}
+                    icon={<FlagCircleOutlinedIcon sx={{ pl: '2px' }} />}
+                    onClick={() => null}
+                />
+            </Tooltip>
+        );
+    }
 
     return (
         <Box
@@ -17,6 +103,36 @@ function CustomToolbar(props) {
                 ...props.sx
             }}
         >
+            {/* STATUS CHIPS */}
+            <Box
+                sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    mr: 1,
+                    gap: 1,
+                }}
+            >
+                {getEmptyApiLimitChip()}
+                {
+                    hasGlobalApiCooldown &&
+                    <Tooltip
+                        title={"The API is on cooldown, please wait a moment."}
+                        component="span"
+                        sx={{
+                            zIndex: 1001,
+                        }}
+                    >
+                        <Chip
+                            variant="outlined"
+                            label={"API Cooldown"}
+                            size="small"
+                            color={"error"}
+                            icon={<VpnLockOutlinedIcon sx={{ pl: '2px' }} />}
+                            onClick={() => null}
+                        />
+                    </Tooltip>
+                }
+            </Box>
             <ButtonGroup
                 disableElevation
                 variant="text"
