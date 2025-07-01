95b4b3fe5443950108a11271172e4f75adcc76fc
MaikEight <mail@maik8.de>
2025-07-02 01:51:32 +0200

    🎨 Refactor GradientBorderButton and GradientBorderButtonInner

--- src/components/Sidebar/ButtonParts/GradientBorderButton.jsx
+++ src/components/Sidebar/ButtonParts/GradientBorderButton.jsx
@@ -3,14 +3,13 @@ import styled from "styled-components";
 const GradientBorderButton = styled('div').withConfig({
     shouldForwardProp: (prop) => prop !== 'sx'
 })(({ theme, selected, sx }) => ({
-    position: 'relative',
     color: !selected
         ? theme.palette.text.primary
         : theme.palette.mode === 'light'
             ? theme.palette.background.default
             : theme.palette.text.primary,
-    textTransform: 'none',
     borderRadius: '30px',
+    height: 'fit-content',
     cursor: 'pointer',
     padding: '2px',
     background: 'linear-gradient(98deg, rgb(198, 167, 254), rgba(145, 85, 253) 94%)',
--- src/components/Sidebar/ButtonParts/GradientBorderButtonInner.jsx
+++ src/components/Sidebar/ButtonParts/GradientBorderButtonInner.jsx
@@ -3,11 +3,11 @@ import styled from "styled-components";
 const GradientBorderButtonInner = styled('div').withConfig({
     shouldForwardProp: (prop) => prop !== 'sx'
 })(({ theme, selected, sx }) => ({
+    height: '36px',
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
     padding: '6px',
-    gap: '8px',
     borderRadius: '28px',
     background: selected ? 'linear-gradient(98deg, rgb(198, 167, 254), rgba(145, 85, 253) 94%)' : theme.palette.background.default,
     transition: theme.transitions.create('background-color'),
