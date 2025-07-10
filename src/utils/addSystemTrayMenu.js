d2b414babcb4868534822c46bb04d91e8420974a
MaikEight <mail@maik8.de>
2025-07-10 02:04:28 +0200

    ✨ Enhance system tray menu with additional options and navigation functionality

--- src/utils/addSystemTrayMenu.js
+++ src/utils/addSystemTrayMenu.js
@@ -2,19 +2,80 @@ import { TrayIcon } from '@tauri-apps/api/tray';
 import { getCurrentWindow } from '@tauri-apps/api/window'
 import { defaultWindowIcon } from '@tauri-apps/api/app';
 import { Menu } from '@tauri-apps/api/menu';
+import navigationService from './navigationService';
 
 async function addSystemTray() {
-
+    
     const menu = await Menu.new({
         items: [
             {
                 id: 'open',
-                text: 'Open Exalt Account Manager',
+                text: '📂 Open',                
                 action: () => onTrayMenuClick('open'),
             },
             {
+                id: 'accounts',
+                text: '👥 Accounts',
+                action: () => onTrayMenuClick('accounts'),
+            },
+            {
+                id: 'vaultPeeker',
+                text: '🔍 Vault Peeker',
+                action: () => onTrayMenuClick('vaultPeeker'),
+            },
+            {
+                id: 'dailyLogins',
+                text: '📅 Daily Logins',
+                action: () => onTrayMenuClick('dailyLogins'),
+            },
+            {
+                id: 'settings',
+                text: '⚙️ Settings',
+                action: () => onTrayMenuClick('settings'),
+            },
+            {
+                id: 'profile',
+                text: '👤 Profile',
+                action: () => onTrayMenuClick('profile'),
+            },
+            {
+                id: 'more',
+                text: '➕ More',
+                items: [
+                    {
+                        id: 'utilities',
+                        text: '🔧 Utilities',
+                        action: () => onTrayMenuClick('utilities'),
+                    },
+                    {
+                        id: 'logs',
+                        text: '📋 Logs',
+                        action: () => onTrayMenuClick('logs'),
+                    },
+                    {
+                        id: 'importer',
+                        text: '📥 Importer',
+                        action: () => onTrayMenuClick('importer'),
+                    },
+                    {
+                        id: 'feedback',
+                        text: '💬 Feedback',
+                        action: () => onTrayMenuClick('feedback'),
+                    },
+                    {
+                        id: 'about',
+                        text: 'ℹ️ About',
+                        action: () => onTrayMenuClick('about'),
+                    },
+                ]
+            },
+            {
+                id: 'separator',
+                text: '—————————————————',
+            },
+            {
                 id: 'quit',
-                text: 'Quit Exalt Account Manager',
+                text: '❎ Quit Exalt Account Manager',
                 action: () => onTrayMenuClick('quit'),
             },
         ],
@@ -23,12 +84,47 @@ async function addSystemTray() {
     const onTrayMenuClick = async (itemId) => {
         switch (itemId) {
             case 'open':
-                await getCurrentWindow().show().catch(console.error);
-                const isMinimized = await getCurrentWindow().isMinimized().catch(console.error);
-                if (isMinimized) {
-                    await getCurrentWindow().unminimize().catch(console.error);
-                }
-                await getCurrentWindow().setFocus().catch(console.error);
+                await showAndFocusWindow();
+                break;
+            case 'accounts':
+                await showAndFocusWindow();
+                navigationService.goToAccounts();
+                break;
+            case 'vaultPeeker':
+                await showAndFocusWindow();
+                navigationService.goToVaultPeeker();
+                break;
+            case 'dailyLogins':
+                await showAndFocusWindow();
+                navigationService.goToDailyLogins();
+                break;
+            case 'utilities':
+                await showAndFocusWindow();
+                navigationService.goToUtilities();
+                break;
+            case 'settings':
+                await showAndFocusWindow();
+                navigationService.goToSettings();
+                break;
+            case 'logs':
+                await showAndFocusWindow();
+                navigationService.goToLogs();
+                break;
+            case 'about':
+                await showAndFocusWindow();
+                navigationService.goToAbout();
+                break;
+            case 'feedback':
+                await showAndFocusWindow();
+                navigationService.goToFeedback();
+                break;
+            case 'importer':
+                await showAndFocusWindow();
+                navigationService.goToImporter();
+                break;                
+            case 'profile':
+                await showAndFocusWindow();
+                navigationService.goToProfile();
                 break;
             case 'quit':
                 getCurrentWindow().close().catch(console.error);
@@ -39,6 +135,15 @@ async function addSystemTray() {
         }
     }
 
+    const showAndFocusWindow = async () => {
+        await getCurrentWindow().show().catch(console.error);
+        const isMinimized = await getCurrentWindow().isMinimized().catch(console.error);
+        if (isMinimized) {
+            await getCurrentWindow().unminimize().catch(console.error);
+        }
+        await getCurrentWindow().setFocus().catch(console.error);
+    }
+
     const options = {
         title: 'Exalt Account Manager',
         tooltip: 'Exalt Account Manager',
@@ -47,6 +152,7 @@ async function addSystemTray() {
         menu: menu,
 
     };
+    
     const trayIconId = sessionStorage.getItem('trayIconId');
     if (trayIconId) {
         TrayIcon.removeById(trayIconId).catch(console.error);
