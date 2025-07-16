e48b95eda9d048922f2310951b2680fb1c0feb2e
MaikEight <mail@maik8.de>
2025-07-17 01:49:50 +0200

    🎨 Enhance CustomToolbar with export and filter functionalities, integrating new MUI components

--- src/components/GridComponents/CustomToolbar.jsx
+++ src/components/GridComponents/CustomToolbar.jsx
@@ -1,6 +1,6 @@
 import { useTheme } from "@emotion/react";
-import { Box, Button, Checkbox, FormControlLabel, LinearProgress, Popover, Tooltip, Typography } from "@mui/material";
-import { GridToolbarColumnsButton, GridToolbarContainer, GridToolbarFilterButton } from "@mui/x-data-grid";
+import { Badge, Box, Button, Checkbox, FormControlLabel, LinearProgress, Popover, Tooltip, Typography } from "@mui/material";
+import { ColumnsPanelTrigger, FilterPanelTrigger, GridToolbarColumnsButton, GridToolbarContainer, GridToolbarFilterButton, Toolbar, ToolbarButton } from "@mui/x-data-grid";
 import Searchbar from "./Searchbar";
 import AddIcon from '@mui/icons-material/Add';
 import FileUploadOutlinedIcon from '@mui/icons-material/FileUploadOutlined';
@@ -11,6 +11,8 @@ import ListOutlinedIcon from '@mui/icons-material/ListOutlined';
 import useAccounts from './../../hooks/useAccounts';
 import Papa from 'papaparse';
 import { invoke } from "@tauri-apps/api/core";
+import ViewColumnIcon from '@mui/icons-material/ViewColumn';
+import FilterListIcon from '@mui/icons-material/FilterList';
 
 function CustomToolbar({ onSearchChanged, onAddNew }) {
     const [anchorElPopover, setAnchorElPopover] = useState(null);
@@ -84,6 +86,213 @@ function CustomToolbar({ onSearchChanged, onAddNew }) {
     const handleCloseExportPopover = () => {
         setAnchorElPopover(null);
     };
+
+    return (
+        <Toolbar>
+            <Tooltip title="Columns">
+                <ColumnsPanelTrigger
+                    render={
+                        <ToolbarButton
+                            render={
+                                <Button
+                                    aria-label="Columns"
+                                    size="small"
+                                    variant="text"
+                                    startIcon={<ViewColumnIcon />}
+                                    sx={{
+                                        width: 'fit-content',
+                                        height: 'fit-content',
+                                        px: 1,
+                                    }}
+                                >
+                                    Columns
+                                </Button>
+                            }
+                        />
+                    }
+                />
+            </Tooltip>
+            <Tooltip title="Filters">
+                <FilterPanelTrigger
+                    render={(props, state) => (
+                        <ToolbarButton
+                            {...props}
+                            render={
+                                <Badge badgeContent={state.filterCount} color="primary" variant="dot">
+                                    <Button
+                                        aria-label="Filters"
+                                        size="small"
+                                        variant="text"
+                                        startIcon={<FilterListIcon />}
+                                        component={'div'}
+                                        sx={{
+                                            width: 'fit-content',
+                                            height: 'fit-content',
+                                            px: 1,
+                                        }}
+                                    >
+                                        Filters
+                                    </Button>
+                                </Badge>
+                            }
+                        />
+                    )}
+                />
+            </Tooltip>
+            <Tooltip title="Export Accounts">
+                <ToolbarButton
+                    onClick={handleClickExport}
+                    render={
+                        <Button
+                            variant="text"
+                            color="primary"
+                            size="small"
+                            sx={{
+                                mt: 0.5,
+                                mb: 0.5,
+                            }}
+                            startIcon={<FileUploadOutlinedIcon />}
+                        >
+                            Export
+                        </Button>
+                    }
+                />
+            </Tooltip>
+            <Box
+                sx={{
+                    ml: 'auto',
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'end',
+                    alignItems: 'center',
+                }}
+            >
+                {
+                    onAddNew &&
+                    <Tooltip title="Add Accounts">
+                        <ToolbarButton
+                            onClick={onAddNew}
+                            render={
+                                <Button
+                                    variant="text"
+                                    color="primary"
+                                    size="small"
+                                    sx={{
+                                        mt: 0.5,
+                                        mb: 0.5,
+                                    }}
+                                    startIcon={<AddIcon />}
+                                >
+                                    Add
+                                </Button>
+                            }
+                        />
+                    </Tooltip>
+                }
+                <Box
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        maxHeight: 49,
+                    }}
+                >
+                    <Searchbar onSearchChanged={onSearchChanged} />
+                </Box>
+            </Box>
+            <Popover
+                id={popoverId}
+                open={openExportPopover}
+                anchorEl={anchorElPopover}
+                onClose={handleCloseExportPopover}
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'center',
+                }}
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'center',
+                }}
+            >
+                <Box
+                    sx={{
+                        position: 'relative',
+                        pt: 2,
+                        px: 2,
+                        borderRadius: `${theme.shape.borderRadius}px`,
+                    }}
+                >
+                    {
+                        isExporting &&
+                        <LinearProgress
+                            sx={{
+                                position: 'absolute',
+                                top: 0,
+                                left: 0,
+                                right: 0,
+                            }}
+                        />
+                    }
+                    <Typography
+                        variant="body1"
+                    >
+                        Please choose the format you want to export to have
+                    </Typography>
+                    <FormControlLabel
+                        control={
+                            <Checkbox
+                                disabled={isExporting}
+                                value={includeAditionalColumns}
+                                onChange={(event) => setIncludeAditionalColumns(event.target.checked)}
+                            />
+                        }
+                        label={
+                            <Typography
+                                variant="body2"
+                            >
+                                Include additional columns
+                            </Typography>
+                        }
+                    />
+                </Box>
+                <Box
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        px: 1,
+                        pb: 1,
+                        gap: 0.5,
+                    }}
+                >
+                    <StyledButton
+                        disabled={isExporting}
+                        color="primary"
+                        size="small"
+                        sx={{
+                            mt: 0.5,
+                            mb: 0.5,
+                        }}
+                        startIcon={<ListOutlinedIcon />}
+                        onClick={() => handleExport('CSV')}
+                    >
+                        CSV
+                    </StyledButton>
+                    <StyledButton
+                        disabled={isExporting}
+                        color="primary"
+                        size="small"
+                        sx={{
+                            mt: 0.5,
+                            mb: 0.5,
+                        }}
+                        startIcon={<DataArrayOutlinedIcon />}
+                        onClick={() => handleExport('JSON')}
+                    >
+                        JSON
+                    </StyledButton>
+                </Box>
+            </Popover>
+        </Toolbar>
+    );
 
     return (
         <Box style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', borderRadius: theme.shape.borderRadius }}>
