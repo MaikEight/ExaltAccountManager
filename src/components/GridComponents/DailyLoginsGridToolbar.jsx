7fec3fd900eca1b44682824a716a98ee87c63595
MaikEight <mail@maik8.de>
2025-07-17 01:50:11 +0200

    🎨 Refactor DailyLoginsGridToolbar to use new MUI components for improved functionality and UI

--- src/components/GridComponents/DailyLoginsGridToolbar.jsx
+++ src/components/GridComponents/DailyLoginsGridToolbar.jsx
@@ -1,50 +1,76 @@
-import { useTheme } from "@emotion/react";
-import { Box, IconButton, Tooltip } from "@mui/material";
-import { GridToolbarColumnsButton, GridToolbarContainer, GridToolbarFilterButton } from "@mui/x-data-grid";
+import { Badge, Button, IconButton, Tooltip } from "@mui/material";
+import { ColumnsPanelTrigger, FilterPanelTrigger, Toolbar, ToolbarButton } from "@mui/x-data-grid";
 import RefreshOutlinedIcon from '@mui/icons-material/RefreshOutlined';
+import ViewColumnIcon from '@mui/icons-material/ViewColumn';
+import FilterListIcon from '@mui/icons-material/FilterList';
 
 function DailyLoginsGridToolbar({ onRefresh }) {
-    const theme = useTheme();
-
     return (
-        <Box style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-            <GridToolbarContainer
-                sx={{
-                    display: "flex",
-                    justifyContent: "start",
-                    minHeight: 49,
-                    backgroundColor: theme.palette.background.paper,
-                    borderRadius: `${theme.shape.borderRadius}px ${theme.shape.borderRadius}px 0 0`,
-                    pt: 0.5,
-                    pb: 0.5,
-                    pl: 1,
-                    pr: 1,
-                }}
+        <Toolbar>
+            <Tooltip title="Columns">
+                <ColumnsPanelTrigger
+                    render={
+                        <ToolbarButton
+                            render={
+                                <Button
+                                    aria-label="Columns"
+                                    size="small"
+                                    variant="text"
+                                    startIcon={<ViewColumnIcon />}
+                                    sx={{
+                                        width: 'fit-content',
+                                        height: 'fit-content',
+                                        px: 1,
+                                    }}
+                                >
+                                    Columns
+                                </Button>
+                            }
+                        />
+                    }
+                />
+            </Tooltip>
+            <Tooltip title="Filters">
+                <FilterPanelTrigger
+                    render={(props, state) => (
+                        <ToolbarButton
+                            {...props}
+                            render={
+                                <Badge badgeContent={state.filterCount} color="primary" variant="dot">
+                                    <Button
+                                        aria-label="Filters"
+                                        size="small"
+                                        variant="text"
+                                        startIcon={<FilterListIcon />}
+                                        component={'div'}
+                                        sx={{
+                                            width: 'fit-content',
+                                            height: 'fit-content',
+                                            px: 1,
+                                        }}
+                                    >
+                                        Filters
+                                    </Button>
+                                </Badge>
+                            }
+                        />
+                    )}
+                />
+            </Tooltip>
+            <Tooltip 
+            sx={{
+                ml: 'auto',
+            }}
+            title="Refresh data"
             >
-                <Tooltip title="Show/Hide Columns">
-                    <GridToolbarColumnsButton />
-                </Tooltip>
-                <GridToolbarFilterButton />
-            </GridToolbarContainer>
-            <Box
-                sx={{
-                    mr: 1.5,
-                    display: 'flex',
-                    flexDirection: 'row',
-                    maxHeight: 49,
-                    overflow: 'hidden',
-                }}
-            >
-                <Tooltip title="Refresh data">
-                    <IconButton
-                        size="small"
-                        onClick={onRefresh}
-                    >
-                        <RefreshOutlinedIcon />
-                    </IconButton>
-                </Tooltip>
-            </Box>
-        </Box>
+                <IconButton
+                    size="small"
+                    onClick={onRefresh}
+                >
+                    <RefreshOutlinedIcon />
+                </IconButton>
+            </Tooltip>
+        </Toolbar>
     );
 }
 
