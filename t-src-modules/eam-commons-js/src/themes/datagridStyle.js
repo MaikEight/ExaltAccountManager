c95c7057f43950fb495c431f3941b730ab7eee21
MaikEight <mail@maik8.de>
2025-07-17 01:52:40 +0200

    ✨ Add datagridStyle for dynamic theming based on mode

---
+++ t-src-modules/eam-commons-js/src/themes/datagridStyle.js
@@ -0,0 +1,7 @@
+export const datagridStyle = (mode) => ({
+     DataGrid: {
+        bg: mode === 'dark' ? '#312D4B' : '#FFFFFF',
+        pinnedBg: mode === 'dark' ? '#3D3759' : '#F9FAFC',
+        headerBg: mode === 'dark' ? '#3D3759' : '#F9FAFC',
+      },
+});
