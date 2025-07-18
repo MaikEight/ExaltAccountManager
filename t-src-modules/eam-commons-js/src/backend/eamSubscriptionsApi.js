7457464382b3adf802808ddbb2ffea751acb2699
MaikEight <mail@maik8.de>
2025-07-18 03:08:45 +0200

    ✨ Improve error handling in postPlusToken and validatePlusToken functions

--- t-src-modules/eam-commons-js/src/backend/eamSubscriptionsApi.js
+++ t-src-modules/eam-commons-js/src/backend/eamSubscriptionsApi.js
@@ -12,11 +12,16 @@ async function postPlusToken(id_token, deviceId) {
     const response = await invoke('send_post_request_with_json_body', { url: `${EAM_SUBSCRIPTIONS_API}/plus/token`, data: JSON.stringify(body), headersOpt: myHeaders })
         .catch(error => {
             console.error('Error posting Plus token:', error);
-            throw error;
+            return { error: true, message: error };
         });
 
     if (!response) {
         console.error('No response received from Plus token API');
+        return null;
+    }
+
+    if (response.error) {
+        console.error('Error in Plus token response:', response.message);
         return null;
     }
 
@@ -32,7 +37,52 @@ async function postPlusToken(id_token, deviceId) {
         console.error('Error parsing Plus token response:', error);
         return null;
     }
-
 }
 
-export { postPlusToken };
+async function validatePlusToken(jwt, deviceId) {
+    const debugFlag = sessionStorage.getItem('flag:debug') === 'true';
+    const myHeaders = new Map();
+    myHeaders.set('Authorization', `Bearer ${jwt}`);
+
+    if(debugFlag) {
+        console.log('Validating Plus token', jwt, deviceId);
+    }
+
+    const body = {
+        "deviceId": deviceId
+    };
+
+    const response = await invoke('send_get_request_with_json_body', { url: `${EAM_SUBSCRIPTIONS_API}/plus/token/validation`, data: JSON.stringify(body), customHeaders: myHeaders })
+        .catch(error => {
+            console.error('Error validating Plus token:', error);
+            if (debugFlag) {
+                console.error(error);
+            }
+            return { error: true, message: error };
+        });
+
+    if (!response) {
+        console.error('No response received from Plus token validation API');
+        return null;
+    }
+
+    if (response.error) {
+        console.error('Error in Plus token response:', response.message);
+        return null;
+    }
+
+    try {
+        const data = JSON.parse(response);
+
+        if (debugFlag) {
+            console.log('Plus token validation response:', data);
+        }
+
+        return data;
+    } catch (error) {
+        console.error('Error parsing Plus token validation response:', error);
+        return null;
+    }
+}
+
+export { postPlusToken, validatePlusToken };
