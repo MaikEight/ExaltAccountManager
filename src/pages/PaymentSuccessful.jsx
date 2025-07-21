5f93a6d6d2efa4405468a09b3fc67701e4ad4505
MaikEight <mail@maik8.de>
2025-07-21 04:09:03 +0200

    ✨ Add random success message display and mascot image to PaymentSuccessful component

--- src/pages/PaymentSuccessful.jsx
+++ src/pages/PaymentSuccessful.jsx
@@ -1,12 +1,18 @@
 import { Box, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import Confetti from "react-confetti";
+import { EAM_PLUS_PURCHASE_SUCCESS_MESSAGES, MASCOT_NAME } from "../constants";
+import { useRef } from "react";
 
 function PaymentSuccessful() {
     const [windowSize, setWindowSize] = useState({
         width: window.innerWidth,
         height: window.innerHeight,
     });
+
+    const textToDisplay = useRef(
+        EAM_PLUS_PURCHASE_SUCCESS_MESSAGES[Math.floor(Math.random() * EAM_PLUS_PURCHASE_SUCCESS_MESSAGES.length)]
+    );
 
     useEffect(() => {
         const handleResize = () => {
@@ -34,7 +40,7 @@ function PaymentSuccessful() {
             <Confetti
                 width={windowSize.width}
                 height={windowSize.height}
-                
+
             />
             <h1>🥳 EAM Plus Successfully activated 🎉</h1>
             <Box
@@ -44,11 +50,21 @@ function PaymentSuccessful() {
                     gap: 0,
                     alignItems: 'center',
                     justifyContent: 'center',
-                    p:0,
-                }}            
+                    p: 0,
+                }}
             >
                 <Typography>Thank you for your purchase and support!</Typography>
                 <Typography>Enjoy the additional features and benefits of EAM Plus.</Typography>
+                <img
+                    src="/mascot/Happy/cheer_very_low_res.png"
+                    alt={`${MASCOT_NAME} is happy to see you using EAM Plus!`}
+                    style={{
+                        marginTop: '24px',
+                    }}
+                />
+                <Typography sx={{ marginTop: '16px' }}>
+                    {textToDisplay?.current}
+                </Typography>
             </Box>
         </Box>
     );
