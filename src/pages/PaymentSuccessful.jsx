94e8eeb0044a2e2d945e97e3396121bbebc07e56
MaikEight <mail@maik8.de>
2025-07-26 23:26:22 +0200

    🎨 Refactor PaymentSuccessful: rearrange windowSize state initialization and enhance layout with zIndex adjustments and additional user guidance

--- src/pages/PaymentSuccessful.jsx
+++ src/pages/PaymentSuccessful.jsx
@@ -5,14 +5,16 @@ import { EAM_PLUS_PURCHASE_SUCCESS_MESSAGES, MASCOT_NAME } from "../constants";
 import { useRef } from "react";
 
 function PaymentSuccessful() {
-    const [windowSize, setWindowSize] = useState({
-        width: window.innerWidth,
-        height: window.innerHeight,
-    });
+
 
     const textToDisplay = useRef(
         EAM_PLUS_PURCHASE_SUCCESS_MESSAGES[Math.floor(Math.random() * EAM_PLUS_PURCHASE_SUCCESS_MESSAGES.length)]
     );
+
+    const [windowSize, setWindowSize] = useState({
+        width: window.innerWidth,
+        height: window.innerHeight,
+    });
 
     useEffect(() => {
         const handleResize = () => {
@@ -40,7 +42,7 @@ function PaymentSuccessful() {
             <Confetti
                 width={windowSize.width}
                 height={windowSize.height}
-
+                style={{ zIndex: 0 }}
             />
             <h1>🥳 EAM Plus Successfully activated 🎉</h1>
             <Box
@@ -51,10 +53,23 @@ function PaymentSuccessful() {
                     alignItems: 'center',
                     justifyContent: 'center',
                     p: 0,
+                    zIndex: 1
                 }}
             >
                 <Typography>Thank you for your purchase and support!</Typography>
                 <Typography>Enjoy the additional features and benefits of EAM Plus.</Typography>
+            </Box>
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    width: '100%',
+                    my: 'auto',
+                    zIndex: 1
+                }}
+            >
                 <img
                     src="/mascot/Happy/cheer_very_low_res.png"
                     alt={`${MASCOT_NAME} is happy to see you using EAM Plus!`}
@@ -62,10 +77,13 @@ function PaymentSuccessful() {
                         marginTop: '24px',
                     }}
                 />
-                <Typography sx={{ marginTop: '16px' }}>
+                <Typography sx={{ marginTop: 2 }}>
                     {textToDisplay?.current}
                 </Typography>
             </Box>
+            <Typography sx={{ mb: 2 }}>
+                If your purchase does not activate right away, please try to check for Plus subscription on the profile page.
+            </Typography>
         </Box>
     );
 }
