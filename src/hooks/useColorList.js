14e5050c0e52b8db7964e5e717f7612573b0158e
MaikEight <mail@maik8.de>
2025-07-27 03:29:15 +0200

    🔧 Improve documentation and formatting in useColorList hook

--- src/hooks/useColorList.js
+++ src/hooks/useColorList.js
@@ -1,9 +1,14 @@
 import { useMemo } from 'react';
 import { useTheme } from "@emotion/react";
 
+/**
+ * Custom hook to get a list of colors based on the provided index.
+ * @param {number|string} index - The index or name of the color.
+ * @returns {object[]} - An array of color objects.
+ */
 const useColorList = (index) => {
     const theme = useTheme();
-    
+
     const colorList = useMemo(
         () => [
             { background: '#9155fd1f', color: theme.palette.primary.main },
@@ -19,7 +24,22 @@ const useColorList = (index) => {
         return { background: '#8a8d931f', color: '#8a8d931f' };
     }
 
-    return index !== undefined ? colorList[index >= colorList.length ? index % colorList.length : index] : colorList;
+    return (
+        index !== undefined ? (
+            typeof index === 'number' ?
+                colorList[index >= colorList.length ? index % colorList.length : index]
+                : colorList[getIndexByName(index)]
+        ) : colorList
+    );
 };
+
+function getIndexByName(name) {
+    const colorList = [
+        'primary', 'info', 'success', 'error', 'warning'
+    ];
+
+    const index = colorList.indexOf(name);
+    return index !== -1 ? index : 0; // Default to primary if not found
+}
 
 export { useColorList };
