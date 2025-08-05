9fae5a52d06d43daac749b905c96bf9bf01811d1
MaikEight <mail@maik8.de>
2025-08-05 02:36:23 +0200

    🔧 Refactor readFile function to use aliased import for better clarity

--- t-src-modules/eam-commons-js/src/utils/readFileUtil.js
+++ t-src-modules/eam-commons-js/src/utils/readFileUtil.js
@@ -1,19 +1,19 @@
 import { logToErrorLog } from "./loggingUtils";
-import { exists, readFile, BaseDirectory } from '@tauri-apps/plugin-fs';
+import { exists, readFile as tauriReadFile, BaseDirectory } from '@tauri-apps/plugin-fs';
 
 async function readFileUTF8(filePath, parseAsJSON = false) {
-    try {        
+    try {
         if (await exists(filePath, { dir: BaseDirectory.AppData })) {
-            const bytes = await readFile(filePath, { dir: BaseDirectory.AppData });
+            const bytes = await tauriReadFile(filePath, { dir: BaseDirectory.AppData });
             const stringContent = new TextDecoder().decode(bytes);
             return parseAsJSON ? JSON.parse(stringContent) : stringContent;
         }
         console.warn("File does not exist:", filePath);
         logToErrorLog('readFileUTF8', `File does not exist: ${filePath}`);
     } catch (error) {
-        if((error).includes('os error 2')) return;
+        if ((error).includes('os error 2')) return;
 
-        console.error('Error reading file:',filePath, error);
+        console.error('Error reading file:', filePath, error);
         logToErrorLog('readFileUTF8', `Error reading file: ${filePath} - ${error}`);
     }
     return null;
