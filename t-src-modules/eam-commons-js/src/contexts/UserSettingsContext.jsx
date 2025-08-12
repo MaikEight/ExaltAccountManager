7ab239e10f8603cec4bdab30d97074f15481e06f
MaikEight <mail@maik8.de>
2025-08-13 00:41:10 +0200

    ✨ Implement user setting for disabling auto-hide on daily login startup and update local storage handling

--- t-src-modules/eam-commons-js/src/contexts/UserSettingsContext.jsx
+++ t-src-modules/eam-commons-js/src/contexts/UserSettingsContext.jsx
@@ -34,10 +34,26 @@ const getGameExePath = async () => {
         });
 };
 
+const getDisableAutoHideOnDailyLoginStartup = async () => {
+    return invoke('get_user_data_by_key', { key: 'disable_auto_hide_on_daily_login_startup' })
+        .then((res) => {
+            if (res && res.dataValue) {
+                return res.dataValue === 'true';
+            }
+            return false;
+        })
+        .catch(() => {
+            invoke('insert_or_update_user_data', { userData: { dataKey: 'disable_auto_hide_on_daily_login_startup', dataValue: 'false' } }).catch(console.error);
+            return false;
+        });
+}
+
 const defaultSettings = {
     general: {
         theme: "dark",
         minimizeToTray: true,
+        hideOnAutostart: false,
+        discordRichPresenceEnabled: true,
     },
     accounts: {
         columnsHidden: {
@@ -48,6 +64,7 @@ const defaultSettings = {
     },
     game: {
         defaultServer: "Last server",
+        //HIDDEN: exePath: string // Path to the game executable, store in the database
     },
     vaultPeeker: {
         collapsedFileds: {
@@ -58,6 +75,13 @@ const defaultSettings = {
         accountView: {
             hiddenVaults: [], //Array of vault names that are hidden
         }
+    },
+    backgroundSync: {
+        enabled: true,
+    },
+    dailyLogin: {
+        closeAfterFinish: false,
+        //HIDDEN: disableAutoHideOnDailyLoginStartup: boolean // Whether to disable auto-hide on daily login startup, store in the database
     }
 };
 
@@ -84,10 +108,20 @@ function UserSettingsProvider({ children }) {
             return;
         }
 
-        localStorage.setItem("userSettings", JSON.stringify(userSettingsData));
+        const userSettingsDataToSave = { ...userSettingsData };
+        // Remove any transient data
+        if (userSettingsDataToSave?.game?.exePath) {
+            delete userSettingsDataToSave.game.exePath;
+        }
+        if (userSettingsDataToSave?.dailyLogin?.disableAutoHideOnDailyLoginStartup !== null) {
+            delete userSettingsDataToSave.dailyLogin.disableAutoHideOnDailyLoginStartup;
+        }
+
+        localStorage.setItem("userSettings", JSON.stringify(userSettingsDataToSave));
+
         const lastUpdated = sessionStorage.getItem("lastUserSettingsUpdated");
-        if (lastUpdated && new Date().getTime() - new Date(lastUpdated).getTime() < 15000) {
-            //When the user settings where updated less than 15 seconds ago, don't log it to the audit log
+        if (lastUpdated && new Date().getTime() - new Date(lastUpdated).getTime() < 30_000) {
+            //When the user settings where updated less than 30 seconds ago, don't log it to the audit log
             //This is to prevent spamming the audit log with user settings updates
             return;
         }
@@ -122,20 +156,42 @@ function UserSettingsProvider({ children }) {
         getByKey: (key) => { return userSettingsData ? userSettingsData[key] : null; },
         setByKey: (key, value) => { setUserSettingsData({ ...userSettingsData, [key]: value }); },
         getByKeyAndSubKey: (key, subKey) => {
-            if (key === 'game' && subKey === 'exePath') {
-                return getGameExePath();
-            }
+            const request = { key, subKey };
+            switch (true) {
+                case request.key === 'game' && request.subKey === 'exePath': {
+                    if (userSettingsData && userSettingsData[key] && userSettingsData[key][subKey]) {
+                        return userSettingsData[key][subKey];
+                    }
 
-            return userSettingsData && userSettingsData[key] ? userSettingsData[key][subKey] : null;
+                    const gameExePath = getGameExePath();
+                    if (gameExePath) {
+                        setUserSettingsData({ ...userSettingsData, [key]: { ...userSettingsData?.[key], [subKey]: gameExePath } });
+                    }
+
+                    return gameExePath;
+                }
+                case request.key === 'dailyLogin' && request.subKey === 'disableAutoHideOnDailyLoginStartup':
+                    return getDisableAutoHideOnDailyLoginStartup();
+                default: {
+                    return userSettingsData && userSettingsData[key] ? userSettingsData[key][subKey] : null;
+                }
+            }
         },
         setByKeyAndSubKey: (key, subKey, value) => {
-            if (key === 'game' && subKey === 'exePath') { //C:\\Users\\Maik8\\Documents\\RealmOfTheMadGod\\Production\\RotMG Exalt.exe
-                invoke('insert_or_update_user_data', { userData: { dataKey: 'game_exe_path', dataValue: value } });
-                return;
+            switch (true) {
+                case key === 'game' && subKey === 'exePath':
+                    invoke('insert_or_update_user_data', { userData: { dataKey: 'game_exe_path', dataValue: value } });
+                    return;
+                case key === 'dailyLogin' && subKey === 'disableAutoHideOnDailyLoginStartup':
+                    console.log('Setting daily_login_disable_auto_hide to', value);
+                    invoke('insert_or_update_user_data', { userData: { dataKey: 'daily_login_disable_auto_hide', dataValue: value?.toString() } });
+                    return;
+                default: {
+                    const data = { ...userSettingsData };
+                    data[key] ? data[key][subKey] = value : data[key] = { [subKey]: value };
+                    setUserSettingsData(data);
+                }
             }
-            const data = { ...userSettingsData };
-            data[key] ? data[key][subKey] = value : data[key] = { [subKey]: value };
-            setUserSettingsData(data);
         },
         reset: () => {
             setUserSettingsData(expandSettings(defaultSettings));
