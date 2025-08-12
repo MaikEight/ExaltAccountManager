76274783c90608109c8d49d5b5866ddb7f0cd362
MaikEight <mail@maik8.de>
2025-08-13 00:38:47 +0200

    🔖 Bump app version to 4.2.6; set pre-release flag to true

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,8 @@
 import { invoke } from '@tauri-apps/api/core';
 
-export const APP_VERSION = '4.2.5';
-export const APP_VERSION_RELEASE_DATE = '01.08.2025';
-export const IS_PRE_RELEASE = false;
+export const APP_VERSION = '4.2.6';
+export const APP_VERSION_RELEASE_DATE = '13.08.2025';
+export const IS_PRE_RELEASE = true;
 
 export const CACHE_PREFIX = '!cache-';
 
