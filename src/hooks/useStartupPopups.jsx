d6e7e4e2976eed5eb1f0cb782b36a8eeb0922c63
MaikEight <mail@maik8.de>
2025-08-24 08:00:24 +0200

    ✨ Add ChangelogVersion4_2_7 to startup popups

--- src/hooks/useStartupPopups.jsx
+++ src/hooks/useStartupPopups.jsx
@@ -12,6 +12,7 @@ import ChangelogVersion4_2_3 from "../components/Popups/Changelogs/ChangelogVers
 import ChangelogVersion4_2_4 from "../components/Popups/Changelogs/ChangelogVersion4_2_4";
 import ChangelogVersion4_2_5 from './../components/Popups/Changelogs/ChangelogVersion4_2_5';
 import ChangelogVersion4_2_6 from './../components/Popups/Changelogs/ChangelogVersion4_2_6';
+import ChangelogVersion4_2_7 from './../components/Popups/Changelogs/ChangelogVersion4_2_7';
 
 const isBetaVersion = false;
 
@@ -63,6 +64,11 @@ function useStartupPopups() {
             version: "4.2.6",
             preventClose: false,
             content: <ChangelogVersion4_2_6 />
+        },
+        {
+            version: "4.2.7",
+            preventClose: false,
+            content: <ChangelogVersion4_2_7 />
         },
     ];
 
