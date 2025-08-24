1975404a4e05f4f3f1e20b1d623f8f1b25a22efd
MaikEight <mail@maik8.de>
2025-08-24 07:52:49 +0200

    ✨ Implement getLatestPopup function for fetching latest news popups

---
+++ src/backend/newsApi.js
@@ -0,0 +1,45 @@
+import { EAM_NEWS_BASE_URL } from "../constants";
+import { fetch } from "@tauri-apps/plugin-http";
+
+async function getLatestPopup(lastSeenPopupId) {
+    const url = `${EAM_NEWS_BASE_URL}popups${lastSeenPopupId ? `?id=${lastSeenPopupId}` : ""}`;
+    const headers = {
+        'eam-application-id': 'ExaltAccountManager'
+    };
+
+    //Returns either the latest popup or 204 when no new popup is available
+    const response = await fetch(url, {
+        method: 'GET',
+        headers,
+    })
+        .catch(error => {
+            console.error("Error fetching latest popup:", error);
+            return {
+                error: error.message || "Unknown error",
+                status: error.status || 500
+            };
+        });
+
+    if (response && response.status === 204) {
+        return null;
+    }
+
+    if (response && response.status !== 200) {
+        return {
+            error: `Failed to fetch latest popup. Status code: ${response.status}`,
+            status: response.status
+        };
+    }
+
+    const data = await response.json();
+    
+    if(data.createdAt) {
+        data.timestamp = new Date(data.createdAt);
+    }
+
+    return data;
+}
+
+export {
+    getLatestPopup
+};
