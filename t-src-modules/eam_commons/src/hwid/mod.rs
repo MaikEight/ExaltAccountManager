3c484ca66c3b6ab46a2594ba8744595d8b20fb70
MaikEight <mail@maik8.de>
2025-09-28 05:48:28 +0200

    ✨ Add platform-specific modules for retrieving device unique identifiers on Windows and macOS

---
+++ t-src-modules/eam_commons/src/hwid/mod.rs
@@ -0,0 +1,9 @@
+#[cfg(target_os = "windows")]
+mod windows;
+#[cfg(target_os = "windows")]
+pub use windows::get_device_unique_identifier;
+
+#[cfg(target_os = "macos")]
+mod macos;
+#[cfg(target_os = "macos")]
+pub use macos::get_device_unique_identifier;
