10a3aa85b982eeebbefd23571badda8e3bf99daa
MaikEight <mail@maik8.de>
2025-09-28 07:17:24 +0200

    🎨 Remove unused parameter from get_device_unique_identifier function in macOS implementation

--- t-src-modules/eam_commons/src/hwid/macos.rs
+++ t-src-modules/eam_commons/src/hwid/macos.rs
@@ -1,3 +1,3 @@
-pub async fn get_device_unique_identifier(_: &str) -> Result<String, String> {
+pub async fn get_device_unique_identifier() -> Result<String, String> {
     Err("macOS impl not available yet".into())
 }
