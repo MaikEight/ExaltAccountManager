b2566a6d62d9229f79a903bbe7ef87e9d7678065
MaikEight <mail@maik8.de>
2025-10-04 01:46:41 +0200

    ✨ Add delete_data function to Windows and macOS encryption modules

--- t-src-modules/eam_commons/src/encryption_utils/mod.rs
+++ t-src-modules/eam_commons/src/encryption_utils/mod.rs
@@ -1,12 +1,12 @@
 #[cfg(target_os = "windows")]
 mod windows;
 #[cfg(target_os = "windows")]
-pub use windows::{decrypt_data, encrypt_data};
+pub use windows::{decrypt_data, encrypt_data, delete_data};
 
 #[cfg(target_os = "macos")]
 mod macos;
 #[cfg(target_os = "macos")]
-pub use macos::{decrypt_data, encrypt_data};
+pub use macos::{decrypt_data, encrypt_data, delete_data};
 
 #[cfg(not(any(target_os = "windows", target_os = "macos")))]
 mod unsupported;
