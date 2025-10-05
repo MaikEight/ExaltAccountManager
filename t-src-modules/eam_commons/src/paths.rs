1c0eaa6c717be8d638294152e1649c750fccaeb8
MaikEight <mail@maik8.de>
2025-10-06 01:01:08 +0200

    🎨 Refactor get_save_file_path functions: streamline return types and improve clarity

--- t-src-modules/eam_commons/src/paths.rs
+++ t-src-modules/eam_commons/src/paths.rs
@@ -1,12 +1,24 @@
+use std::path::PathBuf;
+
 ///OS dependent fixed path.
 /// 
 ///Windows: `C:\Users\USERNAME\AppData\Local\ExaltAccountManager\v4\`
 /// 
 ///Mac: `~/Library/Application Support/ExaltAccountManager/v4/`
-pub fn get_save_file_path() -> String {
-    
+pub fn get_save_file_path() -> String {    
+    let path = get_save_file_path_buf();
+    path.to_str().unwrap().to_string()
+}
+
+
+///OS dependent fixed path.
+/// 
+///Windows: `C:\Users\USERNAME\AppData\Local\ExaltAccountManager\v4\`
+/// 
+///Mac: `~/Library/Application Support/ExaltAccountManager/v4/`
+pub fn get_save_file_path_buf() -> PathBuf {    
     let mut path = dirs::data_local_dir().unwrap();
     path.push("ExaltAccountManager");
     path.push("v4");
-    path.to_str().unwrap().to_string()
+    path
 }
