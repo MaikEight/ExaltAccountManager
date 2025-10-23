683c374d5bc15d3dfcbf39bf655b224f33aea4fe
MaikEight <mail@maik8.de>
2025-10-23 02:01:37 +0200

    ✨ Add macOS support for default game path: implement path retrieval for RotMGExalt.app

--- t-src-modules/eam_commons/src/paths.rs
+++ t-src-modules/eam_commons/src/paths.rs
@@ -10,7 +10,6 @@ pub fn get_save_file_path() -> String {
     path.to_str().unwrap().to_string()
 }
 
-
 ///OS dependent fixed path.
 /// 
 ///Windows: `C:\Users\USERNAME\AppData\Local\ExaltAccountManager\v4\`
@@ -21,4 +20,30 @@ pub fn get_save_file_path_buf() -> PathBuf {
     path.push("ExaltAccountManager");
     path.push("v4");
     path
+}
+
+///OS dependent default game path.
+/// 
+///Windows: `C:\Users\USERNAME\Documents\RealmOfTheMadGod\Production\RotMG Exalt.exe`
+/// 
+///Mac: `~/RealmOfTheMadGod/Production/RotMGExalt.app`
+pub fn get_default_game_path() -> String {
+    #[cfg(target_os = "windows")]
+    {
+        //C:\Users\USERNAME\Documents\RealmOfTheMadGod\Production\RotMG Exalt.exe
+        let mut path = dirs::document_dir().unwrap();
+        path.push("RealmOfTheMadGod");
+        path.push("Production");
+        path.push("RotMG Exalt.exe");
+        path.to_str().unwrap().to_string()
+    }
+    #[cfg(target_os = "macos")]
+    {
+        //~/RealmOfTheMadGod/Production/RotMGExalt.app
+        let mut path = dirs::home_dir().unwrap();
+        path.push("RealmOfTheMadGod");
+        path.push("Production");
+        path.push("RotMGExalt.app");
+        path.to_str().unwrap().to_string()        
+    }
 }
