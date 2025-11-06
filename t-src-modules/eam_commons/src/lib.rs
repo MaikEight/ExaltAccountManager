9e07262e21522ed7881ff3e08cfe2afa80c48cd5
MaikEight <mail@maik8.de>
2025-11-06 02:07:54 +0100

    🎨 Refactor lib.rs to include daily login task module and clean up Windows-specific code

--- t-src-modules/eam_commons/src/lib.rs
+++ t-src-modules/eam_commons/src/lib.rs
@@ -1,7 +1,11 @@
 pub mod encryption_utils;
 pub use encryption_utils::{encrypt_data, decrypt_data};
+
 pub mod hwid;
 pub use hwid::get_device_unique_identifier;
+
+pub mod daily_login_task;
+pub use daily_login_task::{check_for_installed_eam_daily_login_task, install_eam_daily_login_task, uninstall_eam_daily_login_task};
 
 pub mod paths;
 pub use paths::{get_save_file_path, get_default_game_path};
@@ -19,6 +23,3 @@ pub use diesel_functions::*;
 pub mod rotmg_updater;
 pub mod limiter;
 pub mod parser;
-
-#[cfg(target_os = "windows")]
-pub mod windows_specifics;
