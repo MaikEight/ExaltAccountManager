26d3a82216032d16784253eccc75ccd1bf0d1b22
MaikEight <mail@maik8.de>
2025-12-14 12:59:25 +0100

    🎨 Enhance WidgetBarBase with edit mode functionality and improved layout

--- src/components/Widgets/WidgetBars/WidgetBarBase.jsx
+++ src/components/Widgets/WidgetBars/WidgetBarBase.jsx
@@ -1,19 +1,21 @@
 
-import { Box } from '@mui/material';
+import { Box, Button } from '@mui/material';
 import useWidgets from '../../../hooks/useWidgets';
+import { alpha } from '@mui/material/styles';
 
 function WidgetBarBase() {
-    const { widgetBarState } = useWidgets();
+    const { widgetBarState, updateWidgetBarEditMode } = useWidgets();
 
     return (
         <Box
             sx={{
+                position: 'relative',
                 display: 'flex',
                 flexDirection: 'column',
                 height: '100%',
                 width: '100%',
                 overflowY: 'auto',
-                backgroundColor: 'background.paper',
+                backgroundColor: 'background.default',
                 borderRadius: (theme) => `${theme.shape.borderRadius * 2}px 0 0 ${theme.shape.borderRadius * 2}px`,
                 borderLeft: (theme) => `1px solid ${theme.palette.divider}`,
             }}
@@ -34,15 +36,42 @@ function WidgetBarBase() {
                     width: '100%',
                     flexGrow: 1,
                     overflowY: 'auto',
-                    px: 1,
+                    p: 1,
+                    gap: 1,
                 }}
             >
-            {
-                widgetBarState?.type?.components &&                
-                widgetBarState.type.components.map((Component, index) => (
-                    <Component key={"C_" + index} />
-                ))
-            }
+                {
+                    widgetBarState?.editMode &&
+                    <Box
+                        sx={{
+                            backgroundColor: (theme) => alpha(theme.palette.warning.main, 0.6),
+                            border: (theme) => `1px solid ${theme.palette.divider}`,
+                            borderRadius: (theme) => `${theme.shape.borderRadius}px`,
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'space-between',
+                            p: 1,
+                            mb: 1,
+                        }}
+                    >
+                        Edit Mode Enabled
+
+                        <Button
+                            variant="contained"
+                            size="small"
+                            color="primary"
+                            onClick={() => updateWidgetBarEditMode(false)}
+                        >
+                            End
+                        </Button>
+                    </Box>
+                }
+                {
+                    widgetBarState?.type?.components &&
+                    widgetBarState.type.components.map((Component, index) => (
+                        <Component key={"C_" + index} />
+                    ))
+                }
             </Box>
 
             {/* Footer */}
@@ -51,6 +80,7 @@ function WidgetBarBase() {
                     display: 'flex',
                     flexDirection: 'column',
                     width: '100%',
+                    height: 'fit-content',
                     marginTop: 'auto',
                 }}
             >
@@ -61,6 +91,14 @@ function WidgetBarBase() {
                     ))
                 }
             </Box>
+
+            {/* Floating Components */}
+            {
+                widgetBarState?.type?.floatingComponents &&
+                widgetBarState.type.floatingComponents.map((Component, index) => (
+                    <Component key={"FL_" + index} />
+                ))
+            }
         </Box>
     );
 }
