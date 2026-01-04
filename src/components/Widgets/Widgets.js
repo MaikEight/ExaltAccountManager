93632afb114be7994d62bfa4060eed4855191564
MaikEight <mail@maik8.de>
2026-01-04 01:45:56 +0100

    ✨ Add BASIC_ACTIONS, BEST_CHARACTERS, and AUDITLOG widgets with respective configurations

--- src/components/Widgets/Widgets.js
+++ src/components/Widgets/Widgets.js
@@ -3,10 +3,17 @@ import TestWidget from './Widgets/TestWidget';
 import StatsWidget from './Widgets/StatsWidget';
 import ActivityWidget from './Widgets/ActivityWidget';
 import QuickActionsWidget from './Widgets/QuickActionsWidget';
+import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
+import BasicActions from './Widgets/BasicActions';
+import AutoAwesomeMosaicOutlinedIconRotate from '../Icons/AutoAwesomeMosaicOutlinedIconRotate';
+import BestCharactersWidget from './Widgets/BestCharactersWidget';
+import AuditLogWidget from './Widgets/AuditLogWidget';
 
 export class Widgets {
     static getWidgetByType(type) {
         switch (type) {
+            case Widgets.BASIC_ACTIONS.type:
+                return Widgets.BASIC_ACTIONS;
             case Widgets.TEST.type:
                 return Widgets.TEST;
             case Widgets.STATS.type:
@@ -15,14 +22,62 @@ export class Widgets {
                 return Widgets.ACTIVITY;
             case Widgets.QUICK_ACTIONS.type:
                 return Widgets.QUICK_ACTIONS;
+            case Widgets.BEST_CHARACTERS.type:
+                return Widgets.BEST_CHARACTERS;
+            case Widgets.AUDITLOG.type:
+                return Widgets.AUDITLOG;
             default:
                 return null;
+        }
+    }
+
+    static BASIC_ACTIONS = {
+        type: 'BASIC_ACTIONS',
+        name: 'Actions',
+        icon: AutoAwesomeMosaicOutlinedIconRotate,
+        Component: BasicActions,
+        minSlots: 1,
+        maxSlots: 2,
+        defaultConfig: {
+            slots: 2,
+            settings: {
+
+            }
+        }
+    }
+
+    static BEST_CHARACTERS = {
+        type: 'BEST_CHARACTERS',
+        name: 'Best Characters',
+        Component: BestCharactersWidget,
+        minSlots: 1,
+        maxSlots: 2,
+        defaultConfig: {
+            slots: 1,
+            settings: {
+
+            }
+        }
+    }
+
+    static AUDITLOG = {
+        type: 'AUDITLOG',
+        name: 'Audit Log',
+        Component: AuditLogWidget,
+        minSlots: 1,
+        maxSlots: 2,
+        defaultConfig: {
+            slots: 1,
+            settings: {
+                
+            }
         }
     }
 
     static TEST = {
         type: 'TEST',
         name: 'Test Widget',
+        icon: EditOutlinedIcon,
         Component: TestWidget,
         minSlots: 1, // Minimum slots this widget can occupy
         maxSlots: 2, // Maximum slots this widget can occupy
