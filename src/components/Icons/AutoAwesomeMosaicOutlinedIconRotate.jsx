899be83ed64198c00f28585aa47eaa19f9b012e7
MaikEight <mail@maik8.de>
2026-01-04 01:43:53 +0100

    ✨ Add AutoAwesomeMosaicOutlinedIconRotate component with rotation style

---
+++ src/components/Icons/AutoAwesomeMosaicOutlinedIconRotate.jsx
@@ -0,0 +1,10 @@
+
+import AutoAwesomeMosaicOutlinedIcon from '@mui/icons-material/AutoAwesomeMosaicOutlined';
+
+function AutoAwesomeMosaicOutlinedIconRotate() {
+    return (
+        <AutoAwesomeMosaicOutlinedIcon style={{ transform: 'rotate(90deg)' }} />
+    )
+}
+
+export default AutoAwesomeMosaicOutlinedIconRotate;
