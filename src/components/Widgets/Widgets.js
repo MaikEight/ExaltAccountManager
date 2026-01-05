2da4fdd82e5ef7fa6842d69fa2b93b6634b71df4
MaikEight <mail@maik8.de>
2026-01-05 02:23:01 +0100

    ✨ Add ACCOUNT_DETAILS widget with configuration for user data fields

--- src/components/Widgets/Widgets.js
+++ src/components/Widgets/Widgets.js
@@ -8,6 +8,7 @@ import BasicActionsWidget from './Widgets/BasicActionsWidget';
 import AutoAwesomeMosaicOutlinedIconRotate from '../Icons/AutoAwesomeMosaicOutlinedIconRotate';
 import BestCharactersWidget from './Widgets/BestCharactersWidget';
 import AuditLogWidget from './Widgets/AuditLogWidget';
+import TableDataWidget from './Widgets/TableDataWidget';
 
 export class Widgets {
     static getWidgetByType(type) {
@@ -26,6 +27,8 @@ export class Widgets {
                 return Widgets.BEST_CHARACTERS;
             case Widgets.AUDITLOG.type:
                 return Widgets.AUDITLOG;
+            case Widgets.ACCOUNT_DETAILS.type:
+                return Widgets.ACCOUNT_DETAILS;
             default:
                 return null;
         }
@@ -70,6 +73,103 @@ export class Widgets {
             slots: 1,
             settings: {
                 
+            }
+        }
+    }
+
+    static ACCOUNT_DETAILS = {
+        type: 'ACCOUNT_DETAILS',
+        name: 'Account Details',
+        Component: TableDataWidget,
+        dataSourceObject: null, // to be used for sub-data sources
+        editFunctionType: 'ACCOUNT',
+        minSlots: 1,
+        maxSlots: 2,
+        defaultConfig: {
+            slots: 1,
+            settings: {                
+                fields: {
+                    email: {
+                        dataField: 'email',
+                        displayName: 'Email',
+                        visible: true,
+                        editable: false,
+                        dataType: 'string',
+                        showCopyButton: true,
+                    },
+                    steamId: {
+                        dataField: 'steamId',
+                        displayName: 'Steam ID',
+                        visible: false,
+                        editable: false,
+                        dataType: 'string',
+                        showCopyButton: true,
+                    },
+                    name: {
+                        dataField: 'name',
+                        displayName: 'Name',
+                        visible: true,
+                        editable: false,
+                        dataType: 'string',
+                        showCopyButton: true,
+                    },
+                    password: {
+                        dataField: 'password',
+                        displayName: 'Password',
+                        visible: true,
+                        editable: true,
+                        dataType: 'password',
+                        showCopyButton: false,
+                    },
+                    performDailyLogin: {
+                        dataField: 'performDailyLogin',
+                        displayName: 'Daily Login',
+                        visible: true,
+                        editable: true,
+                        dataType: 'boolean',
+                        showCopyButton: false,
+                    },
+                    lastLogin: {
+                        dataField: 'lastLogin',
+                        displayName: 'Last Login',
+                        visible: true,
+                        editable: false,
+                        dataType: 'datetime',
+                        showCopyButton: false,
+                    },
+                    lastRefresh: {
+                        dataField: 'lastRefresh',
+                        displayName: 'Last Refresh',
+                        visible: false,
+                        editable: false,
+                        dataType: 'datetime',
+                        showCopyButton: false,
+                    },
+                    serverName: {
+                        dataField: 'serverName',
+                        displayName: 'Server',
+                        visible: false,
+                        editable: false,
+                        dataType: 'server',
+                        showCopyButton: false,
+                    },
+                    state: {
+                        dataField: 'state',
+                        displayName: 'State',
+                        visible: false,
+                        editable: false,
+                        dataType: 'state',
+                        showCopyButton: false,
+                    },
+                    orderId: {
+                        dataField: 'orderId',
+                        displayName: 'Order ID',
+                        visible: false,
+                        editable: false,
+                        dataType: 'string',
+                        showCopyButton: false,
+                    },
+                }
             }
         }
     }
