b44767cd69dec5691cd1e9a3827c866aa2f113ea
MaikEight <mail@maik8.de>
2026-01-06 01:21:46 +0100

    ✨ Enhance EamIconButton with customizable tooltip background and improved hover effects

--- src/components/EamIconButton.jsx
+++ src/components/EamIconButton.jsx
@@ -1,6 +1,6 @@
 import { Box, Typography } from "@mui/material";
 
-function EamIconButton({ icon, tooltip, tooltipDirection = 'left', onClick, sx }) {
+function EamIconButton({ icon, tooltip, tooltipDirection = 'left', tooltipBackground = 'background.paperLight', onClick, sx }) {
     tooltipDirection = ['left', 'right'].includes(tooltipDirection) ? tooltipDirection : 'left';
     
     return (
@@ -8,15 +8,23 @@ function EamIconButton({ icon, tooltip, tooltipDirection = 'left', onClick, sx }
             sx={{
                 position: 'relative',
                 display: 'inline-flex',
+                zIndex: 1,
+                '&:hover': {
+                    zIndex: 10,
+                },
                 '&:hover .tooltip-box': {
                     maxWidth: '200px',
                     opacity: 1,
+                },
+                '&:hover .icon-box': {
+                    transform: 'scale(1.1)',
                 },
                 ...sx
             }}
         >
             {/* Icon - no background */}
             <Box
+                className="icon-box"
                 sx={{
                     display: 'flex',
                     justifyContent: 'center',
@@ -26,6 +34,7 @@ function EamIconButton({ icon, tooltip, tooltipDirection = 'left', onClick, sx }
                     cursor: 'pointer',
                     position: 'relative',
                     zIndex: 2,
+                    transition: 'transform 0.2s ease-in-out',
                     ...(!tooltip && {
                         '&:hover': {
                             backgroundColor: 'action.hover',
@@ -44,6 +53,7 @@ function EamIconButton({ icon, tooltip, tooltipDirection = 'left', onClick, sx }
             {tooltip && (
                 <Box
                     className="tooltip-box"
+                    onClick={onClick}
                     sx={{
                         position: 'absolute',
                         top: 0,
@@ -51,7 +61,7 @@ function EamIconButton({ icon, tooltip, tooltipDirection = 'left', onClick, sx }
                         height: '30px',
                         display: 'flex',
                         alignItems: 'center',
-                        backgroundColor: 'action.hover',
+                        backgroundColor: tooltipBackground,
                         border: '1px solid',
                         borderColor: 'divider',
                         borderRadius: '15px',
@@ -59,7 +69,8 @@ function EamIconButton({ icon, tooltip, tooltipDirection = 'left', onClick, sx }
                         maxWidth: '30px',
                         opacity: 0,
                         transition: 'max-width 0.3s ease-in-out, opacity 0.2s ease-in-out',
-                        pointerEvents: 'none',
+                        cursor: 'pointer',
+                        zIndex: 1,
                         [tooltipDirection === 'right' ? 'paddingLeft' : 'paddingRight']: '20px',
                         [tooltipDirection === 'left' ? 'justifyContent' : 'justifyContent']: 'flex-start',
                     }}
