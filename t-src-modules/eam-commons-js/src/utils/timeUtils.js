23ca210d19eed0a2f5cc2f3f44affee88f6d82c9
MaikEight <mail@maik8.de>
2026-01-20 02:42:46 +0100

    ✨ Update formatTime function to support dateOnly parameter for flexible formatting

--- t-src-modules/eam-commons-js/src/utils/timeUtils.js
+++ t-src-modules/eam-commons-js/src/utils/timeUtils.js
@@ -1,24 +1,26 @@
 
-function formatTime(time) {
-  if(!time) return null;
-  // Convert the string to a Date object
-  const date = typeof time !== 'object' ? new Date(time) : time;
+function formatTime(time, dateOnly = false) {
+    if (!time) return null;
+    // Convert the string to a Date object
+    const date = typeof time !== 'object' ? new Date(time) : time;
 
-  // Create a DateTimeFormat object with the desired format
-  const formatter = new Intl.DateTimeFormat('en-GB', {
-      year: 'numeric',
-      month: '2-digit',
-      day: '2-digit',
-      hour: '2-digit',
-      minute: '2-digit',
-      hour12: false,
-  });
+    // Create a DateTimeFormat object with the desired format
+    const formatter = new Intl.DateTimeFormat('en-GB', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        ...(!dateOnly ? {
+            hour: '2-digit',
+            minute: '2-digit',
+            hour12: false,
+        } : {})
+    });
 
-  // Format the date and time
-  const formattedDateTime = formatter.format(date);
+    // Format the date and time
+    const formattedDateTime = formatter.format(date);
 
-  // Replace '/' with '.' and remove commas
-  return formattedDateTime.replace(/\//g, '.').replace(/,/g, '');
+    // Replace '/' with '.' and remove commas
+    return formattedDateTime.replace(/\//g, '.').replace(/,/g, '');
 }
 
 function getCurrentTime() {
