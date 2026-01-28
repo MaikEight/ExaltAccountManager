34650de8d548657a5d8b28125e9023959ad4155c
MaikEight <mail@maik8.de>
2026-01-29 00:57:40 +0100

    ✨ Update mod.rs to expose send_and_parse_char_list_request function

--- t-src-modules/eam_commons/src/requests/mod.rs
+++ t-src-modules/eam_commons/src/requests/mod.rs
@@ -1,8 +1,8 @@
 pub mod account_verify;
-pub use account_verify::{send_account_verify_request};
+pub use account_verify::send_account_verify_request;
 
 pub mod char_list;
-pub use char_list::{send_char_list_request};
+pub use char_list::{send_char_list_request, send_and_parse_char_list_request};
 
 pub mod types;
 pub use types::{ApiLimiterBlocked, GameAccessToken};
