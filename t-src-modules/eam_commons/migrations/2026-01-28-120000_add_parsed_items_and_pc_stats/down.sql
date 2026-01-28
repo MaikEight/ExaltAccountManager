359b71b390dbd4cd3c556bb531a5e5fdc11a4125
MaikEight <mail@maik8.de>
2026-01-29 00:56:29 +0100

    🗃️ Add down migration for ParsedItems and PcStats tables

---
+++ t-src-modules/eam_commons/migrations/2026-01-28-120000_add_parsed_items_and_pc_stats/down.sql
@@ -0,0 +1,6 @@
+DROP INDEX IF EXISTS idx_pc_stats_entry_char;
+DROP TABLE IF EXISTS PcStats;
+
+DROP INDEX IF EXISTS idx_parsed_items_item_id;
+DROP INDEX IF EXISTS idx_parsed_items_entry_id;
+DROP TABLE IF EXISTS ParsedItems;
