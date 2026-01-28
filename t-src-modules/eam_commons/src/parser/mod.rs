f0582397bccc4d2c89f60f598f3534fae268cba0
MaikEight <mail@maik8.de>
2026-01-29 00:57:13 +0100

    ✨ Organize parser modules and expose server and request state parsing functions

--- t-src-modules/eam_commons/src/parser/mod.rs
+++ t-src-modules/eam_commons/src/parser/mod.rs
@@ -2,3 +2,9 @@ pub mod parser;
 pub use parser::parse_char_list;
 
 pub mod character_stats_mapper;
+
+pub mod server_parser;
+pub use server_parser::parse_servers;
+
+pub mod request_state_parser;
+pub use request_state_parser::{parse_request_state, parse_account_name, RequestState};
