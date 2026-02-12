d763435ae689e96c5b6418a0fbad29f377240bad
MaikEight <mail@maik8.de>
2026-02-12 01:09:25 +0100

    ✨ Add Vault Peeker V2 component exports for easy importing

---
+++ src/components/VaultPeeker/V2/index.js
@@ -0,0 +1,15 @@
+// Vault Peeker V2 Components
+// Exports all V2 components for easy importing
+
+export { default as ItemGridV2 } from './ItemGridV2';
+export { default as ItemCanvasGridV2 } from './ItemCanvasGridV2';
+export { default as TotalsViewV2 } from './TotalsViewV2';
+export { default as ExportModalV2 } from './ExportModalV2';
+export { default as AccountsViewV2 } from './AccountsViewV2';
+export { default as AccountViewV2 } from './AccountViewV2';
+export { default as CharacterCardV2 } from './CharacterCardV2';
+export { default as CharacterGridV2 } from './CharacterGridV2';
+export { default as StorageContainerV2, StorageModalV2, STORAGE_CONFIG } from './StorageContainerV2';
+export { default as ItemFilterBoxV2 } from './ItemFilterBoxV2';
+export { default as FilterPresetSelector } from './FilterPresetSelector';
+export { default as ItemDetailPopoverV2 } from './ItemDetailPopoverV2';
