af75023451c005ed6459da34e0a297a29d9b4425
MaikEight <mail@maik8.de>
2026-02-12 01:12:40 +0100

    🔧 Fix typo in defaultSettings for vaultPeeker: corrected 'collapsedFileds' to 'collapsedFields'

--- t-src-modules/eam-commons-js/src/contexts/UserSettingsContext.jsx
+++ t-src-modules/eam-commons-js/src/contexts/UserSettingsContext.jsx
@@ -68,7 +68,7 @@ const defaultSettings = {
         //HIDDEN: exePath: string // Path to the game executable, store in the database
     },
     vaultPeeker: {
-        collapsedFileds: {
+        collapsedFields: {
             filter: true,
             totals: false,
             accounts: [] //Array of account emails that are collapsed
@@ -77,6 +77,7 @@ const defaultSettings = {
             hiddenVaults: [], //Array of vault names that are hidden
         },
         rowsPerPage: 10,
+        density: 'comfortable', // 'dense' | 'comfortable' | 'spacious'
     },
     backgroundSync: {
         enabled: true,
