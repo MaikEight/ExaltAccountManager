99966e8b7eb26fb1cb39df334ebbc9be2b966622
MaikEight <mail@maik8.de>
2026-03-07 17:48:47 +0100

    🎨 Refactor logging in getEamPlusPrices: replace console.log with log function for improved consistency and error handling

--- t-src-modules/eam-commons-js/src/backend/eamPaymentsApi.js
+++ t-src-modules/eam-commons-js/src/backend/eamPaymentsApi.js
@@ -1,5 +1,5 @@
 import { invoke } from "@tauri-apps/api/core";
-import { logToErrorLog } from "../utils/loggingUtils";
+import { log, logToErrorLog } from "../utils/loggingUtils";
 
 async function getEamPlusPrices() {
     const requestHeaders = new Map();
@@ -18,13 +18,13 @@ async function getEamPlusPrices() {
         });
 
         if (sessionStorage.getItem('flag:debug') === 'true') {
-            console.log('getEamPlusPrices response', response);
+            log('getEamPlusPrices response', response);
         }
 
         if (sessionStorage.getItem('flag:copy:eam-plus-prices') === 'true') {
             navigator.clipboard.writeText(JSON.stringify(response, null, 2))
                 .then(() => console.log('getEamPlusPrices response copied to clipboard'))
-                .catch(err => console.error('Failed to copy getEamPlusPrices response: ', err));
+                .catch(err => logToErrorLog('Failed to copy getEamPlusPrices response: ', err));
         }
 
         return response || null;
