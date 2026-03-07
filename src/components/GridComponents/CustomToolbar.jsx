75102d9304c5709c69864a98de8f868820c6bcf8
MaikEight <mail@maik8.de>
2026-03-07 17:42:34 +0100

    🎨 Refactor CustomToolbar: streamline imports, enhance export success notification, and improve snackbar display

--- src/components/GridComponents/CustomToolbar.jsx
+++ src/components/GridComponents/CustomToolbar.jsx
@@ -1,6 +1,6 @@
 import { useTheme } from "@emotion/react";
 import { Badge, Box, Button, Checkbox, FormControlLabel, LinearProgress, Popover, Tooltip, Typography } from "@mui/material";
-import { ColumnsPanelTrigger, FilterPanelTrigger, GridToolbarColumnsButton, GridToolbarContainer, GridToolbarFilterButton, Toolbar, ToolbarButton } from "@mui/x-data-grid";
+import { ColumnsPanelTrigger, FilterPanelTrigger, Toolbar, ToolbarButton } from "@mui/x-data-grid";
 import Searchbar from "./Searchbar";
 import AddIcon from '@mui/icons-material/Add';
 import FileUploadOutlinedIcon from '@mui/icons-material/FileUploadOutlined';
@@ -13,17 +13,87 @@ import Papa from 'papaparse';
 import { invoke } from "@tauri-apps/api/core";
 import ViewColumnIcon from '@mui/icons-material/ViewColumn';
 import FilterListIcon from '@mui/icons-material/FilterList';
+import { EXPORT_SUCCESS_MESSAGES, MASCOT_NAME } from "../../constants";
+import isMacOS from "../../utils/isMacOS";
+import useSnack from "../../hooks/useSnack";
 
 function CustomToolbar({ onSearchChanged, onAddNew }) {
+    const { accounts } = useAccounts();
+    const theme = useTheme();
+    const { showSnackbar } = useSnack();
+
     const [anchorElPopover, setAnchorElPopover] = useState(null);
     const [includeAditionalColumns, setIncludeAditionalColumns] = useState(false);
     const [isExporting, setIsExporting] = useState(false);
 
-    const theme = useTheme();
-    const { accounts } = useAccounts();
-
     const openExportPopover = Boolean(anchorElPopover);
     const popoverId = openExportPopover ? 'export-popover' : undefined;
+    const isMac = isMacOS();
+
+    const sendExportSucceededSnackbar = () => {
+        const message = EXPORT_SUCCESS_MESSAGES[Math.floor(Math.random() * EXPORT_SUCCESS_MESSAGES.length)];
+
+        let snackbarKey = null;
+        snackbarKey = showSnackbar(
+            (
+                <Box
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        gap: 2,
+                    }}
+                >
+                    <img
+                        src="/mascot/Happy/happy_very_low_res.png"
+                        alt={`${MASCOT_NAME} notifies you`}
+                        style={{ width: '120px' }}
+                    />
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            textAlign: 'center',
+                        }}
+                    >
+                        <Typography variant="body1">
+                            {message}
+                        </Typography>
+                    </Box>
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            flexDirection: 'row',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            gap: 2,
+                        }}
+                    >
+                        <StyledButton
+                            variant="contained"
+                            size="small"
+                            onClick={async () => {
+                                // Open the folder containing the exported file
+                                const downloadsPath = await invoke('get_download_directory');
+                                await invoke('open_folder_in_explorer', { path: downloadsPath });
+
+                                if (snackbarKey) {
+                                    closeSnackbar(snackbarKey);
+                                }
+                            }}
+                        >
+                            Open File in {isMac ? 'Finder' : 'Explorer'}
+                        </StyledButton>
+                    </Box>
+                </Box>
+            ),
+            'message',
+            true //persistent
+        );
+    };
 
     const decryptPassword = async (password) => {
         try {
@@ -40,7 +110,10 @@ function CustomToolbar({ onSearchChanged, onAddNew }) {
         let blob;
         let fileName;
 
-        const accs = accounts;
+        const accs = [];
+        for (const acc of accounts) {
+            accs.push({ ...acc });
+        }
         for (const acc of accs) {
             acc.password = await decryptPassword(acc.password);
         }
@@ -74,6 +147,7 @@ function CustomToolbar({ onSearchChanged, onAddNew }) {
         a.download = fileName;
         a.click();
         URL.revokeObjectURL(url);
+        sendExportSucceededSnackbar();
 
         setIsExporting(false);
         handleCloseExportPopover();
@@ -214,7 +288,7 @@ function CustomToolbar({ onSearchChanged, onAddNew }) {
                 }}
                 slotProps={{
                     paper: {
-                        sx:{
+                        sx: {
                             border: '1px solid',
                             borderColor: 'divider',
                         }
@@ -300,173 +374,6 @@ function CustomToolbar({ onSearchChanged, onAddNew }) {
                 </Box>
             </Popover>
         </Toolbar>
-    );
-
-    return (
-        <Box style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', borderRadius: theme.shape.borderRadius }}>
-            <GridToolbarContainer
-                sx={{
-                    display: "flex",
-                    justifyContent: "start",
-                    minHeight: 49,
-                    backgroundColor: theme.palette.background.paper,
-                    borderRadius: `${theme.shape.borderRadius}px ${theme.shape.borderRadius}px 0 0`,
-                    pt: 0.5,
-                    pb: 0.5,
-                    pl: 1,
-                    pr: 1,
-                }}
-            >
-                <Tooltip title="Show/Hide Columns">
-                    <GridToolbarColumnsButton />
-                </Tooltip>
-                <GridToolbarFilterButton />
-                <Tooltip title="Export Accounts">
-                    <Button
-                        variant="text"
-                        color="primary"
-                        size="small"
-                        sx={{
-                            mt: 0.5,
-                            mb: 0.5,
-                        }}
-                        startIcon={<FileUploadOutlinedIcon />}
-                        onClick={handleClickExport}
-                    >
-                        Export
-                    </Button>
-                </Tooltip>
-            </GridToolbarContainer>
-            <Box
-                sx={{
-                    display: 'flex',
-                    flexDirection: 'row',
-                    justifyContent: 'end',
-                    alignItems: 'center',
-                }}
-            >
-                {
-                    onAddNew &&
-                    <Tooltip title="Add Accounts">
-                        <Button
-                            variant="text"
-                            color="primary"
-                            onClick={onAddNew}
-                            size="small"
-                            sx={{
-                                mt: 0.5,
-                                mb: 0.5,
-                            }}
-                            startIcon={<AddIcon />}
-                        >
-                            Add
-                        </Button>
-                    </Tooltip>
-                }
-                <Box
-                    sx={{
-                        mr: 0.5,
-                        display: 'flex',
-                        flexDirection: 'row',
-                        maxHeight: 49,
-                    }}
-                >
-                    <Searchbar onSearchChanged={onSearchChanged} />
-                </Box>
-            </Box>
-            <Popover
-                id={popoverId}
-                open={openExportPopover}
-                anchorEl={anchorElPopover}
-                onClose={handleCloseExportPopover}
-                anchorOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'center',
-                }}
-                transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'center',
-                }}
-            >
-                <Box
-                    sx={{
-                        position: 'relative',
-                        pt: 2,
-                        px: 2,
-                        borderRadius: `${theme.shape.borderRadius}px`,
-                    }}
-                >
-                    {
-                        isExporting &&
-                        <LinearProgress
-                            sx={{
-                                position: 'absolute',
-                                top: 0,
-                                left: 0,
-                                right: 0,
-                            }}
-                        />
-                    }
-                    <Typography
-                        variant="body1"
-                    >
-                        Please choose the format you want to export to have
-                    </Typography>
-                    <FormControlLabel
-                        control={
-                            <Checkbox
-                                disabled={isExporting}
-                                value={includeAditionalColumns}
-                                onChange={(event) => setIncludeAditionalColumns(event.target.checked)}
-                            />
-                        }
-                        label={
-                            <Typography
-                                variant="body2"
-                            >
-                                Include additional columns
-                            </Typography>
-                        }
-                    />
-                </Box>
-                <Box
-                    sx={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        px: 1,
-                        pb: 1,
-                        gap: 0.5,
-                    }}
-                >
-                    <StyledButton
-                        disabled={isExporting}
-                        color="primary"
-                        size="small"
-                        sx={{
-                            mt: 0.5,
-                            mb: 0.5,
-                        }}
-                        startIcon={<ListOutlinedIcon />}
-                        onClick={() => handleExport('CSV')}
-                    >
-                        CSV
-                    </StyledButton>
-                    <StyledButton
-                        disabled={isExporting}
-                        color="primary"
-                        size="small"
-                        sx={{
-                            mt: 0.5,
-                            mb: 0.5,
-                        }}
-                        startIcon={<DataArrayOutlinedIcon />}
-                        onClick={() => handleExport('JSON')}
-                    >
-                        JSON
-                    </StyledButton>
-                </Box>
-            </Popover>
-        </Box>
     );
 }
 
